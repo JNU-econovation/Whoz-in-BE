817760c440e01059faad89b69e38b5fa15bee613
Kimjongmin <rlajm1203@naver.com>
2024-03-09 21:26:11 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/econovation/bangdong/bangdong/BangdongApplication.java
@@ -0,0 +1,13 @@
+package com.econovation.bangdong.bangdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangdongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangdongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/econovation/bangdong/bangdong/BangdongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econovation.bangdong.bangdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangdongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
