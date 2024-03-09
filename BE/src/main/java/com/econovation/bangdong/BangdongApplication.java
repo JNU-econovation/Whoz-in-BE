945e414fceb82b33143286bbd7dbb00f8c7bce03
coco3x <inferior3x@gmail.com>
2024-03-09 22:56:29 +0900

    chore: 패키지 구조 수정 및 DB 설정

--- BE/src/main/java/com/econovation/bangdong/BangdongApplication.java
+++ BE/src/main/java/com/econovation/bangdong/BangdongApplication.java
@@ -1,4 +1,4 @@
-package com.econovation.bangdong.bangdong;
+package com.econovation.bangdong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BE/src/test/java/com/econovation/bangdong/BangdongApplicationTests.java
+++ BE/src/test/java/com/econovation/bangdong/BangdongApplicationTests.java
@@ -1,4 +1,4 @@
-package com.econovation.bangdong.bangdong;
+package com.econovation.bangdong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
