4bef9fab4c224170edad460d801f852999e7558b
KimJongMin <whdals011203@naver.com>
2024-09-27 14:14:38 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/whozin/whozin/WhozInApplication.java
@@ -0,0 +1,13 @@
+package com.whozin.whozin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhozInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhozInApplication.class, args);
+    }
+
+}
