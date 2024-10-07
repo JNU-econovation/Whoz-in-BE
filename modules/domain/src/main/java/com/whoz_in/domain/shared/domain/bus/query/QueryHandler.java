4013bc567d6e6810b7324469f58a58dd5b33ff7f
coco3x <inferior3x@gmail.com>
2024-10-08 03:42:09 +0900

    fix: Command / Query Handler를 인터페이스에서 추상 클래스로 변경합니다.

--- modules/domain/src/main/java/com/whoz_in/domain/shared/domain/bus/query/QueryHandler.java
+++ modules/domain/src/main/java/com/whoz_in/domain/shared/domain/bus/query/QueryHandler.java
@@ -3,6 +3,6 @@ package com.whoz_in.domain.shared.domain.bus.query;
 /*
 형변환 없이 구현체마다 다른 Response를 제공하기 위해 제네릭 사용
  */
-public interface QueryHandler<Q extends Query, R extends Response> {
-    R handle(Q query);
+public abstract class QueryHandler<Q extends Query, R extends Response> {
+    public abstract R handle(Q query);
 }
