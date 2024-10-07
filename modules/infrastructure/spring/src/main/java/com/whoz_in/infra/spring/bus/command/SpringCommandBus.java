9bcc1f4dd5c888bb1ddd2d85125699ce2d9215c0
coco3x <inferior3x@gmail.com>
2024-10-08 03:46:30 +0900

    feat: spring framework로 버스를 구현합니다

---
+++ modules/infrastructure/spring/src/main/java/com/whoz_in/infra/spring/bus/command/SpringCommandBus.java
@@ -0,0 +1,23 @@
+package com.whoz_in.infra.spring.bus.command;
+
+import com.whoz_in.domain.shared.domain.bus.command.Command;
+import com.whoz_in.domain.shared.domain.bus.command.CommandBus;
+import com.whoz_in.domain.shared.domain.bus.command.CommandHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/*
+스프링 컨텍스트 내의 핸들러를 찾아 바로 호출하도록 구현되었으므로 동기적으로 동작합니다.
+ */
+@Component
+@RequiredArgsConstructor
+public final class SpringCommandBus implements CommandBus {
+    private final CommandHandlers handlers;
+
+    @Override
+    public void dispatch(Command command){
+        CommandHandler handler = handlers.findBy(command);
+        handler.handle(command);
+    }
+}
+
