6c8b870f6c1de408ec6e28e0c10effcd19433602
coco3x <inferior3x@gmail.com>
2024-10-10 02:43:57 +0900

    feat(domain): 올바르지 않은 Mac 주소일 경우 발생하는 예외

---
+++ modules/domain/src/main/java/com/whoz_in/domain/device/domain/exception/InvalidMacAddressException.java
@@ -0,0 +1,9 @@
+package com.whoz_in.domain.device.domain.exception;
+
+import com.whoz_in.domain.shared.domain.BusinessException;
+
+public class InvalidMacAddressException extends BusinessException {
+    public InvalidMacAddressException() {
+        super("3005", "올바르지 않은 Mac 주소입니다.");
+    }
+}
