96bf5ed89dc47525964fdfa16127daa3762eca07
coco3x <inferior3x@gmail.com>
2024-10-10 03:19:37 +0900

    comment(domain): 주석을 수정합니다.

--- modules/domain/src/main/java/com/whoz_in/domain/shared/domain/AggregateRoot.java
+++ modules/domain/src/main/java/com/whoz_in/domain/shared/domain/AggregateRoot.java
@@ -5,17 +5,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-//애그리거트 루트는 무조건 상속해야 한다.
 public abstract class AggregateRoot {
+    //애그리거트 루트는 자신에 관한 도메인 이벤트를 가지고 있습니다.
     private List<DomainEvent> domainEvents = new ArrayList<>();
 
+    //응용 로직에게 발행할 이벤트를 제공합니다.
     public final List<DomainEvent> pullDomainEvents() {
         List<DomainEvent> events = domainEvents;
         this.domainEvents = Collections.emptyList();
         return events;
     }
 
-    //AggregateRoot 구현체만 이벤트를 등록할 수 있으므로 protected
+    //애그리거트 루트의 변경은 자신이 직접 관리해야 하므로 protected입니다.
     protected final void register(DomainEvent event) {
         domainEvents.add(event);
     }
--- modules/domain/src/main/java/com/whoz_in/domain/shared/domain/bus/event/EventBus.java
+++ modules/domain/src/main/java/com/whoz_in/domain/shared/domain/bus/event/EventBus.java
@@ -2,7 +2,6 @@ package com.whoz_in.domain.shared.domain.bus.event;
 
 import java.util.List;
 
-//DomainEvent
 public interface EventBus {
     void publish(List<DomainEvent> events);
 }
