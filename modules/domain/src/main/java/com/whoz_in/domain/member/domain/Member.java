4cea0a0d6096ff0ee0b42388d92cb33dc14712a3
westzeroright <kt8267@naver.com>
2024-10-09 10:14:29 +0900

    feat(domain): 상속할 수 없도록 final 클래스로 설정

--- modules/domain/src/main/java/com/whoz_in/domain/member/domain/Member.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/domain/Member.java
@@ -2,6 +2,6 @@ package com.whoz_in.domain.member.domain;
 
 import com.whoz_in.domain.shared.domain.AggregateRoot;
 
-public class Member extends AggregateRoot {
+public final class Member extends AggregateRoot {
 
 }
