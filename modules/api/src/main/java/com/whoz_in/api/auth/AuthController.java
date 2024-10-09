b9038ce2420c4703e6912ed59caf2008bc51ba23
westzeroright <kt8267@naver.com>
2024-10-09 10:07:27 +0900

    feat(api): 모든 컨트롤러는 CommandQueryController를 구현

--- modules/api/src/main/java/com/whoz_in/api/auth/AuthController.java
+++ modules/api/src/main/java/com/whoz_in/api/auth/AuthController.java
@@ -1,5 +1,12 @@
 package com.whoz_in.api.auth;
 
-public class AuthController {
+import com.whoz_in.api.shared.CommandQueryController;
+import com.whoz_in.domain.shared.domain.bus.command.CommandBus;
+import com.whoz_in.domain.shared.domain.bus.query.QueryBus;
+
+public class AuthController extends CommandQueryController {
+  public AuthController(CommandBus commandBus, QueryBus queryBus) {
+    super(commandBus, queryBus);
+  }
 
 }
--- modules/api/src/main/java/com/whoz_in/api/member/MemberController.java
+++ modules/api/src/main/java/com/whoz_in/api/member/MemberController.java
@@ -1,5 +1,12 @@
 package com.whoz_in.api.member;
 
-public class MemberController {
+import com.whoz_in.api.shared.CommandQueryController;
+import com.whoz_in.domain.shared.domain.bus.command.CommandBus;
+import com.whoz_in.domain.shared.domain.bus.query.QueryBus;
+
+public class MemberController extends CommandQueryController {
+  public MemberController(CommandBus commandBus, QueryBus queryBus) {
+    super(commandBus,queryBus);
+  }
 
 }
