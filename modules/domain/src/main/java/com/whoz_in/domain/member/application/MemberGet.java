a29b31289b590a866fd987406fc94e8212163ec8
westzeroright <kt8267@naver.com>
2024-10-09 15:13:58 +0900

    refactor(domain): 핸들러 관련 클래스명 수정

--- modules/domain/src/main/java/com/whoz_in/domain/member/application/MemberGet.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/application/MemberGet.java
@@ -2,6 +2,6 @@ package com.whoz_in.domain.member.application;
 
 import com.whoz_in.domain.shared.domain.bus.query.Query;
 
-public class MemberServiceGet implements Query {
+public class MemberGet implements Query {
 
 }
