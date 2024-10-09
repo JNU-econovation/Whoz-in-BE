2a2cd86e775b28fe729b4c30017641505ad2c783
westzeroright <kt8267@naver.com>
2024-10-10 00:00:58 +0900

    feat(infra-jpa): SoftDeleteListener 사용

--- modules/infrastructure/jpa/src/main/java/com/whoz_in/infra/jpa/device/DeviceEntity.java
+++ modules/infrastructure/jpa/src/main/java/com/whoz_in/infra/jpa/device/DeviceEntity.java
@@ -1,15 +1,8 @@
 package com.whoz_in.infra.jpa.device;
 
-import com.whoz_in.infra.jpa.shared.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import com.whoz_in.infra.jpa.shared.BaseEntity;
 
-@Entity
-@Where(clause = "deleted_at is null")
-@Table(name = "device")
-@SQLDelete(sql = "UPDATE device SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+//@Entity
 public class DeviceEntity extends BaseEntity {
 
 }
