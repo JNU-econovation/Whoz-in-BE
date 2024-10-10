11002f73c7d134f9ccdb46d1afb9e1a3188dac85
coco3x <inferior3x@gmail.com>
2024-10-10 15:48:20 +0900

    feat: @Handler, @DomainService 어노테이션 추가

--- modules/domain/src/main/java/com/whoz_in/domain/device/application/ExampleGetHandler.java
+++ modules/domain/src/main/java/com/whoz_in/domain/device/application/ExampleGetHandler.java
@@ -1,9 +1,10 @@
 package com.whoz_in.domain.device.application;
 
+import com.whoz_in.domain.shared.annotation.Handler;
 import com.whoz_in.domain.shared.domain.bus.query.QueryHandler;
 import org.springframework.stereotype.Component;
 
-@Component
+@Handler
 public final class ExampleGetHandler extends QueryHandler<ExampleGet, Example> {
 
     @Override
--- modules/domain/src/main/java/com/whoz_in/domain/member/application/MemberGetHandler.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/application/MemberGetHandler.java
@@ -1,9 +1,10 @@
 package com.whoz_in.domain.member.application;
 
+import com.whoz_in.domain.shared.annotation.Handler;
 import com.whoz_in.domain.shared.domain.bus.query.QueryHandler;
 import org.springframework.stereotype.Component;
 
-@Component
+@Handler
 public class MemberGetHandler extends QueryHandler<MemberGet, MemberResponse> {
 
   @Override
