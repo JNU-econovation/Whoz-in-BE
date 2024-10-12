4a9c0099f66529917cab09af76bc92b2530ede23
coco3x <inferior3x@gmail.com>
2024-10-13 05:00:54 +0900

    chore: 쓸모없는 import 제거

--- modules/domain/src/main/java/com/whoz_in/domain/device/application/ExampleGetHandler.java
+++ modules/domain/src/main/java/com/whoz_in/domain/device/application/ExampleGetHandler.java
@@ -2,7 +2,6 @@ package com.whoz_in.domain.device.application;
 
 import com.whoz_in.domain.shared.annotation.Handler;
 import com.whoz_in.domain.shared.domain.bus.query.QueryHandler;
-import org.springframework.stereotype.Component;
 
 @Handler
 public final class ExampleGetHandler extends QueryHandler<ExampleGet, Example> {
