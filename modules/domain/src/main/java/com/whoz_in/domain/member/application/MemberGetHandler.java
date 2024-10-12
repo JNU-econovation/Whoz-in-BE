727fba7cd74d6b629850a4fa194427f71eadf65f
coco3x <inferior3x@gmail.com>
2024-10-13 05:00:54 +0900

    chore: 쓸모없는 import 제거

--- modules/domain/src/main/java/com/whoz_in/domain/member/application/MemberGetHandler.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/application/MemberGetHandler.java
@@ -2,7 +2,6 @@ package com.whoz_in.domain.member.application;
 
 import com.whoz_in.domain.shared.annotation.Handler;
 import com.whoz_in.domain.shared.domain.bus.query.QueryHandler;
-import org.springframework.stereotype.Component;
 
 @Handler
 public class MemberGetHandler extends QueryHandler<MemberGet, MemberResponse> {
--- modules/domain/src/main/java/com/whoz_in/domain/shared/domain/utils/StubRequesterInfo.java
+++ modules/domain/src/main/java/com/whoz_in/domain/shared/domain/utils/StubRequesterInfo.java
@@ -2,7 +2,6 @@ package com.whoz_in.domain.shared.domain.utils;
 
 import java.util.Optional;
 import org.springframework.stereotype.Component;
-import com.whoz_in.domain.shared.domain.utils.RequesterInfo;
 
 /*
 인증인가 구현하는 사람이 구현체를 제공하기 전까지 사용하는 스텁 구현체
