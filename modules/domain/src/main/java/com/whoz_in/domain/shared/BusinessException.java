16eb4cd1568ab840a561e378d5668336293ae565
coco3x <inferior3x@gmail.com>
2024-10-13 23:29:03 +0900

    refactor(domain): abstract class이므로 생성자를 protected로 명시합니다.

--- modules/domain/src/main/java/com/whoz_in/domain/shared/BusinessException.java
+++ modules/domain/src/main/java/com/whoz_in/domain/shared/BusinessException.java
@@ -1,11 +1,12 @@
 package com.whoz_in.domain.shared;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 //비즈니스 내에서 발생하는 예외이므로 HttpStatus는 담지 않습니다.
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BusinessException extends RuntimeException{
     private final String errorCode;
     private final String errorMessage;
