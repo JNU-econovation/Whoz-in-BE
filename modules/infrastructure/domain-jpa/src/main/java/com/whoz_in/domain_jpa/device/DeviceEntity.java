7ad0d0c8df78d4d7666b6fc28b0dfad49f5f1e7b
coco3x <inferior3x@gmail.com>
2024-10-26 20:49:35 +0900

    fix(domain_jpa): 멀티 모듈에서 Entity와 Jpa Repository를 인식할 수 있도록 함

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
@@ -1,8 +1,12 @@
 package com.whoz_in.domain_jpa.device;
 
 import com.whoz_in.domain_jpa.shared.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
-//@Entity
+@Entity
 public class DeviceEntity extends BaseEntity {
+    @Id
+    private Long deviceId;
 
 }
--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceJpaRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceJpaRepository.java
@@ -1,9 +1,9 @@
 package com.whoz_in.domain_jpa.device;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 public interface DeviceJpaRepository extends JpaRepository<DeviceEntity, Long> {
 
 }
