07ddd4a1965ba06cb1c271a8441b5ebf1a90f6a8
coco3x <inferior3x@gmail.com>
2024-10-26 22:21:56 +0900

    feat(domain-jpa): Jpa Auditing 어노테이션

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/config/JpaConfig.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/config/JpaConfig.java
@@ -2,12 +2,13 @@ package com.whoz_in.domain_jpa.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-/*
-멀티 모듈에서 명시적으로 엔티티와 JpaRepository의 위치를 지정하기 위함
- */
+
 @Configuration
+@EnableJpaAuditing
+//멀티 모듈에서 명시적으로 엔티티와 JpaRepository의 위치를 지정하기 위함
 @EnableJpaRepositories(basePackages = "com.whoz_in.domain_jpa")
 @EntityScan(basePackages = "com.whoz_in.domain_jpa")
 public class JpaConfig {
