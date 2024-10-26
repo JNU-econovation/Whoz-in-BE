8cfed7c61e0c9a706f79b32119ad54cd913116fa
coco3x <inferior3x@gmail.com>
2024-10-26 22:23:00 +0900

    fix(domain-jpa): DeviceEntity - Auto Increment

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
@@ -2,11 +2,14 @@ package com.whoz_in.domain_jpa.device;
 
 import com.whoz_in.domain_jpa.shared.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class DeviceEntity extends BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long deviceId;
 
 }
