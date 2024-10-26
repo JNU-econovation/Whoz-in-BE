f2881d038af7cf50e0c708feafd7c1a1dcd2a6bf
coco3x <inferior3x@gmail.com>
2024-10-26 20:49:35 +0900

    fix(domain_jpa): 멀티 모듈에서 Entity와 Jpa Repository를 인식할 수 있도록 함

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceJpaAdapter.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceJpaAdapter.java
@@ -2,11 +2,13 @@ package com.whoz_in.domain_jpa.device;
 
 import com.whoz_in.domain.device.DeviceRepository;
 import com.whoz_in.domain.device.model.Device;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class DeviceJpaAdapter implements DeviceRepository {
-
+    private final DeviceJpaRepository repository;
     @Override
     public void save(Device device) {
 
