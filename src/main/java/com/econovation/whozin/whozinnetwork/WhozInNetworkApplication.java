b33da789bfcfc631bf8fbfade94ece3e541ba25f
bellmin <whdlas011203@naver.com>
2024-11-13 22:51:05 +0900

    chore(gradle): gradle 및 springbooy application 초기 설정
    - gradlew
    - gradlew.bat
    - WhozInNetworkApplication.java

---
+++ src/main/java/com/econovation/whozin/whozinnetwork/WhozInNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.econovation.whozin.whozinnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhozInNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhozInNetworkApplication.class, args);
+    }
+
+}
