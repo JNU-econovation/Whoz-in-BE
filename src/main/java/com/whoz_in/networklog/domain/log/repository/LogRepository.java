2914aa8633b8db5c7b04dd5f00fa68e5616866ff
bellmin <rlajm1203@naver.com>
2024-11-17 22:27:03 +0900

    feat(LogRepository): LogRepository 인터페이스 정의

--- src/main/java/com/whoz_in/networklog/domain/log/repository/LogRepository.java
+++ src/main/java/com/whoz_in/networklog/domain/log/repository/LogRepository.java
@@ -1,4 +1,10 @@
 package com.whoz_in.networklog.domain.log.repository;
 
+import com.whoz_in.networklog.infrastructure.jpa.log.NetworkLog;
+import java.util.Collection;
+
 public interface LogRepository {
+
+    void saveAll(Collection<NetworkLog> logs);
+
 }
