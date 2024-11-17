b93cb9ef21f6c7a3acfdad7374c76dda323bc3d2
bellmin <rlajm1203@naver.com>
2024-11-17 22:32:22 +0900

    feat(LogAdaptor): LogAdaptor 구현
    - saveAll : NetworkLog 컬렉션을 저장하는 메소드

--- src/main/java/com/whoz_in/networklog/application/log/adaptor/LogAdaptor.java
+++ src/main/java/com/whoz_in/networklog/application/log/adaptor/LogAdaptor.java
@@ -1,8 +1,20 @@
 package com.whoz_in.networklog.application.log.adaptor;
 
 import com.whoz_in.networklog.domain.log.repository.LogRepository;
+import com.whoz_in.networklog.infrastructure.jpa.log.LogJpaRepository;
+import com.whoz_in.networklog.infrastructure.jpa.log.NetworkLog;
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class LogAdaptor implements LogRepository {
+    
+    private final LogJpaRepository logJpaRepository;
+
+    @Override
+    public void saveAll(Collection<NetworkLog> logs) {
+        logJpaRepository.saveAll(logs);
+    }
 }
