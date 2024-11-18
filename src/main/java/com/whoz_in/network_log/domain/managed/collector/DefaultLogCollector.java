9900dd129cac24d9f2ff8f2da3f827288c2851fb
bellmin <rlajm1203@naver.com>
2024-11-18 17:56:55 +0900

    feat(DefaultLogCollector): Async 적용
    - 기존 스레드를 사용하여 비동기로 동작

--- src/main/java/com/whoz_in/network_log/domain/managed/collector/DefaultLogCollector.java
+++ src/main/java/com/whoz_in/network_log/domain/managed/collector/DefaultLogCollector.java
@@ -6,11 +6,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 // TODO: 이 클래스를 AbstractAddressProcess로 만든 이유는, 네트워클 정보를 읽어오는 수단이 꼭 tShark가 아닐 수도 있기 떄문에
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Component;
 public class DefaultLogCollector implements LogCollector {
 
     private final ProcessBuilder pb;
+    private Process process;
     private BufferedWriter bw;
     private BufferedReader br;
     private LogManager logManager;
@@ -30,15 +31,6 @@ public class DefaultLogCollector implements LogCollector {
     public DefaultLogCollector(ProcessConfig config) {
         this.pb = new ProcessBuilder(config.mDnsCommand())
                 .redirectErrorStream(true);
-
-        try{
-            Process process = pb.start();
-            bw = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
-            br = new BufferedReader(new InputStreamReader(process.getInputStream()), 100);
-        } catch (IOException e){
-            throw new RuntimeException(e);
-        }
-
     }
 
     public DefaultLogCollector(String command) {
@@ -46,24 +38,9 @@ public class DefaultLogCollector implements LogCollector {
     }
 
     @Override
+    @Async
     public void collect() {
-        try {
-            String line;
-            int cnt=0;
-
-            while ((line = br.readLine()) != null) {
-                logInfos.add(line);
-                cnt++;
-
-                if(cnt>=1000) {
-                    callBack(logInfos);
-                    cnt=0;
-                }
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        execute();
     }
 
     @Override
@@ -71,9 +48,34 @@ public class DefaultLogCollector implements LogCollector {
         this.logManager = manager;
     }
 
-    private void callBack(Set<String> logInfos) {
-        logManager.callBack(logInfos);
+    private void execute(){
+        // 프로세스 초기화
+        try {
+            process = pb.start();
+            br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
 
+        // 읽어오기
+        try {
+            String line;
+
+           while ((line = br.readLine()) != null) {
+               if(line.contains("Capturing on")) continue;
+               logInfos.add(line);
+               callBack(new HashSet<>(logInfos));
+           }
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void callBack(Set<String> logInfos) {
+        logManager.receive(logInfos);
     }
 
 }
