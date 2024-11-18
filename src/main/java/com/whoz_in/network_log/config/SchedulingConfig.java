1807ab8859bb22faa225c2642df99ffdba0b87a3
coco3x <inferior3x@gmail.com>
2024-11-18 16:24:52 +0900

    feat: @Scheduled 활성화

---
+++ src/main/java/com/whoz_in/network_log/config/SchedulingConfig.java
@@ -0,0 +1,11 @@
+package com.whoz_in.network_log.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+// @Scheduled 활성화를 위한 설정
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+}
