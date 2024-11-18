5ee17d5d3d0272f7ed96e595cb76d176a7e8655c
coco3x <inferior3x@gmail.com>
2024-11-18 16:22:25 +0900

    refactor: monitor log의 saveAll -> 벌크 인서트

--- src/main/java/com/whoz_in/network_log/domain/monitor/MonitorLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.whoz_in.network_log.domain.monitor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonitorLogRepository extends JpaRepository<MonitorLog, String> {
-
-}
