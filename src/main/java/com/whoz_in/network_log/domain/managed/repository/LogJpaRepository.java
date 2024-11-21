7633dcf00d654606fc8e9b66f3add8aa39048de5
bellmin <whdlas011203@naver.com>
2024-11-21 13:18:51 +0900

    feat: 공통 API 응답 형식 개발

--- src/main/java/com/whoz_in/network_log/domain/managed/repository/LogJpaRepository.java
+++ src/main/java/com/whoz_in/network_log/domain/managed/repository/LogJpaRepository.java
@@ -2,6 +2,12 @@ package com.whoz_in.network_log.domain.managed.repository;
 
 import com.whoz_in.network_log.domain.managed.ManagedLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LogJpaRepository extends JpaRepository<ManagedLog, String> {
+
+    @Query("SELECT log FROM ManagedLog log WHERE log.logId.ip=:ip")
+    ManagedLog findByIp(@Param("ip") String ip);
+
 }
