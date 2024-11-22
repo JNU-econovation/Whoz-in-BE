a10f56a30381152ebf9eac65828c5b3c4b10fa0b
coco3x <inferior3x@gmail.com>
2024-11-23 02:32:24 +0900

    REFACTOR: 변경된 log writer 구조를 반영

---
+++ src/main/java/com/whoz_in/network_log/infra/managed/mdns/MdnsLog.java
@@ -0,0 +1,29 @@
+package com.whoz_in.network_log.infra.managed.mdns;
+
+import jakarta.annotation.Nullable;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+//TODO: mac과 ip를 equals and hashcode
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class MdnsLog {
+    @EqualsAndHashCode.Include
+    private final String mac;
+    @EqualsAndHashCode.Include
+    private final String ip;
+
+    private final String device;
+    private final LocalDateTime createdAt;
+
+    //기기 이름이 없는 경우 null
+    public MdnsLog(String mac, String ip, @Nullable String device) {
+        this.mac = mac;
+        this.ip = ip;
+        this.device = device;
+        this.createdAt = LocalDateTime.now();
+    }
+}
