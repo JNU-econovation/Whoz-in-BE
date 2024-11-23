7875b5f41701cc62012c2f2b35aa07cfe75c8a7c
coco3x <inferior3x@gmail.com>
2024-11-23 20:37:35 +0900

    fix: Config 클래스의 @Configuration -> @Component

--- src/main/java/com/whoz_in/network_log/infra/monitor/MonitorConfig.java
+++ src/main/java/com/whoz_in/network_log/infra/monitor/MonitorConfig.java
@@ -3,9 +3,10 @@ package com.whoz_in.network_log.infra.monitor;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Getter
-@Configuration
+@Component
 public final class MonitorConfig {
     private final String command;
     private final String sudoPassword;
