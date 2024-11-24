8661bf251eeedce2097f1183469e5eaf41a71b39
bellmin <whdlas011203@naver.com>
2024-11-24 15:00:15 +0900

    feat(ArpLogProcess): Arp 로그를 수집하는 Process 작성

--- src/main/java/com/whoz_in/network_log/infra/managed/arp/ArpLogProcess.java
+++ src/main/java/com/whoz_in/network_log/infra/managed/arp/ArpLogProcess.java
@@ -3,25 +3,34 @@ package com.whoz_in.network_log.infra.managed.arp;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 
+//TODO: 얘는 주기적으로 실행해줘야 하는 친구 이므 ArpLogProcess 객체는 하나로 유지하되, 클래스 내부에서 apr-scan 프로세스를 여러번 실행 시키는 방식으로
 public class ArpLogProcess {
 
     private Process process;
-    private ProcessBuilder pb;
     private BufferedReader br;
     private BufferedWriter bw;
 
-    public ArpLogProcess(String command){
+    public ArpLogProcess(String command, String passwrod){
         try {
             process = new ProcessBuilder(command.split(" ")).start();
+            this.bw = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            this.br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            this.bw.write(passwrod);
+            this.bw.newLine();
+            this.bw.flush();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public Set<String> start(String command){
+    public Set<String> start(){
         Set<String> logs = new HashSet<>();
 
         try{
