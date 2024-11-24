7f80f3c1759c1f2975767be0084cfb5e11d04b9c
bellmin <rlajm1203@naver.com>
2024-11-25 01:33:34 +0900

    feat(log-api): Spring Component Scan 범위 설정

--- modules/log-api/src/main/java/com/whoz_in/network_log/WhozInNetworkLogApplication.java
+++ modules/log-api/src/main/java/com/whoz_in/network_log/WhozInNetworkLogApplication.java
@@ -3,7 +3,7 @@ package com.whoz_in.network_log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.whoz_in.domain_jpa"})
 public class WhozInNetworkLogApplication {
 
     public static void main(String[] args) {
