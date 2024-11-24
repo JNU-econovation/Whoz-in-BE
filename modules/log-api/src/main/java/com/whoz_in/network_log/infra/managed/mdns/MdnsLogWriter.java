b8a522c11a66c39b42bffda947f46b07ab71af9a
bellmin <whdlas011203@naver.com>
2024-11-25 01:42:20 +0900

    feat(log-api): ManagedDAO, MonitorDAO 추가
    - domain-jpa 모듈 의존
    - ManagedLog, MonitorLog 는 domain-jpa 에 속함

--- modules/log-api/src/main/java/com/whoz_in/network_log/infra/managed/mdns/MdnsLogWriter.java
+++ modules/log-api/src/main/java/com/whoz_in/network_log/infra/managed/mdns/MdnsLogWriter.java
@@ -1,7 +1,7 @@
 package com.whoz_in.network_log.infra.managed.mdns;
 
-import com.whoz_in.network_log.persistence.ManagedLog;
-import com.whoz_in.network_log.persistence.ManagedLogDAO;
+import com.whoz_in.domain_jpa.log.managed.ManagedLog;
+import com.whoz_in.network_log.infra.managed.ManagedLogDAO;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
---
+++ modules/log-api/src/main/java/com/whoz_in/network_log/infra/monitor/MonitorLogDAO.java
@@ -0,0 +1,25 @@
+package com.whoz_in.network_log.infra.monitor;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.Set;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class MonitorLogDAO {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Modifying
+    @Transactional
+    public void upsertAll(Set<String> macs) {
+        if (macs.isEmpty()) return;
+        StringBuilder sql = new StringBuilder("INSERT INTO monitor_log (mac, created_date, updated_date) VALUES ");
+        macs.forEach(mac -> sql.append(String.format("('%s', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),", mac)));
+        sql.setLength(sql.length() - 1);
+        sql.append(" ON DUPLICATE KEY UPDATE updated_date = CURRENT_TIMESTAMP");
+        em.createNativeQuery(sql.toString()).executeUpdate();
+    }
+}
