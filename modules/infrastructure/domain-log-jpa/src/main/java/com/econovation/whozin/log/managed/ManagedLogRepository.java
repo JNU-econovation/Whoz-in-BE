1d43d85bd6ccd2aaf25b4917b64544708696a490
bellmin <whdlas011203@naver.com>
2024-11-25 22:48:06 +0900

    feat(domain-log-jpa): log 엔티티 이동 및 의존성 추가
    - spring-boot-starte-data-jpa 의존성 추가
    - mysql-connector 추가

---
+++ modules/infrastructure/domain-log-jpa/src/main/java/com/econovation/whozin/log/managed/ManagedLogRepository.java
@@ -0,0 +1,18 @@
+package com.econovation.whozin.log.managed;
+
+import com.whoz_in.domain_jpa.log.managed.ManagedLog;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ManagedLogRepository extends JpaRepository<ManagedLog, String> {
+
+    @Query("SELECT log FROM ManagedLog log WHERE log.logId.ip=:ip ORDER BY log.createdAt DESC LIMIT 1")
+    Optional<ManagedLog> findByIp(@Param("ip") String ip);
+
+    @Query("SELECT log FROM ManagedLog log WHERE log.logId.ip=:ip")
+    List<ManagedLog> findAllByIp(@Param("ip") String ip);
+
+}
