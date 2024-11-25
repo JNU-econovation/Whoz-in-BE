83c0e08516201db7839b1b79a72bf61e4800e5ab
bellmin <whdlas011203@naver.com>
2024-11-25 12:48:24 +0900

    chore(log-writer): log-writer 모듈 세팅

---
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/infra/managed/ManagedLogConverter.java
@@ -0,0 +1,26 @@
+package com.whoz_in.log_writer.infra.managed;
+
+import com.whoz_in.domain_jpa.log.managed.ManagedLog;
+import com.whoz_in.domain_jpa.log.managed.ManagedLog.LogId;
+import com.whoz_in.log_writer.infra.managed.arp.ArpLog;
+import com.whoz_in.log_writer.infra.managed.mdns.MdnsLog;
+
+public class ManagedLogConverter {
+
+    public static ManagedLog toEntity(MdnsLog log){
+        return ManagedLog.builder()
+                .logId(new LogId(log.getMac(), log.getIp()))
+                .deviceName(log.getDevice())
+                .createdAt(log.getCreatedAt())
+                .build();
+    }
+
+    public static ManagedLog toEntity(ArpLog log){
+        return ManagedLog.builder()
+                .logId(new LogId(log.getMac(), log.getIp()))
+                .deviceName(log.getDevice())
+                .createdAt(log.getCreatedAt())
+                .build();
+    }
+
+}
