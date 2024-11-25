6a99dd5ca88ecf218479f4300830740afd380422
bellmin <rlajm1203@naver.com>
2024-11-25 23:53:01 +0900

    feat(domain-jpa): domain-common-jpa 적용
    - BaseEntity, SoftDeleteEventListener 이동

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
@@ -1,6 +1,5 @@
 package com.whoz_in.domain_jpa.device;
 
-import com.whoz_in.domain_jpa.shared.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
