e296c9458f064b48eeadc1bd324ac7953a554e37
bellmin <whdlas011203@naver.com>
2024-11-25 11:22:42 +0900

    feat(log-api): ManagedLogConverter 정의
    - 외부 모듈에 있는 Entity 클래스로 바꿔주기 위함

---
+++ modules/log-api/src/main/java/com/whoz_in/network_log/infra/managed/ManagedLogConverter.java
@@ -0,0 +1,26 @@
+package com.whoz_in.network_log.infra.managed;
+
+import com.whoz_in.domain_jpa.log.managed.ManagedLog;
+import com.whoz_in.domain_jpa.log.managed.ManagedLog.LogId;
+import com.whoz_in.network_log.infra.managed.arp.ArpLog;
+import com.whoz_in.network_log.infra.managed.mdns.MdnsLog;
+
+public class ManagedLogConverter {
+
+    public static ManagedLog from(MdnsLog log){
+        return ManagedLog.builder()
+                .logId(new LogId(log.getMac(), log.getIp()))
+                .deviceName(log.getDevice())
+                .createdAt(log.getCreatedAt())
+                .build();
+    }
+
+    public static ManagedLog from(ArpLog log){
+        return ManagedLog.builder()
+                .logId(new LogId(log.getMac(), log.getIp()))
+                .deviceName(log.getDevice())
+                .createdAt(log.getCreatedAt())
+                .build();
+    }
+
+}
