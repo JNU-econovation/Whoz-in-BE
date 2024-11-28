2e7e345b1f70d2c98684f3de0cf3eb5e670e3e27
coco3x <inferior3x@gmail.com>
2024-11-28 15:09:30 +0900

    feat: 커맨드를 json으로 관리 + ssid까지 관리 가능하도록 함

---
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/config/NetworkConfig.java
@@ -0,0 +1,67 @@
+package com.whoz_in.log_writer.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class NetworkConfig {
+    // Getters and setters
+    private final Map<String, String> interfaceInfo; //k: name, v: ssid
+    private final Monitor monitor;
+    private final List<Managed> mdnsList;
+    private final List<Managed> arpList;
+
+    public NetworkConfig(ResourceLoader loader, ObjectMapper mapper) {
+        Resource resource = loader.getResource("classpath:/network.json");
+        try {
+            //JSON 파일 읽기
+            Map<String, Object> map = mapper.readValue(resource.getFile(), Map.class);
+
+            //interface_info
+            List<Map<String, String>> interfaceInfoList = (List<Map<String, String>>) map.get(
+                    "interface_info");
+            this.interfaceInfo = interfaceInfoList.stream()
+                    .collect(Collectors.toMap(info-> info.get("name"), info->info.get("ssid")));
+
+            //monitor
+            Map<String, String> monitorMap = (Map<String, String>) map.get("monitor");
+            this.monitor = new Monitor(
+                    monitorMap.get("interface"),
+                    generateCommand(monitorMap.get("command"), monitorMap.get("interface"))
+            );
+
+            // managed
+            Map<String, Object> managedMap = (Map<String, Object>) map.get("managed");
+            // mdns
+            Map<String, Object> mdnsMap = (Map<String, Object>) managedMap.get("mdns");
+            String mdnsCommand = (String) mdnsMap.get("command");
+            this.mdnsList = ((List<String>) mdnsMap.get("interfaces")).stream()
+                    .map(interfaceName->new Managed(interfaceName, interfaceInfo.get(interfaceName), generateCommand(mdnsCommand, interfaceName)))
+                    .toList();
+            // arp
+            Map<String, Object> arpMap = (Map<String, Object>) managedMap.get("arp");
+            String arpCommand = (String) arpMap.get("command");
+            this.arpList = ((List<String>) arpMap.get("interfaces")).stream()
+                    .map(interfaceName->new Managed(interfaceName, interfaceInfo.get(interfaceName), generateCommand(arpCommand, interfaceName)))
+                    .toList();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load configuration from JSON file", e);
+        }
+    }
+
+    private String generateCommand(String commandTemplate, String interfaceName) {
+        return commandTemplate.replace("{{interface}}", interfaceName);
+    }
+
+    public record Monitor(String interfaceName, String command) {}
+    public record Managed(String interfaceName, String ssid, String command) {}
+}
