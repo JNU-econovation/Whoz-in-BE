c91a8620ecc3b7357ac773a841ad0979539b4335
coco3x <inferior3x@gmail.com>
2024-11-28 15:14:26 +0900

    refactor(log-w): arp, mdns - NetworkConfig를 이용하여 프로세스를 실행하도록 변경하고 로그에 SSID 기록함. arp - 스프링 빈 제거

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/arp/ArpLogWriter.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/arp/ArpLogWriter.java
@@ -1,48 +1,51 @@
 package com.whoz_in.log_writer.managed.arp;
 
 
+import com.whoz_in.log_writer.config.NetworkConfig;
+import com.whoz_in.log_writer.config.NetworkConfig.Managed;
 import com.whoz_in.log_writer.managed.ManagedLog;
 import com.whoz_in.log_writer.managed.ManagedLogDAO;
-import java.util.Collections;
+import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArpLogWriter {
+    private final ManagedLogDAO dao;
+    private final ArpLogParser parser;
+    private final List<Managed> arpList;
+    private final String sudoPassword;
 
-    private final ManagedLogDAO managedLogDAO;
-    private final ArpLogParser arpLogParser;
-    private final ArpLogProcess arpLogProcess;
-
-    private final Set<String> logs = Collections.newSetFromMap(new ConcurrentHashMap<>()); //TODO: 일반 HashSet으로 변경
-
-    public ArpLogWriter(ManagedLogDAO managedLogDAO,
-                        ArpLogParser arpLogParser,
-                        ArpLogProcess arpLogProcess) {
-        this.managedLogDAO = managedLogDAO;
-        this.arpLogParser = arpLogParser;
-        this.arpLogProcess = arpLogProcess;
+    public ArpLogWriter(ManagedLogDAO dao,
+                        ArpLogParser parser,
+                        NetworkConfig arpList,
+                        @Value("${sudo_password}") String sudoPassword
+            ) {
+        this.dao = dao;
+        this.parser = parser;
+        this.arpList = arpList.getArpList();
+        this.sudoPassword = sudoPassword;
     }
 
     @Scheduled(fixedRate = 5000)
     private void scan() {
-        logs.addAll(arpLogProcess.run());
-
-        save();
+        Set<ManagedLog> logs= arpList.stream()
+                .flatMap(arp-> {
+                    ArpLogProcess proc = new ArpLogProcess(arp.command(), sudoPassword); //프로세스 실행
+                    List<String> lines = proc.readLines(); //프로세스의 모든 출력 가져오기
+                    return lines.stream() //출력 라인들을 ManagedLog 변환하며 ssid도 넣어줌
+                            .filter(parser::validate)
+                            .map(line->{
+                                ManagedLog log = parser.parse(line);
+                                log.setSsid(arp.ssid());
+                                return log;
+                            });
+                })
+                .collect(Collectors.toSet()); //Set으로 중복 제거
+        System.out.println("[managed - arp] 저장할 로그 개수 : " +logs.size());
+        dao.insertAll(logs);
     }
-
-    private void save(){
-        Set<ManagedLog> managedLogs = logs.stream()
-                .filter(arpLogParser::validate)
-                .map(arpLogParser::parse)
-                .collect(Collectors.toSet());
-
-        System.out.println(String.format("[managed - arp] 저장할 로그 개수 : %d", managedLogs.size()));
-
-        managedLogDAO.insertAll(managedLogs);
-    }
-
 }
