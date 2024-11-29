b8434298afafcd0eecb891a36afdacec710dbee0
coco3x <inferior3x@gmail.com>
2024-11-30 00:50:30 +0900

    update(log-w): monitor - error stream 임시 처리 기능, 닫혀있을경우 로깅, 프로세스 정보 VO를 통해 초기화하도록 변경

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/monitor/MonitorLogProcess.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/monitor/MonitorLogProcess.java
@@ -2,6 +2,7 @@ package com.whoz_in.log_writer.monitor;
 
 import com.whoz_in.log_writer.common.util.NonBlockingBufferedReader;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -11,22 +12,27 @@ public final class MonitorLogProcess {
     private final Process process;
     private final NonBlockingBufferedReader cbr;
 
-    public MonitorLogProcess(String command, String sudoPassword) {
-        ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+    public MonitorLogProcess(MonitorInfo info, String sudoPassword) {
         try {
-            process = pb.start();
+            process = new ProcessBuilder(info.command().split(" "))
+                    .redirectError(new File("../error", "monitor.txt"))
+                    .start();
             this.cbr = new NonBlockingBufferedReader(new BufferedReader(new InputStreamReader(process.getInputStream())));
 
             Writer writer = new OutputStreamWriter(process.getOutputStream());
             writer.write(sudoPassword + System.lineSeparator());
             writer.flush();
         } catch (IOException e) {
-            throw new RuntimeException(command + " 실행 실패");
+            throw new RuntimeException(info.command() + " 실행 실패");
         }
     }
 
-    public String readLine() throws IOException {
-        return cbr.readLine();
+    public String readLine(){
+        try {
+            return cbr.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean isAlive(){
