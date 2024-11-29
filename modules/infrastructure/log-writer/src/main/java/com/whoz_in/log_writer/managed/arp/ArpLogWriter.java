d2415e9f1662f46c9c855e61f564260d99df1cba
coco3x <inferior3x@gmail.com>
2024-11-30 00:49:38 +0900

    update(log-w): arp - error stream 임시 처리 기능, 실행 실패 시 로깅, 프로세스 정보 VO를 통해 초기화하도록 변경

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/arp/ArpLogWriter.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/arp/ArpLogWriter.java
@@ -2,12 +2,13 @@ package com.whoz_in.log_writer.managed.arp;
 
 
 import com.whoz_in.log_writer.config.NetworkConfig;
-import com.whoz_in.log_writer.config.NetworkConfig.Managed;
+import com.whoz_in.log_writer.managed.ManagedInfo;
 import com.whoz_in.log_writer.managed.ManagedLog;
 import com.whoz_in.log_writer.managed.ManagedLogDAO;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Component;
 public class ArpLogWriter {
     private final ManagedLogDAO dao;
     private final ArpLogParser parser;
-    private final List<Managed> arpList;
+    private final List<ManagedInfo> arpList;
     private final String sudoPassword;
 
     public ArpLogWriter(ManagedLogDAO dao,
@@ -30,23 +31,35 @@ public class ArpLogWriter {
         this.sudoPassword = sudoPassword;
     }
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(initialDelay = 10000, fixedRate = 5000)
     private void scan() {
-        Set<ManagedLog> logs= arpList.stream()
-                .flatMap(arp-> {
-                    ArpLogProcess proc = new ArpLogProcess(arp.command(), sudoPassword, arp.ssid()); //프로세스 실행
+        List<ManagedLog> logs= arpList.stream()
+                .flatMap(arpInfo-> {
+                    ArpLogProcess proc = new ArpLogProcess(arpInfo, sudoPassword); //프로세스 실행
                     List<String> lines = proc.readLines(); //프로세스의 모든 출력 가져오기
-                    proc.printError();
-                    return lines.stream() //출력 라인들을 ManagedLog 변환하며 ssid도 넣어줌
+                    Set<ManagedLog> procLogs = lines.stream() //출력 라인들을 ManagedLog 변환하며 ssid도 넣어줌
                             .filter(parser::validate)
                             .map(line->{
                                 ManagedLog log = parser.parse(line);
-                                log.setSsid(arp.ssid());
+                                log.setSsid(arpInfo.ssid());
                                 return log;
-                            });
+                            })
+                            .collect(Collectors.toSet()); //Set으로 중복 제거
+
+                    /*
+                    Arp-scan은 단발성인데
+                    Process의 isAlive()는 실행 중일 때도 false일 수 있고, 종료 중일 때도 true일 수 있으므로 오류의 판단이 힘들다.
+                    따라서 Arp-scan의 경우 무조건 1개 이상의 결과가 나오므로 0개라면 실행 실패라고 판단한다.
+                     */
+                    if (procLogs.isEmpty()) {
+                        System.err.println("[managed - arp(%s)] 실행 실패 : ERROR".formatted(arpInfo.ssid()));
+                        return Stream.empty();
+                    }
+                    System.out.println("[managed - arp(%s)] 저장할 로그 개수 : %d".formatted(arpInfo.ssid(), procLogs.size()));
+                    return procLogs.stream();
                 })
-                .collect(Collectors.toSet()); //Set으로 중복 제거
-        System.out.println("[managed - arp] 저장할 로그 개수 : " +logs.size());
+                .toList();
+
         dao.insertAll(logs);
     }
 }
