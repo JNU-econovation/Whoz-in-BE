c36aef4d31c70123c85e1bde69777b5d9d672a93
coco3x <inferior3x@gmail.com>
2024-11-30 00:49:38 +0900

    update(log-w): arp - error stream 임시 처리 기능, 실행 실패 시 로깅, 프로세스 정보 VO를 통해 초기화하도록 변경

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/arp/ArpLogProcess.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/arp/ArpLogProcess.java
@@ -1,35 +1,33 @@
 package com.whoz_in.log_writer.managed.arp;
 
+import com.whoz_in.log_writer.managed.ManagedInfo;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.scheduling.annotation.Async;
 
 //단발성 프로세스
 public class ArpLogProcess {
     private final BufferedReader br;
-    private final BufferedReader ebr;
     private final Process process;
-    private final String ssid;
-    public ArpLogProcess(String command, String password, String ssid) {
-        this.ssid = ssid;
+    public ArpLogProcess(ManagedInfo info, String password) {
         try {
-            this.process = new ProcessBuilder(command.split(" ")).start();
+            //TODO: error 처리 수정
+            this.process = new ProcessBuilder(info.command().split(" "))
+                    .redirectError(new File("../error", info.ssid()+".txt"))
+                    .start();
             this.br = new BufferedReader(new InputStreamReader(process.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
             bw.write(password);
             bw.newLine();
             bw.flush();
-            //참고: 종료된 프로세스에서도 Input, Error 스트림은 사용 가능. Output에 접근 시 broken pipe 에러.
-            this.ebr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     //단발성 프로세스로, 프로세스가 종료될 때까지 run은 블로킹된다.
@@ -48,17 +46,7 @@ public class ArpLogProcess {
         return logs;
     }
 
-    @Async
-    public void printError(){
-        try {
-            String errorLine = ebr.readLine();
-            System.err.println("[managed - arp] %s".formatted(this.ssid));
-            System.err.println("\t"+errorLine);
-            while((errorLine = ebr.readLine()) != null){
-                System.err.println("\t"+errorLine);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public boolean isAlive(){
+        return process.isAlive();
     }
 }
