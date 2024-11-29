a54d0a734239181197cd2ba78c8a82a6b6cc3020
coco3x <inferior3x@gmail.com>
2024-11-30 00:46:57 +0900

    update(log-w): NetworkConfig가 log writer들이 요구하는 프로세스에 대한 정보를 제공하도록 변경함, 정보를 검증하도록 함

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/config/NetworkConfig.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/config/NetworkConfig.java
@@ -1,6 +1,8 @@
 package com.whoz_in.log_writer.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.whoz_in.log_writer.managed.ManagedInfo;
+import com.whoz_in.log_writer.monitor.MonitorInfo;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -11,54 +13,63 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+//실행시킬 프로세스들이 필요로 하는 정보를 제공하는 역할을 한다.
+//이를 위해 network-<profile>.json에서 설정값을 가져오며, 올바른 값인지 검증한다.
 @Getter
 @Component
 public class NetworkConfig {
-    // Getters and setters
     private final Map<String, String> ssidInfo; //k: name, v: ssid
-    private final Monitor monitor;
-    private final List<Managed> mdnsList;
-    private final List<Managed> arpList;
+    private final MonitorInfo monitorInfo;
+    private final List<ManagedInfo> mdnsList;
+    private final List<ManagedInfo> arpList;
 
     @SuppressWarnings("unchecked")
     public NetworkConfig(@Value("${spring.profiles.active:default}") String profile, ResourceLoader loader, ObjectMapper mapper) {
-        Resource resource = loader.getResource("classpath:/network-%s.json".formatted(profile));
+        String jsonPath = "classpath:/network-%s.json".formatted(profile);
+        Resource resource = loader.getResource(jsonPath);
+        Map<String, Object> map;
 
         try {
-            //JSON 파일 읽기
-            Map<String, Object> map = mapper.readValue(resource.getFile(), Map.class);
+            map = mapper.readValue(resource.getFile(), Map.class); //JSON 파일 읽기
+        } catch (IOException e) {
+            throw new RuntimeException(jsonPath + " 로드 실패");
+        }
 
+        try {
             //interface_info
-            List<Map<String, String>> interfaceInfoList = (List<Map<String, String>>) map.get("ssid_info");
+            List<Map<String, String>> interfaceInfoList = (List<Map<String, String>>) map.get(
+                    "ssid_info");
             this.ssidInfo = interfaceInfoList.stream()
-                    .collect(Collectors.toMap(info-> info.get("name"), info->info.get("ssid")));
-
+                    .collect(Collectors.toMap(info -> info.get("name"), info -> info.get("ssid")));
             //monitor
             Map<String, String> monitorMap = (Map<String, String>) map.get("monitor");
-            this.monitor = new Monitor(
+            this.monitorInfo = new MonitorInfo(
                     monitorMap.get("interface"),
                     generateCommand(monitorMap.get("command"), monitorMap.get("interface"))
             );
-
             // managed
             Map<String, Object> managedMap = (Map<String, Object>) map.get("managed");
-
             // mdns
             Map<String, Object> mdnsMap = (Map<String, Object>) managedMap.get("mdns");
             String mdnsCommand = (String) mdnsMap.get("command");
             this.mdnsList = ((List<String>) mdnsMap.get("interfaces")).stream()
-                    .map(interfaceName->new Managed(interfaceName, ssidInfo.get(interfaceName), generateCommand(mdnsCommand, interfaceName)))
+                    .map(interfaceName -> new ManagedInfo(interfaceName,
+                            ssidInfo.get(interfaceName),
+                            generateCommand(mdnsCommand, interfaceName)))
                     .toList();
-
             // arp
             Map<String, Object> arpMap = (Map<String, Object>) managedMap.get("arp");
             String arpCommand = (String) arpMap.get("command");
             this.arpList = ((List<String>) arpMap.get("interfaces")).stream()
-                    .map(interfaceName->new Managed(interfaceName, ssidInfo.get(interfaceName), generateCommand(arpCommand, interfaceName)))
+                    .map(interfaceName -> new ManagedInfo(interfaceName,
+                            ssidInfo.get(interfaceName),
+                            generateCommand(arpCommand, interfaceName)))
                     .toList();
 
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load configuration from JSON file", e);
+            validate();
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("network json 구조가 잘못되었음");
         }
     }
 
@@ -66,6 +77,17 @@ public class NetworkConfig {
         return commandTemplate.replace("{{interface}}", interfaceName);
     }
 
-    public record Monitor(String interfaceName, String command) {}
-    public record Managed(String interfaceName, String ssid, String command) {}
+    private void validate(){
+        this.mdnsList.forEach(mdns-> {
+                    if (!ssidInfo.containsKey(mdns.interfaceName()))
+                        throw new IllegalArgumentException("mdns에 정의되지 않은 인터페이스가 있음");
+                });
+        this.arpList.forEach(arp-> {
+                    if (!ssidInfo.containsKey(arp.interfaceName()))
+                        throw new IllegalArgumentException("arp에 정의되지 않은 인터페이스가 있음");
+                });
+        //TODO: 더 많은 검증
+    }
+
+
 }
