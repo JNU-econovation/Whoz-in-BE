98b80798a960cc1f74529a8522783ecf62836f08
coco3x <inferior3x@gmail.com>
2024-11-30 23:21:22 +0900

    feat: 시스템 설정 검증 기능

---
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/common/NetworkInterface.java
@@ -0,0 +1,22 @@
+package com.whoz_in.log_writer.common;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public final class NetworkInterface{
+    @EqualsAndHashCode.Include
+    private final String name;
+    @EqualsAndHashCode.Include
+    private final String essid;
+    @EqualsAndHashCode.Include
+    private final String mode;
+
+    @Override
+    public String toString() {
+        return "name: " + name +", ssid: "+ essid + ", mode: "+ mode;
+    }
+}
