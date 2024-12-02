5dfa6834bd9999c88f43c54797ad8ddb6513bc07
coco3x <inferior3x@gmail.com>
2024-12-03 01:37:26 +0900

    update(log-w): sout -> log

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/monitor/ChannelHopper.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/monitor/ChannelHopper.java
@@ -5,11 +5,13 @@ import com.whoz_in.log_writer.common.process.TransientProcess;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 
 //모니터 모드인 네트워크 인터페이스의 채널을 주기적으로 바꿔주는 역할
 //변경될 채널은 주변 모든 와이파이들을 대상으로 함.
+@Slf4j
 public class ChannelHopper {
     private final NetworkInterface monitor;
     private final String sudoPassword;
@@ -29,7 +31,7 @@ public class ChannelHopper {
                     .filter(split -> (split.length == 2) && split[1].matches("\\d+"))
                     .map(split -> Integer.parseInt(split[1]))
                     .collect(Collectors.toSet());
-            System.out.println("[monitor] channels to hop : "+channels);
+            log.info("channels to hop : "+channels);
             channelsToHop.addAll(channels);
         }
         Integer channel = channelsToHop.iterator().next();
