6b22b9f346dcebf8d62907f0e52e0c83179fe91e
coco3x <inferior3x@gmail.com>
2024-12-05 20:13:13 +0900

    refactor: system validator에 validator 패키지 적용

---
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/system_validator/SystemValidator.java
@@ -0,0 +1,75 @@
+package com.whoz_in.log_writer.system_validator;
+
+
+import com.whoz_in.log_writer.common.NetworkInterface;
+import com.whoz_in.log_writer.common.validation.ValidationException;
+import com.whoz_in.log_writer.common.validation.ValidationResult;
+import com.whoz_in.log_writer.common.validation.Validator;
+import com.whoz_in.log_writer.config.NetworkConfig;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+//시스템의 전체적인 검증을 진행하는 클래스
+//SystemValidatorConfig에 의해 빈으로 등록됩니다
+@Slf4j
+public final class SystemValidator {
+    private final NetworkConfig config;
+    private final SystemNetworkInterfaces systemNetworkInterfaces;
+    private final NetworkInterfaceValidator networkInterfaceValidator;
+
+    //서버 시작 시 검증 (실패 시 예외가 발생하여 서버 시작이 실패하게 됨)
+    public SystemValidator(
+            NetworkConfig config,
+            SystemNetworkInterfaces systemNetworkInterfaces,
+            CommandInstalledValidator commandInstalledValidator,
+            NetworkInterfaceValidator networkInterfaceValidator) {
+        this.config = config;
+        this.systemNetworkInterfaces = systemNetworkInterfaces;
+        this.networkInterfaceValidator = networkInterfaceValidator;
+
+        log.info("시스템 검증을 수행합니다");
+
+        //커맨드 설치 여부 검증
+        List<String> commands = List.of("tshark", "arp-scan", "iwconfig", "nmcli");
+        commands.forEach(commandInstalledValidator::validate);
+
+        //네트워크 인터페이스 정보
+        List<NetworkInterface> system = systemNetworkInterfaces.getLatest();
+        List<NetworkInterface> setting = config.getNetworkInterfaces();
+
+        //네트워크 인터페이스 출력
+        log.info("\n시스템 네트워크 인터페이스 - \n{}\n설정된 네트워크 인터페이스 - \n{}",
+                system.stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining("\n")),
+                setting.stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining("\n")));
+
+        //네트워크 인터페이스 상태 검증
+        networkInterfaceValidator.validate(system, setting);
+
+        log.info("시스템 검증 완료");
+    }
+
+    //정기적으로 시스템 상태를 검사함
+    //예외를 띄우지 않고 로깅만 하기
+    @Scheduled(fixedDelay = 30000)
+    private void checkRegularly() {
+        log.info("시스템 검증 시작..");
+        //네트워크 인터페이스 상태 검증
+        ValidationResult result = networkInterfaceValidator.getValidationResult(
+                systemNetworkInterfaces.getLatest(), config.getNetworkInterfaces());
+        //더 많은 검증하면 result에 추가하기..
+
+        if (result.hasErrors()) {
+            log.error("시스템 검증 실패 : [{}]", result);
+            return;
+        }
+        log.info("시스템 검증 완료");
+    }
+
+}
--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/system_validator/SystemValidatorConfig.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/system_validator/SystemValidatorConfig.java
@@ -1,5 +1,7 @@
-package com.whoz_in.log_writer;
+package com.whoz_in.log_writer.system_validator;
 
+import com.whoz_in.log_writer.common.validation.BiValidator;
+import com.whoz_in.log_writer.common.validation.ValidationResult;
 import com.whoz_in.log_writer.config.NetworkConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,10 +13,20 @@ import org.springframework.context.annotation.Configuration;
 public class SystemValidatorConfig {
     private final String profile;
     private final NetworkConfig config;
+    private final SystemNetworkInterfaces systemNetworkInterfaces;
+    private final CommandInstalledValidator commandInstalledValidator;
+    private final NetworkInterfaceValidator networkInterfaceValidator;
+
     public SystemValidatorConfig(@Value("${spring.profiles.active}") String profile,
-            NetworkConfig config) {
+            NetworkConfig config,
+            SystemNetworkInterfaces systemNetworkInterfaces,
+            CommandInstalledValidator commandInstalledValidator,
+            NetworkInterfaceValidator networkInterfaceValidator) {
         this.profile = profile;
         this.config = config;
+        this.systemNetworkInterfaces = systemNetworkInterfaces;
+        this.commandInstalledValidator = commandInstalledValidator;
+        this.networkInterfaceValidator = networkInterfaceValidator;
     }
 
     @Bean
@@ -27,6 +39,6 @@ public class SystemValidatorConfig {
             log.info("리눅스가 아니거나 스프링 프로필이 prod가 아니므로 시스템 검증을 수행하지 않습니다.");
             return null;
         }
-        return new SystemValidator(config);
+        return new SystemValidator(config, systemNetworkInterfaces, commandInstalledValidator, networkInterfaceValidator);
     }
 }
