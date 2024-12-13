3857d9ad2ef3eedfd8dcdcb7e2943f52b4f87579
coco3x <inferior3x@gmail.com>
2024-12-13 20:06:57 +0900

    update: 프로세스 정보에 네트워크 인터페이스 추가

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/ManagedInfo.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/ManagedInfo.java
@@ -1,4 +1,6 @@
 package com.whoz_in.log_writer.managed;
 
+import com.whoz_in.log_writer.common.NetworkInterface;
+
 //Managed 프로세스를 생성하고 처리하는 과정에서 필요한 정보를 담는다.
-public record ManagedInfo(String interfaceName, String ssid, String command) {}
+public record ManagedInfo(NetworkInterface ni, String command) {}
