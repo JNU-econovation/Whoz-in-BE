f79dadb5a0f454292ab947209a3e482cd7b387fd
coco3x <inferior3x@gmail.com>
2024-12-13 21:04:22 +0900

    update: 모니터 네트워크 인터페이스가 시스템에 있는지 확인하고 실행하도록 함

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/monitor/MonitorLogWriter.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/monitor/MonitorLogWriter.java
@@ -1,8 +1,7 @@
 package com.whoz_in.log_writer.monitor;
 
+import com.whoz_in.log_writer.common.SystemNetworkInterfaces;
 import com.whoz_in.log_writer.config.NetworkConfig;
-import com.whoz_in.log_writer.managed.ManagedInfo;
-import com.whoz_in.log_writer.managed.mdns.MdnsLogProcess;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -15,30 +14,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class MonitorLogWriter {
     private MonitorLogProcess process; //교체될 수 있으므로 final X
-    private Boolean dead = false;
+    private boolean isProcDead;
     private final MonitorLogParser parser;
     private final MonitorLogDAO repo;
     private final String sudoPassword;
     private final MonitorInfo monitorInfo;
+    private final SystemNetworkInterfaces systemNIs;
 
-    public MonitorLogWriter(MonitorLogParser parser, MonitorLogDAO repo, NetworkConfig config, @Value("${sudo_password}") String sudoPassword) {
+    public MonitorLogWriter(MonitorLogParser parser, MonitorLogDAO repo, NetworkConfig config,
+            @Value("${sudo_password}") String sudoPassword,
+            SystemNetworkInterfaces systemNIs) {
         this.parser = parser;
         this.repo = repo;
         this.monitorInfo = config.getMonitorInfo();
         this.sudoPassword = sudoPassword;
+        this.systemNIs = systemNIs;
         startProcess();
     }
+
     @Scheduled(initialDelay = 10000, fixedDelay = 10000)
     private void saveLogs(){
-        if (!process.isAlive()) {
-            if (dead.equals(Boolean.FALSE)) {
-                dead = true;
-                log.error("[monitor] 프로세스가 종료됨 :\n{}\n{}", "프로세스의 에러 스트림 내용:", process.readErrorLines());
-                log.error("[monitor] 프로세스를 재실행합니다.");
-                startProcess();
-            }
-            return;
-        }
+        //프로세스 죽었으면 기록 안함
+        if (!process.isAlive()) return;
         Set<String> macs = new HashSet<>();
         String line;
         for(;;) {
@@ -52,12 +49,31 @@ public class MonitorLogWriter {
         repo.upsertAll(macs);
     }
 
+    @Scheduled(initialDelay = 10000, fixedDelay = 10000)
+    private void checkProcess(){
+        if (process.isAlive()) return;
+
+        if (!isProcDead) {
+            isProcDead = true;
+            log.error("[monitor] 프로세스가 종료됨 :\n{}\n{}", "프로세스의 에러 스트림 내용:", process.readErrorLines());
+        }
+        startProcess();
+    }
+
+    //이전 프로세스를 제거하고 새로운 프로세스를 실행시켜 교체하는 역할
     private void startProcess(){
+        log.info("[monitor] 프로세스를 실행합니다.");
+
+        //TODO: validator
+        if (!systemNIs.exists(monitorInfo.ni())){
+            log.error("[monitor] 실행 실패 : 설정된 모니터 네트워크 인터페이스가 시스템에 존재하지 않습니다.");
+            return;
+        }
         Optional.ofNullable(this.process).ifPresent(MonitorLogProcess::terminate);
-        MonitorLogProcess p = new MonitorLogProcess(monitorInfo, sudoPassword);
-        this.process = p;
+        this.process = new MonitorLogProcess(monitorInfo, sudoPassword);
+        this.isProcDead = false;
+
         log.info("[monitor] 프로세스 실행 완료");
-        this.dead = false;
     }
 
 }
