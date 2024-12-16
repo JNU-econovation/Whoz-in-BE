685a4cd2bce96233a6299bf6289b332b75f863f1
coco3x <inferior3x@gmail.com>
2024-12-16 18:36:15 +0900

    rename: main api, network api - application 이름 변경

--- modules/network-api/src/main/java/com/whoz_in/network_log/NetworkLogApplication.java
+++ modules/network-api/src/main/java/com/whoz_in/network_log/NetworkLogApplication.java
@@ -1,13 +1,13 @@
-package com.whoz_in.main_api;
+package com.whoz_in.network_log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.whoz_in"})
-public class WhozInApiApplication {
+public class NetworkLogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WhozInApiApplication.class, args);
+        SpringApplication.run(NetworkLogApplication.class, args);
     }
 
 }
