141c4b8c76b83d04234accab04ea95d97ff2d7fb
coco3x <inferior3x@gmail.com>
2024-12-20 14:19:27 +0900

    rename: NetworkLog -> NetworkApi

--- modules/network-api/src/main/java/com/whoz_in/network_api/common/util/HttpUtils.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/common/util/HttpUtils.java
@@ -1,4 +1,4 @@
-package com.whoz_in.network_log.common.util;
+package com.whoz_in.network_api.common.util;
 
 import jakarta.servlet.http.HttpServletRequest;
 
--- modules/network-api/src/main/java/com/whoz_in/network_api/controller/IpAddressController.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/controller/IpAddressController.java
@@ -1,9 +1,8 @@
-package com.whoz_in.network_log.controller;
+package com.whoz_in.network_api.controller;
 
-import com.whoz_in.network_log.common.util.RequesterInfo;
+import com.whoz_in.network_api.common.util.RequesterInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
