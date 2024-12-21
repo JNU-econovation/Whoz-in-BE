9351d182a9965653ecb44789bdc97aba52e1a1fa
coco3x <inferior3x@gmail.com>
2024-12-22 01:40:42 +0900

    fix: AOP를 위해 DAO 클래스 final 제거

--- modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/ManagedLogDAO.java
+++ modules/infrastructure/log-writer/src/main/java/com/whoz_in/log_writer/managed/ManagedLogDAO.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public final class ManagedLogDAO {
+public class ManagedLogDAO {
 
     private final JdbcTemplate jdbcTemplate;
 
