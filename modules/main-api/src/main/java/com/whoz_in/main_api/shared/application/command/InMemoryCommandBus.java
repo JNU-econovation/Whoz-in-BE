da80ee6aece949364420d1d77851ec4126fd87a9
coco3x <inferior3x@gmail.com>
2024-12-23 07:19:56 +0900

    chore: spring 모듈 제거
    CommandHandlers는 스프링을 사용하는 이상 다른 방식으로 구현될 일이 없기 때문에 api 모듈의 것이 맞다고 판단
    기존에 구현된 CommandBus(InMemory)는 단일 서버인 main-api가 기본으로 사용하기 때문에 api에 있는 것이 맞다고 판단

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/application/command/InMemoryCommandBus.java
@@ -0,0 +1,17 @@
+package com.whoz_in.main_api.shared.application.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public final class InMemoryCommandBus implements CommandBus {
+    private final CommandHandlers handlers;
+
+    @Override
+    public void dispatch(Command command){
+        CommandHandler handler = handlers.findBy(command);
+        handler.handle(command);
+    }
+}
+
