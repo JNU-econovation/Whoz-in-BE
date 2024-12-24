5468a8d04239c59e34adba68119671562a8854f6
coco3x <inferior3x@gmail.com>
2024-12-24 20:45:00 +0900

    refactor(domain-jpa): yaml에서 env 로드

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/config/EnvConfig.java
+++
@@ -1,8 +0,0 @@
-package com.whoz_in.domain_jpa.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:/env.properties")
-public class EnvConfig {}
