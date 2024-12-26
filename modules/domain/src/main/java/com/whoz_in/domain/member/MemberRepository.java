b3a010db695b71bd40362c4f22de99b8922b57dd
coco3x <inferior3x@gmail.com>
2024-12-27 02:59:05 +0900

    feat: Member 도메인 추가

--- modules/domain/src/main/java/com/whoz_in/domain/member/MemberRepository.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.whoz_in.domain.member;
 
+import com.whoz_in.domain.member.model.Member;
+
 public interface MemberRepository {
   void save(Member member);
 }
---
+++ modules/domain/src/main/java/com/whoz_in/domain/member/exception/NotAuthMemberException.java
@@ -0,0 +1,9 @@
+package com.whoz_in.domain.member.exception;
+
+import com.whoz_in.domain.shared.BusinessException;
+
+public class NotAuthMemberException extends BusinessException {
+    public NotAuthMemberException() {
+        super("2006", "일반 로그인 정보가 없는 회원입니다.");
+    }
+}
