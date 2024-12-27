ef4a16eee471dd757f7779ef039b57da0f3a5ffc
coco3x <inferior3x@gmail.com>
2024-12-27 16:45:40 +0900

    feat(domain-jpa): JpaEntity <-> domain 모델 변환기

---
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/shared/BaseConverter.java
@@ -0,0 +1,10 @@
+package com.whoz_in.domain_jpa.shared;
+
+import com.whoz_in.domain.shared.AggregateRoot;
+
+//domain 모델(AggregateRoot)과 domain-jpa 모델(JpaEntity) 간 상호변환 기능을 담당한다.
+//domain-jpa의 Converter이므로 JpaEntity를 기준으로 메서드명을 작명하였다.
+public abstract class BaseConverter<T extends BaseEntity, R extends AggregateRoot> {
+    public abstract T from(R r);
+    public abstract R to(T t);
+}
