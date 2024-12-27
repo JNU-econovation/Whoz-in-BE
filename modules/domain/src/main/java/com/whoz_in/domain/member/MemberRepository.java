9a40a01f1d0be1077be0693fd5d8e02490dc2e61
coco3x <inferior3x@gmail.com>
2024-12-27 22:16:44 +0900

    feat(domain): loginId로 Member 찾기

--- modules/domain/src/main/java/com/whoz_in/domain/member/MemberRepository.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/MemberRepository.java
@@ -1,7 +1,13 @@
 package com.whoz_in.domain.member;
 
+import com.whoz_in.domain.member.exception.NoMemberException;
 import com.whoz_in.domain.member.model.Member;
+import java.util.Optional;
 
 public interface MemberRepository {
   void save(Member member);
+  Optional<Member> findByLoginId(String loginId);
+  default Member getByLoginId(String loginId){
+    return findByLoginId(loginId).orElseThrow(NoMemberException::new);
+  }
 }
