f4dd5c3a3c2faf5d2f5d1ffc8e9e12ac11f860e8
coco3x <inferior3x@gmail.com>
2024-12-27 21:12:35 +0900

    feat: MemberRepository 구현체 추가

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/member/MemberJpaAdapter.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/member/MemberJpaAdapter.java
@@ -2,13 +2,18 @@ package com.whoz_in.domain_jpa.member;
 
 import com.whoz_in.domain.member.MemberRepository;
 import com.whoz_in.domain.member.model.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberJpaAdapter implements MemberRepository {
+  private final MemberConverter converter;
+  private final MemberJpaRepository jpaRepository;
 
   @Override
   public void save(Member member) {
-
+    MemberEntity memberEntity = converter.from(member);
+    jpaRepository.save(memberEntity);
   }
 }
