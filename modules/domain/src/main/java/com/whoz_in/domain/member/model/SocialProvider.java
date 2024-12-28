d72c095032d41f5cd25736b8fd45168ace97b515
bellmin <whdlas011203@naver.com>
2024-12-29 00:23:00 +0900

    feat: SocialProvider Enum 클래스 정의
    - public static find 메소드 정의

--- modules/domain/src/main/java/com/whoz_in/domain/member/model/SocialProvider.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/model/SocialProvider.java
@@ -1,5 +1,24 @@
 package com.whoz_in.domain.member.model;
 
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
 public enum SocialProvider {
-    KAKAO
+    KAKAO("kakao");
+
+    private String socialProvider;
+
+    SocialProvider(String socialProvider) {
+        this.socialProvider = socialProvider;
+    }
+
+    public static SocialProvider findSocialProvider(String socialProvider){
+        return Arrays.stream(SocialProvider.values())
+                .filter(provider -> provider.getSocialProvider().equals(socialProvider))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("no social provider"));
+    }
+
+
 }
