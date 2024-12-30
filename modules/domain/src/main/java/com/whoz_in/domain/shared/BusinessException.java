dfdca173865c915f09b46e19e4e5f32b7cf82961
bellmin <whdlas011203@naver.com>
2024-12-30 14:21:18 +0900

    feat: BusinessException 수정
    - DomainException 과 동일
    - 에러코드 가지지 않는다

--- modules/domain/src/main/java/com/whoz_in/domain/shared/BusinessException.java
+++ modules/domain/src/main/java/com/whoz_in/domain/shared/BusinessException.java
@@ -3,12 +3,12 @@ package com.whoz_in.domain.shared;
 import lombok.Getter;
 
 //비즈니스 내에서 발생하는 예외이므로 HttpStatus는 담지 않습니다.
+// DomainException (= BusinessException) 은 에러 코드를 가지지 않는다.
 @Getter
 public abstract class BusinessException extends RuntimeException{
-    private final String errorCode;
 
-    protected BusinessException(String errorCode, String errorMessage) {
+    protected BusinessException(String errorMessage) {
         super(errorMessage);
-        this.errorCode = errorCode;
     }
+    
 }
