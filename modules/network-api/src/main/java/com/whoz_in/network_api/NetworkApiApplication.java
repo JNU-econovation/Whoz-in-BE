d7a1174703f6c78ead24865f7f11cbfccafa20cf
coco3x <inferior3x@gmail.com>
2025-01-01 01:50:50 +0900

    feat(network-api): log-writer가 요구하는 네트워크 인터페이스 정보를 network-api에서 관리합니다

--- modules/network-api/src/main/java/com/whoz_in/network_api/NetworkApiApplication.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/NetworkApiApplication.java
@@ -2,7 +2,9 @@ package com.whoz_in.network_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = {"com.whoz_in"})
 public class NetworkApiApplication {
 
