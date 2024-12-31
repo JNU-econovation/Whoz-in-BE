0e66e582ffa54b77a9b046e0b6f2e9820005bd3b
bellmin <whdlas011203@naver.com>
2024-12-31 17:52:00 +0900

    feat: LoginFailureHandler, LoginSuccessHandler 정의
    - Login 성공시, 실패시에 따른 처리 핸들러 구현

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/LoginFailureHandler.java
@@ -0,0 +1,21 @@
+package com.whoz_in.main_api.config.security.oauth2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        exception.printStackTrace();
+        response.sendRedirect("/login"); //TODO: Static
+    }
+}
