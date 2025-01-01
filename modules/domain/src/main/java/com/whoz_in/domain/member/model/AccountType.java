b0bd2ed6b5611c430f5b9a3790bc17534d6d23a9
bellmin <rlajm1203@naver.com>
2025-01-02 00:13:20 +0900

    feat: AccountType Enum 클래스 정의
    - admin , user : 일반 사용자와 관리자를 구분

---
+++ modules/domain/src/main/java/com/whoz_in/domain/member/model/AccountType.java
@@ -0,0 +1,23 @@
+package com.whoz_in.domain.member.model;
+
+import java.util.Arrays;
+
+public enum AccountType {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String accountType;
+
+    AccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public static AccountType findAccountType(String accountType){
+        return Arrays.stream(AccountType.values())
+                .filter(at -> at.name().equals(accountType))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("no account type"));
+    }
+
+}
