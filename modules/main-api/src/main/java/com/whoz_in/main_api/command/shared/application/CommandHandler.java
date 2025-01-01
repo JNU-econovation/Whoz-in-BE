7647c1bae87b12e72a59e9540ee00df629a8994d
coco3x <inferior3x@gmail.com>
2025-01-02 04:41:54 +0900

    update(main-api): CommandHandler가 반환값을 가질 수 있도록 합니다

--- modules/main-api/src/main/java/com/whoz_in/main_api/command/shared/application/CommandHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/command/shared/application/CommandHandler.java
@@ -3,7 +3,6 @@ package com.whoz_in.main_api.command.shared.application;
 /*
 우리의 새로운 서비스의 기능을 추가하고 싶을 때마다 이 핸들러와 적절한 Command를 구현하면 된다.
  */
-public interface CommandHandler<C extends Command>{
-    //TODO: 결과의 메타 데이터를 가지는 CommandResult를 반환하도록
-    void handle(C command);
+public interface CommandHandler<C extends Command, R>{
+    R handle(C command);
 }
