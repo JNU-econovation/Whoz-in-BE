e5f622b6cb9e7ffb263ad868f45e72431b71140a
bellmin <whdlas011203@naver.com>
2025-01-01 23:03:48 +0900

    feat: findByMemberId 메소드 추가
    - MemberId 로 Member를 찾는 메소드

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/member/MemberEntityJpaRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/member/MemberEntityJpaRepository.java
@@ -1,9 +1,11 @@
 package com.whoz_in.domain_jpa.member;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberEntityJpaRepository extends JpaRepository<MemberEntity, Long> {
+public interface MemberEntityJpaRepository extends JpaRepository<MemberEntity, UUID> {
     Optional<MemberEntity> findByLoginId(String loginId);
     Optional<MemberEntity> findByName(String name);
+    Optional<MemberEntity> findById(UUID id);
 }
