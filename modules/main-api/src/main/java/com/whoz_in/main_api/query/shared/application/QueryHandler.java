fc9243010cd0d65e618bbf07085a7d197040a787
coco3x <inferior3x@gmail.com>
2025-01-02 04:41:54 +0900

    update(main-api): CommandHandler가 반환값을 가질 수 있도록 합니다

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/shared/application/QueryHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/shared/application/QueryHandler.java
@@ -4,5 +4,5 @@ package com.whoz_in.main_api.query.shared.application;
 형변환 없이 구현체마다 다른 Response를 제공하기 위해 제네릭을 사용했습니다.
  */
 public interface QueryHandler<Q extends Query, R extends Response> {
-    public abstract R handle(Q query);
+    R handle(Q query);
 }
