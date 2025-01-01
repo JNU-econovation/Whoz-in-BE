0a4c210fca4e5e41fd3f6e2c8fa4cca97257a2dd
bellmin <whdlas011203@naver.com>
2025-01-01 21:31:04 +0900

    refactor: providerName 지역 변수 추가
    - clientRegistrationId 로 사용

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/ClientRegistrations.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/ClientRegistrations.java
@@ -1,5 +1,6 @@
 package com.whoz_in.main_api.config.security.oauth2;
 
+import com.whoz_in.domain.member.model.SocialProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -19,13 +20,14 @@ public class ClientRegistrations {
     private String redirectUri;
 
     public ClientRegistration kakaoClientRegistration() {
+        String providerName = SocialProvider.KAKAO.getProviderName();
 
-        return ClientRegistration.withRegistrationId("kakao")
+        return ClientRegistration.withRegistrationId(providerName)
                 .clientId(KAKAO_CLIENT_ID)
                 .clientSecret(KAKAO_SECRET)
                 .redirectUri(redirectUri)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .scope("account_email")
+                .scope("account_email", "profile_nickname", "profile_image")
                 .authorizationUri("https://kauth.kakao.com/oauth/authorize")
                 .tokenUri("https://kauth.kakao.com/oauth/token")
                 .userInfoUri("https://kapi.kakao.com/v2/user/me")
