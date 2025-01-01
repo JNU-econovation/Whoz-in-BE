74daf029d8a2b140026d8a728dc4f1d2dc72c8c3
bellmin <rlajm1203@naver.com>
2025-01-01 21:10:10 +0900

    feat: redirectUri 필드 추가

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/ClientRegistrations.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/ClientRegistrations.java
@@ -3,6 +3,7 @@ package com.whoz_in.main_api.config.security.oauth2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,17 +12,25 @@ public class ClientRegistrations {
     @Value("${oauth.kakao.secret}")
     private String KAKAO_SECRET;
 
+    @Value("${oauth.kakao.clientId}")
+    private String KAKAO_CLIENT_ID;
+
+    @Value("${oauth.redirectUri}")
+    private String redirectUri;
+
     public ClientRegistration kakaoClientRegistration() {
 
         return ClientRegistration.withRegistrationId("kakao")
-                .clientId("01b6ed2ff575a110d1b875dd96040e8a")
+                .clientId(KAKAO_CLIENT_ID)
                 .clientSecret(KAKAO_SECRET)
-                .redirectUri("http://localhost:8080/login/oauth2/code/kakao")
+                .redirectUri(redirectUri)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .scope("account_email")
                 .authorizationUri("https://kauth.kakao.com/oauth/authorize")
                 .tokenUri("https://kauth.kakao.com/oauth/token")
                 .userInfoUri("https://kapi.kakao.com/v2/user/me")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .userNameAttributeName("id")
                 .build();
     }
