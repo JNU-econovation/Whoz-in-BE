d3d78a377a188034194018a2eb4c2bccb2d3f568
bellmin <whdlas011203@naver.com>
2025-01-04 02:46:11 +0900

    feat(api): OAuth2TempToken 만료 기간 추가

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/JwtProperties.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/JwtProperties.java
@@ -23,11 +23,12 @@ public class JwtProperties {
     }
 
     @ConstructorBinding
-    public JwtProperties(String secret, Duration accessTokenExpiry, Duration refreshTokenExpiry, Duration oAuth2LoginTokenExpiry) {
+    public JwtProperties(String secret, Duration accessTokenExpiry, Duration refreshTokenExpiry, Duration oAuth2LoginTokenExpiry, Duration oAuth2TempTokenExpiry) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
         tokenExpiryDurations.put(TokenType.ACCESS, accessTokenExpiry);
         tokenExpiryDurations.put(TokenType.REFRESH, refreshTokenExpiry);
         tokenExpiryDurations.put(TokenType.OAUTH2_LOGIN_INFO, oAuth2LoginTokenExpiry);
+        tokenExpiryDurations.put(TokenType.OAUTH2_TEMP, oAuth2TempTokenExpiry);
         Arrays.stream(TokenType.values()).forEach(tokenType -> {
             if (!tokenExpiryDurations.containsKey(tokenType))
                 throw new IllegalStateException("토큰 만료 기간 설정 안됨");
