53c0814caef354b37371d0e821e6792d93f4a41a
bellmin <whdlas011203@naver.com>
2025-01-04 02:09:33 +0900

    feat(api): 새로운 토큰 타입 추가
    - OAuth2TempToken : 소셜 회원가입 시 임시로 발급해주는 토큰

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/TokenType.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/TokenType.java
@@ -2,10 +2,12 @@ package com.whoz_in.main_api.shared.jwt;
 
 import static com.whoz_in.main_api.config.security.consts.JwtConst.ACCESS_TOKEN;
 import static com.whoz_in.main_api.config.security.consts.JwtConst.OAUTH2_LOGIN_TOKEN;
+import static com.whoz_in.main_api.config.security.consts.JwtConst.OAUTH2_TEMP_TOKEN;
 import static com.whoz_in.main_api.config.security.consts.JwtConst.REFRESH_TOKEN;
 
 import com.whoz_in.main_api.shared.jwt.tokens.AccessToken;
 import com.whoz_in.main_api.shared.jwt.tokens.OAuth2LoginToken;
+import com.whoz_in.main_api.shared.jwt.tokens.OAuth2TempToken;
 import com.whoz_in.main_api.shared.jwt.tokens.RefreshToken;
 import com.whoz_in.main_api.shared.jwt.tokens.Token;
 import lombok.Getter;
@@ -17,7 +19,8 @@ import lombok.RequiredArgsConstructor;
 public enum TokenType {
     ACCESS(AccessToken.class, ACCESS_TOKEN),
     REFRESH(RefreshToken.class, REFRESH_TOKEN),
-    OAUTH2_LOGIN_INFO(OAuth2LoginToken.class, OAUTH2_LOGIN_TOKEN);
+    OAUTH2_LOGIN_INFO(OAuth2LoginToken.class, OAUTH2_LOGIN_TOKEN),
+    OAUTH2_TEMP(OAuth2TempToken.class, OAUTH2_TEMP_TOKEN);
 
     private final Class<? extends Token> tokenClass;
     //TODO: 웹에 종속되었다! 더 깔끔한 방법이 없을까..
