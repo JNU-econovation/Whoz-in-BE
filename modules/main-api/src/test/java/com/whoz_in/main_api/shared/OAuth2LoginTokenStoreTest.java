ee1b3593f9ac5047dcf2104fbdb6ab03caed6bba
bellmin <rlajm1203@naver.com>
2025-01-04 00:06:39 +0900

    refactor(api): 수정된 사항에 맞게 테스트 코드 수정

--- modules/main-api/src/test/java/com/whoz_in/main_api/shared/OAuth2LoginTokenStoreTest.java
+++ modules/main-api/src/test/java/com/whoz_in/main_api/shared/OAuth2LoginTokenStoreTest.java
@@ -1,37 +1,46 @@
 package com.whoz_in.main_api.shared;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.whoz_in.domain.member.model.SocialProvider;
 import com.whoz_in.main_api.shared.jwt.tokens.OAuth2LoginToken;
+import com.whoz_in.main_api.shared.jwt.tokens.OAuth2LoginTokenProvider;
 import com.whoz_in.main_api.shared.utils.OAuth2TokenStore;
 import com.whoz_in.main_api.shared.utils.OAuth2TokenStore.OAuth2TokenKey;
 import java.time.Instant;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class OAuth2LoginTokenStoreTest {
 
     private final OAuth2LoginToken testToken = new OAuth2LoginToken(SocialProvider.KAKAO, "naver", "testUser");
 
+    @Autowired
+    private OAuth2LoginTokenProvider oAuth2LoginTokenProvider;
+
     @Test
     @DisplayName("SocialIdKey 생성 테스트")
-    void SocialIdKey생성테스트() {
+    void OAuth2TokenKey생성테스트() {
         OAuth2TokenKey key = OAuth2TokenKey.create(testToken);
         System.out.println(key);
     }
 
     @Test
     @DisplayName("정상적인 SocialIdKey 테스트")
-    void SocialIdKey만료시간테스트() {
+    void OAuth2TokenKey만료시간테스트() {
         OAuth2TokenKey key = OAuth2TokenKey.create(testToken);
         System.out.println(key);
         System.out.println("현재 시간 : " + Instant.now().getEpochSecond());
-        Assertions.assertDoesNotThrow(key::ensureNotExpired);
+
+        Assertions.assertDoesNotThrow(()-> OAuth2TokenKey.ensureNotExpired(key.toString()));
     }
 
     @Test
     @DisplayName("만료된 SocialIdKey 테스트")
-    void SocialIdKey만료시간테스트2() {
+    void OAuth2TokenKey만료시간테스트2() {
         OAuth2TokenKey key = OAuth2TokenKey.create(testToken);
         System.out.println(key);
         System.out.println("4분 잠자기..");
@@ -41,18 +50,34 @@ public class OAuth2LoginTokenStoreTest {
             Thread.currentThread().start();
         }
 
-        Assertions.assertThrows(IllegalArgumentException.class, key::ensureNotExpired);
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> OAuth2TokenKey.ensureNotExpired(key.toString()));
     }
 
     @Test
     @DisplayName("저장소 테스트")
-    void SocialIdKey저장소테스트() {
-        OAuth2TokenStore store = new OAuth2TokenStore();
-        String socialId = "12356";
+    void OAuth2TokenKey생성테스트2() {
         OAuth2TokenKey key = OAuth2TokenKey.create(testToken);
         System.out.println("발급된 key : " + key);
         System.out.println("현재 시간 : " + Instant.now().getEpochSecond());
-        Assertions.assertDoesNotThrow(key::ensureNotExpired);
+        Assertions.assertDoesNotThrow(()->OAuth2TokenKey.ensureNotExpired(key.toString()));
+    }
+
+    @Test
+    @DisplayName("OAuth2TokenStore 저장 테스트")
+    void OAuth2TokenStore저장소테스트2() {
+        String key = OAuth2TokenStore.save(testToken);
+        System.out.println(key);
+        Assertions.assertNotEquals(key, OAuth2TokenKey.create(testToken));
+    }
+
+    @Test
+    @DisplayName("OAuth2TokenStore 저장된 토큰 추출 테스트")
+    void OAuth2TokenStore저장소테스트3(){
+        String key = OAuth2TokenStore.save(testToken);
+        System.out.println(key);
+        Assertions.assertEquals(testToken, OAuth2TokenStore.getSocialId(key));
+        System.out.println("추출된 OAuth Token : " + OAuth2TokenStore.getSocialId(key));
+        System.out.println("저장한 OAuth Token : " + testToken);
     }
 
 }
