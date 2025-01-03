7952d1e669693c6039c2f732d237901adff6c1b1
coco3x <inferior3x@gmail.com>
2025-01-04 00:54:14 +0900

    update(domain): network log는 이제 발생한 방도 기록합니다

--- modules/domain/src/main/java/com/whoz_in/domain/network_log/ManagedLog.java
+++ modules/domain/src/main/java/com/whoz_in/domain/network_log/ManagedLog.java
@@ -1,15 +1,18 @@
 package com.whoz_in.domain.network_log;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @RequiredArgsConstructor
 public final class ManagedLog {
     private final String mac;
     private final String ip;
-    private final String ssid;
     private final String deviceName;
+    private final String ssid;
+    private final String room;
     private final LocalDateTime createdAt;
 }
