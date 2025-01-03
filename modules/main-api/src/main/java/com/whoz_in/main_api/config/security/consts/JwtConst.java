70cbf98309b98f46be63a031091d81c70720a8cf
bellmin <whdlas011203@naver.com>
2025-01-04 02:17:10 +0900

    refactor(api): oauth temp token 상수 추가 및 변경

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/security/consts/JwtConst.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/security/consts/JwtConst.java
@@ -7,9 +7,10 @@ public class JwtConst {
     public static final String ACCESS_TOKEN = "access-token";
     public static final String REFRESH_TOKEN = "refresh-token";
     public static final String OAUTH2_LOGIN_TOKEN = "oauth2-login-token";
+    public static final String OAUTH2_TEMP_TOKEN = "oauth2-temp-token";
     public static final String TOKEN_TYPE = "token-type";
     public static final String TOKEN_ID = "token-id";
-    public static final String OAUTH2_TOKEN_KEY = "oauth2-token-key";
+    public static final String USER_INFO_KEY = "oauth-user-info-key";
     public static final String OAUTH2_TOKEN_KEY_DELIMITER = "::";
     public static final long OAUTH2_TOKEN_KEY_EXPIRATION_MIN = 3;
 
@@ -18,6 +19,7 @@ public class JwtConst {
     public static final String SOCIAL_PROVIDER = "social-provider";
     public static final String SOCIAL_ID = "social-id";
     public static final String EMAIL = "email";
+    public static final String NAME = "name";
     public static final String IS_REGISTERED = "is-registered";
     public static final String USER_ID = "user-id";
     public static final String ACCOUNT_TYPE = "account-type";
