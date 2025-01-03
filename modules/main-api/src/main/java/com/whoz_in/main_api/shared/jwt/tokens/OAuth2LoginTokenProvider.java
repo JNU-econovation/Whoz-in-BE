c8cc9e0300f99ad9f96150ca5b6997770b90bea5
bellmin <whdlas011203@naver.com>
2025-01-03 23:05:58 +0900

    refactor: OAuth2LoginTokenStore 변경에 따른 수정
    - Claim에 담을 정보, TokenKey 하나로 축소
    - TokenStore static 멤버화

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/OAuth2LoginTokenProvider.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/OAuth2LoginTokenProvider.java
@@ -1,50 +1,40 @@
 package com.whoz_in.main_api.shared.jwt.tokens;
 
 
-import static com.whoz_in.main_api.config.security.consts.JwtConst.EMAIL;
-import static com.whoz_in.main_api.config.security.consts.JwtConst.SOCIAL_ID;
-import static com.whoz_in.main_api.config.security.consts.JwtConst.SOCIAL_ID_KEY;
+import static com.whoz_in.main_api.config.security.consts.JwtConst.OAUTH2_TOKEN_KEY;
 import static com.whoz_in.main_api.config.security.consts.JwtConst.SOCIAL_PROVIDER;
 
 import com.whoz_in.domain.member.model.SocialProvider;
 import com.whoz_in.main_api.shared.jwt.JwtProperties;
 import com.whoz_in.main_api.shared.jwt.JwtUtil;
 import com.whoz_in.main_api.shared.jwt.TokenType;
-import com.whoz_in.main_api.shared.utils.SocialIdStore;
-import com.whoz_in.main_api.shared.utils.SocialIdStore.SocialIdKey;
+import com.whoz_in.main_api.shared.utils.OAuth2TokenStore;
+import com.whoz_in.main_api.shared.utils.OAuth2TokenStore.OAuth2TokenKey;
 import io.jsonwebtoken.Claims;
 import java.util.Map;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 //TODO: 블랙리스트에 있으면 예외
 @Component
 public final class OAuth2LoginTokenProvider extends TokenSerializer<OAuth2LoginToken> {
 
-    private final SocialIdStore socialIdStore;
-
     public OAuth2LoginTokenProvider(JwtUtil jwtUtil,
-                                    JwtProperties jwtProperties,
-                                    SocialIdStore socialIdStore) {
+                                    JwtProperties jwtProperties) {
         super(jwtUtil, jwtProperties);
-        this.socialIdStore = socialIdStore;
     }
 
     @Override
     protected OAuth2LoginToken buildToken(Claims claims) {
         SocialProvider socialProvider = SocialProvider.findSocialProvider(claims.get(SOCIAL_PROVIDER, String.class));
-        SocialIdKey socialIdKey = claims.get(SOCIAL_ID_KEY, SocialIdKey.class);
-        String socialId = socialIdStore.getSocialId(socialIdKey);
-        String email = claims.get(EMAIL, String.class);
-        return new OAuth2LoginToken(socialProvider, socialId, email);
+        OAuth2TokenKey socialIdKey = claims.get(OAUTH2_TOKEN_KEY, OAuth2TokenKey.class);
+
+        return OAuth2TokenStore.getSocialId(socialIdKey);
     }
 
     @Override
     public Map<String, String> buildClaims(OAuth2LoginToken oAuth2LoginToken) {
         return Map.of(
-                SOCIAL_PROVIDER, oAuth2LoginToken.getSocialProvider().getProviderName(),
-                SOCIAL_ID_KEY, socialIdStore.save(oAuth2LoginToken.getSocialId()).toString(),
-                EMAIL, oAuth2LoginToken.getEmail()
+                OAUTH2_TOKEN_KEY, OAuth2TokenStore.save(oAuth2LoginToken).toString()
         );
     }
 
