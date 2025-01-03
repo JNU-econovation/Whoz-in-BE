909b78afa22e09ef5626f3a72e3db3c785a94cfb
coco3x <inferior3x@gmail.com>
2025-01-03 17:31:57 +0900

    update(domain-jpa): 애그리거트 루트가 아닌 객체도 변환 가능하도록 함

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/shared/BaseConverter.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/shared/BaseConverter.java
@@ -4,7 +4,7 @@ import com.whoz_in.domain.shared.AggregateRoot;
 
 //domain 모델(AggregateRoot)과 domain-jpa 모델(JpaEntity) 간 상호변환 기능을 담당한다.
 //domain-jpa의 Converter이므로 JpaEntity를 기준으로 메서드명을 작명하였다.
-public abstract class BaseConverter<T extends BaseEntity, R extends AggregateRoot> {
+public abstract class BaseConverter<T extends BaseEntity, R> {
     public abstract T from(R r);
     public abstract R to(T t);
 }
