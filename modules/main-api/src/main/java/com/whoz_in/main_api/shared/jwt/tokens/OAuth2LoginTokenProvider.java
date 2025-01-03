b21a09e15928604acad51a0505312158fff6a404
bellmin <whdlas011203@naver.com>
2025-01-03 16:34:14 +0900

    refactor: OAuth2 JWT 토큰에 SocialIdKey를 담도록 수정
    - 기존 : SocialId 원문
    - SocialId를 그대로 담으면, 유출될 위험

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/OAuth2LoginTokenProvider.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/OAuth2LoginTokenProvider.java
@@ -3,28 +3,38 @@ package com.whoz_in.main_api.shared.jwt.tokens;
 
 import static com.whoz_in.main_api.config.security.consts.JwtConst.EMAIL;
 import static com.whoz_in.main_api.config.security.consts.JwtConst.SOCIAL_ID;
+import static com.whoz_in.main_api.config.security.consts.JwtConst.SOCIAL_ID_KEY;
 import static com.whoz_in.main_api.config.security.consts.JwtConst.SOCIAL_PROVIDER;
 
 import com.whoz_in.domain.member.model.SocialProvider;
 import com.whoz_in.main_api.shared.jwt.JwtProperties;
 import com.whoz_in.main_api.shared.jwt.JwtUtil;
 import com.whoz_in.main_api.shared.jwt.TokenType;
+import com.whoz_in.main_api.shared.utils.SocialIdStore;
+import com.whoz_in.main_api.shared.utils.SocialIdStore.SocialIdKey;
 import io.jsonwebtoken.Claims;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 //TODO: 블랙리스트에 있으면 예외
 @Component
 public final class OAuth2LoginTokenProvider extends TokenSerializer<OAuth2LoginToken> {
 
-    public OAuth2LoginTokenProvider(JwtUtil jwtUtil, JwtProperties jwtProperties) {
+    private final SocialIdStore socialIdStore;
+
+    public OAuth2LoginTokenProvider(JwtUtil jwtUtil,
+                                    JwtProperties jwtProperties,
+                                    SocialIdStore socialIdStore) {
         super(jwtUtil, jwtProperties);
+        this.socialIdStore = socialIdStore;
     }
 
     @Override
     protected OAuth2LoginToken buildToken(Claims claims) {
         SocialProvider socialProvider = SocialProvider.findSocialProvider(claims.get(SOCIAL_PROVIDER, String.class));
-        String socialId = claims.get(SOCIAL_ID, String.class);
+        SocialIdKey socialIdKey = claims.get(SOCIAL_ID_KEY, SocialIdKey.class);
+        String socialId = socialIdStore.getSocialId(socialIdKey);
         String email = claims.get(EMAIL, String.class);
         return new OAuth2LoginToken(socialProvider, socialId, email);
     }
@@ -33,7 +43,7 @@ public final class OAuth2LoginTokenProvider extends TokenSerializer<OAuth2LoginT
     public Map<String, String> buildClaims(OAuth2LoginToken oAuth2LoginToken) {
         return Map.of(
                 SOCIAL_PROVIDER, oAuth2LoginToken.getSocialProvider().getProviderName(),
-                SOCIAL_ID, oAuth2LoginToken.getSocialId(),
+                SOCIAL_ID_KEY, socialIdStore.save(oAuth2LoginToken.getSocialId()).toString(),
                 EMAIL, oAuth2LoginToken.getEmail()
         );
     }
