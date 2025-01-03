8ab991352657c55ae04c43b305d8cd9ec9620652
coco3x <inferior3x@gmail.com>
2025-01-03 23:36:16 +0900

    feat(domain, domain-jpa): network log 벌크 인서트 메서드 추가 및 구현

--- modules/domain/src/main/java/com/whoz_in/domain/network_log/MonitorLogRepository.java
+++ modules/domain/src/main/java/com/whoz_in/domain/network_log/MonitorLogRepository.java
@@ -1,5 +1,8 @@
 package com.whoz_in.domain.network_log;
 
+import java.util.Collection;
+
 public interface MonitorLogRepository {
     void save(MonitorLog log);
+    void saveAll(Collection<MonitorLog> logs);
 }
