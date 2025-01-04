86689c01dc28b3d8da94dca1e494c3e029b277c4
coco3x <inferior3x@gmail.com>
2025-01-04 20:41:36 +0900

    update(domain): 가장 최신의 managed log를 아이피로 조회합니다

--- modules/domain/src/main/java/com/whoz_in/domain/network_log/ManagedLogRepository.java
+++ modules/domain/src/main/java/com/whoz_in/domain/network_log/ManagedLogRepository.java
@@ -1,8 +1,10 @@
 package com.whoz_in.domain.network_log;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface ManagedLogRepository {
     void save(ManagedLog log);
     void saveAll(Collection<ManagedLog> logs);
+    Optional<ManagedLog> findLatestByIp(String ip);
 }
