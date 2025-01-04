7789b694c5d6a66629649e0360c06dff8a81eb37
coco3x <inferior3x@gmail.com>
2025-01-05 04:08:28 +0900

    update(domain): DeviceRepository - mac으로 device를 조회하는 메서드

--- modules/domain/src/main/java/com/whoz_in/domain/device/DeviceRepository.java
+++ modules/domain/src/main/java/com/whoz_in/domain/device/DeviceRepository.java
@@ -1,7 +1,12 @@
 package com.whoz_in.domain.device;
 
 import com.whoz_in.domain.device.model.Device;
+import com.whoz_in.domain.device.model.MacAddress;
+import java.util.Optional;
 
 public interface DeviceRepository {
     void save(Device device);
+
+    //해당 mac을 포함하는 device를 찾는 메서드
+    Optional<Device> findByMac(MacAddress mac);
 }
--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntityRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntityRepository.java
@@ -1,9 +1,12 @@
 package com.whoz_in.domain_jpa.device;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeviceEntityRepository extends JpaRepository<DeviceEntity, Long> {
-
+    @Query("SELECT d FROM DeviceEntity d JOIN d.deviceInfoEntity di WHERE di.mac = :mac")
+    Optional<DeviceEntity> findByMac(String mac);
 }
