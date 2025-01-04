2346c1801b7573981f132d93f9d400d861c06fdb
coco3x <inferior3x@gmail.com>
2025-01-04 23:26:43 +0900

    feat(domain-jpa): Device 도메인 구현

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/device/DeviceEntity.java
@@ -1,15 +1,39 @@
 package com.whoz_in.domain_jpa.device;
 
+import com.whoz_in.domain.member.model.Member;
 import com.whoz_in.domain_jpa.shared.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class DeviceEntity extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long deviceId;
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    private UUID id;
 
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    private UUID memberId;
+
+    private String name;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "device_id")
+    private List<DeviceInfoEntity> deviceInfoEntity;
 }
