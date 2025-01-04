a0d4da4c59ebec982135043e3c0042b4ec7ebf93
bellmin <whdlas011203@naver.com>
2025-01-04 15:38:36 +0900

    feat(api): ConfigurationPropertiesScan 추가

--- modules/main-api/src/main/java/com/whoz_in/main_api/MainApiApplication.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/MainApiApplication.java
@@ -2,8 +2,10 @@ package com.whoz_in.main_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(scanBasePackages = {"com.whoz_in"})
+@ConfigurationPropertiesScan
 public class MainApiApplication {
 
     public static void main(String[] args) {
