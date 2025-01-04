79d70c3fd126e3564b982dd784f8bad4a549abb8
coco3x <inferior3x@gmail.com>
2025-01-05 05:16:34 +0900

    update(domain): 시간 조건이 붙은 ManagedLog, MonitorLog 조회 메서드를 구현합니다

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
@@ -1,6 +1,10 @@
 package com.whoz_in.domain_jpa.monitor;
 
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MonitorLogEntityRepository extends JpaRepository<MonitorLogEntity, String> {
+    @Query("SELECT 1 FROM MonitorLogEntity m WHERE m.mac = :mac AND m.updatedAt > :time ORDER BY m.updatedAt DESC")
+    boolean existsTopByMacOrderByUpdatedAtDescAfter(String mac, LocalDateTime time);
 }
