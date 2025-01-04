098cbfdbb11a880f1b1c3418d67d942065a5aa3f
coco3x <inferior3x@gmail.com>
2025-01-04 23:12:47 +0900

    update(domain): DeviceInfo load 추가

--- modules/domain/src/main/java/com/whoz_in/domain/device/model/DeviceInfo.java
+++ modules/domain/src/main/java/com/whoz_in/domain/device/model/DeviceInfo.java
@@ -2,7 +2,6 @@ package com.whoz_in.domain.device.model;
 
 
 import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -16,5 +15,7 @@ public final class DeviceInfo {
     public static DeviceInfo create(String room, String ssid, MacAddress mac){
         return new DeviceInfo(room, ssid, mac);
     }
-
+    public static DeviceInfo load(String room, String ssid, String mac){
+        return new DeviceInfo(room, ssid, MacAddress.load(mac));
+    }
 }
