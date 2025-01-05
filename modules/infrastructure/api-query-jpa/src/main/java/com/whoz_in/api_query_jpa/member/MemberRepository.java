e945df3dfb9d4b761ac84bef7d4c7813bf2f7ee0
bellmin <rlajm1203@naver.com>
2025-01-05 23:37:16 +0900

    feat(api-query-jpa): MemberRepository 메소드 추가
    - device의 주인을 찾는 메소드
    - device들의 주인을 찾는 메소드

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberRepository.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberRepository.java
@@ -1,11 +1,20 @@
 package com.whoz_in.api_query_jpa.member;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
     Optional<Member> findByLoginId(String loginId);
+    Optional<Member> findByMemberId(UUID memberId);
+
+    @Query("SELECT m FROM Member m WHERE m.id IN (SELECT d.memberId FROM Device d WHERE d.deviceId = :deviceId)")
+    Optional<Member> findByDeviceId(UUID deviceId);
+
+    @Query("SELECT m FROM Member m WHERE m.id IN (SELECT d.memberId FROM Device d WHERE d.deviceId IN (:deviceIds))")
+    List<Member> findByDeviceIds(List<UUID> deviceIds);
 }
