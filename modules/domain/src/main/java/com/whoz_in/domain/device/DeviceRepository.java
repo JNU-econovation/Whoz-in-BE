d36b6aee47b792c5a415e321e2c037b1e9cb377a
bellmin <rlajm1203@naver.com>
2025-01-05 21:27:07 +0900

    feat(main-api): DeviceRepository 메소드 추가
    - findAll 메소드 추가

--- modules/domain/src/main/java/com/whoz_in/domain/device/DeviceRepository.java
+++ modules/domain/src/main/java/com/whoz_in/domain/device/DeviceRepository.java
@@ -1,7 +1,9 @@
 package com.whoz_in.domain.device;
 
 import com.whoz_in.domain.device.model.Device;
+import java.util.List;
 
 public interface DeviceRepository {
     void save(Device device);
+    List<Device> findAll();
 }
