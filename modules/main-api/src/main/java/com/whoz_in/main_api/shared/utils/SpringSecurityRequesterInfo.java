ea6f321fce09cdbd3d6387f0c76dc3251536007f
bellmin <whdlas011203@naver.com>
2025-01-05 16:58:29 +0900

    refactor(main-api): SpringSecurityRequesterInfo 조금 더 짧은 코드로 수정

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/utils/SpringSecurityRequesterInfo.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/utils/SpringSecurityRequesterInfo.java
@@ -2,6 +2,7 @@ package com.whoz_in.main_api.shared.utils;
 
 import com.whoz_in.domain.member.model.MemberId;
 import com.whoz_in.main_api.config.security.oauth2.JwtAuthentication;
+import io.jsonwebtoken.Jwt;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,14 +17,10 @@ public class SpringSecurityRequesterInfo implements RequesterInfo {
 
     @Override
     public Optional<MemberId> findMemberId() {
-        JwtAuthentication authentication;
-        try {
-            authentication = (JwtAuthentication) SecurityContextHolder.getContext()
-                    .getAuthentication(); // 이 결과가 null 일 경우
-        } catch (ClassCastException e){
-            throw new IllegalStateException("Authentication 타입 오류");
+        if(SecurityContextHolder.getContext().getAuthentication() instanceof JwtAuthentication authentication){
+            return Optional.of(authentication.getPrincipal());
         }
 
-        return Optional.of(authentication.getPrincipal());
+        throw new IllegalStateException("Authentication 타입 오류");
     }
 }
