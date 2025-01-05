437d96ae8972255ce4b5a705f076b2d2355416da
coco3x <inferior3x@gmail.com>
2025-01-05 23:58:06 +0900

    fix(main-api): oauth 로그인 로직 수정

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/LoginFailureHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/LoginFailureHandler.java
@@ -1,6 +1,5 @@
 package com.whoz_in.main_api.config.security.oauth2;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,7 +12,7 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-                                        AuthenticationException exception) throws IOException, ServletException {
+                                        AuthenticationException exception) throws IOException {
 
         exception.printStackTrace();
         response.sendRedirect("/login"); //TODO: Static
