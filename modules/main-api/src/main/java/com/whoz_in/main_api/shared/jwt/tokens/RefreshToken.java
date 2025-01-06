f4a5e4911cace2f0745ddad0b70ae38bba56055d
coco3x <inferior3x@gmail.com>
2025-01-06 23:36:33 +0900

    comment(main-api): RefreshToken의 tokenId 역할 기재

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/RefreshToken.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/RefreshToken.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED) //tokenId를 외부에서 설정할 수 없도록 한다.
 public final class RefreshToken extends Token {
     private final MemberId memberId;
-    private final UUID tokenId;
+    private final UUID tokenId; //블랙 리스트 등에 사용될 수 있음
 
     public RefreshToken(MemberId memberId) {
         this(memberId, UUID.randomUUID());
