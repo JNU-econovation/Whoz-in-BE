697514cfb75aa1f6016d1a26cb11c6e2fda70527
bellmin <rlajm1203@naver.com>
2025-01-06 19:53:22 +0900

    delete(main-api): 패키지 이동에 따른 삭제

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/ActiveDeviceListResponse.java
+++
@@ -1,9 +0,0 @@
-package com.whoz_in.main_api.query.device.application;
-
-import com.whoz_in.main_api.query.shared.application.Response;
-import java.util.List;
-
-public record ActiveDeviceListResponse(
-        List<ActiveDeviceResponse> responses
-) implements Response {
-}
