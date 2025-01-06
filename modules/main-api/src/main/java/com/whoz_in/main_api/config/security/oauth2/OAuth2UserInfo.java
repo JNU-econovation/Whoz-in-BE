5a24cf0a2ef23c448cafd7c80b28dbef7907e4f1
coco3x <inferior3x@gmail.com>
2025-01-07 01:51:13 +0900

    feat(main-api): 소셜 로그인 시 등록된 멤버인 경우 access/refresh token 반환

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/OAuth2UserInfo.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/security/oauth2/OAuth2UserInfo.java
@@ -11,8 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 // 우리 서비스에서 OAuth2 제공자로부터 받은 사용자 정보를 구성하는 클래스
 @Getter
 public class OAuth2UserInfo implements OAuth2User {
-
-    private final boolean isRegistered; //후즈인에 회원가입이 되어있는지
+    private final boolean isRegistered; //소셜 로그인한 계정으로 후즈인에 회원가입이 되어있는지
     private final SocialProvider socialProvider;
     private final String socialId;
 
@@ -38,7 +37,6 @@ public class OAuth2UserInfo implements OAuth2User {
         return Map.of();
     }
     // TODO: 일단 빈 리스트, 추후 권한 기능 생기면 변경
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.emptyList();
