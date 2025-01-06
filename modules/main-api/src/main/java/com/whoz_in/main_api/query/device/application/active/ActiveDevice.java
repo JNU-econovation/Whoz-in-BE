56e37fd91f991bbc8c8b2f24b1aaf0a199e1a488
bellmin <rlajm1203@naver.com>
2025-01-06 19:52:13 +0900

    refactor(main-api): device/active 패키지로 이동

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/ActiveDevice.java
@@ -0,0 +1,14 @@
+package com.whoz_in.main_api.query.device.application.active;
+
+import com.whoz_in.main_api.query.shared.application.View;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+// Active 상태인 기기를 나타내는 View
+// 접속 시작 시간, 접속 종료 시간
+public record ActiveDevice(
+    UUID deviceId,
+    UUID memberId,
+    LocalDateTime connectedTime,
+    LocalDateTime disconnectedTime
+) implements View { }
