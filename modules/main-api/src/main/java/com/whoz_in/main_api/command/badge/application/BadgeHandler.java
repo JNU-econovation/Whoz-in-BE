86e64b9ea49b8d6dabc5af31d32a4a60a915c498
westzeroright <kt8267@naver.com>
2025-01-08 05:51:10 +0900

    feat(main-api): 회원가입시 입력받는 분야를 뱃지로 등록
    - 회원가입시 발생하는 멤버 엔티티가 생성되었다는 이벤트를 받음
    - 이벤트로부터 회원의 정보를 얻고 회원의 분야에 대한 뱃지를 찾아 등록해줌

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/command/badge/application/BadgeHandler.java
@@ -0,0 +1,33 @@
+package com.whoz_in.main_api.command.badge.application;
+
+import com.whoz_in.domain.badge.BadgeRepository;
+import com.whoz_in.domain.badge.model.Badge;
+import com.whoz_in.domain.member.event.MemberCreated;
+import com.whoz_in.domain.member.model.Member;
+import com.whoz_in.domain.member.model.MemberId;
+import com.whoz_in.main_api.shared.application.Handler;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
+
+@Handler
+@RequiredArgsConstructor
+public class BadgeHandler {
+    private final BadgeRepository repository;
+
+    @Transactional
+    @EventListener
+    public Void handle(MemberCreated event) {
+        String position = event.getMember().getMainPosition().getPosition();
+        Badge badge = repository.findByName(position).orElseThrow();
+        Member member = event.getMember();
+        Badge newBadge = Badge.load(
+                badge.getId(),
+                badge.getBadgeInfo(),
+                List.of(new MemberId(member.getId().id()))
+        );
+        repository.register(newBadge);
+        return null;
+    }
+}
