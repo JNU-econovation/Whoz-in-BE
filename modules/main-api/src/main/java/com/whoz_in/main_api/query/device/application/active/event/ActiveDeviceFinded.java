dcc3f7d9e820b7ee890f303619292b4f5661e6a7
bellmin <whdlas011203@naver.com>
2025-01-07 16:42:23 +0900

    feat(main-api): ActiveDeviceFinded에 필드 추가
    - 이벤트 핸들러에서 Log 정보 필요

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/event/ActiveDeviceFinded.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/event/ActiveDeviceFinded.java
@@ -1,6 +1,8 @@
 package com.whoz_in.main_api.query.device.application.active.event;
 
 import com.whoz_in.domain.device.model.Device;
+import com.whoz_in.domain.network_log.MonitorLog;
+import com.whoz_in.main_api.query.device.application.active.ActiveDevice;
 import com.whoz_in.main_api.shared.event.Event;
 import java.util.List;
 import lombok.Getter;
@@ -9,14 +11,11 @@ import lombok.Getter;
 public class ActiveDeviceFinded implements Event {
 
     private final List<Device> devices;
+    private final List<MonitorLog> logs;
 
-    public ActiveDeviceFinded(List<Device> devices) {
+    public ActiveDeviceFinded(List<Device> devices, List<MonitorLog> logs) {
         this.devices = devices;
+        this.logs = logs;
     }
-
-    public static ActiveDeviceFinded of(List<Device> devices) {
-        return new ActiveDeviceFinded(devices);
-    }
-
 
 }
