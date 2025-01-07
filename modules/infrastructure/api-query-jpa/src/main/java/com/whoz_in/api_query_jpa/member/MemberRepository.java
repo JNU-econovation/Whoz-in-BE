9889e67b0a834a748260a6aca7842313f27a24ca
bellmin <rlajm1203@naver.com>
2025-01-07 19:05:21 +0900

    refactor(api-query-jpa): Device 파라미터 설정 및 수정된 필드 적용

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberRepository.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberRepository.java
@@ -5,15 +5,16 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
     Optional<Member> findByLoginId(String loginId);
 
-    @Query("SELECT m FROM Member m WHERE m.id IN (SELECT d.memberId FROM Device d WHERE d.deviceId = :deviceId)")
-    Optional<Member> findByDeviceId(UUID deviceId);
+    @Query("SELECT m FROM Member m WHERE m.id IN (SELECT d.memberId FROM Device d WHERE d.id = :deviceId)")
+    Optional<Member> findByDeviceId(@Param("deviceId") UUID deviceId);
 
-    @Query("SELECT m FROM Member m WHERE m.id IN (SELECT d.memberId FROM Device d WHERE d.deviceId IN (:deviceIds))")
-    List<Member> findByDeviceIds(List<UUID> deviceIds);
+    @Query("SELECT m FROM Member m WHERE m.id IN (SELECT d.memberId FROM Device d WHERE d.id IN (:deviceIds))")
+    List<Member> findByDeviceIds(@Param("deviceIds") List<UUID> deviceIds);
 }
