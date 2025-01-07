b672ede92007a11afd32265532b78175d16997c7
bellmin <whdlas011203@naver.com>
2025-01-07 19:06:42 +0900

    refactor(api-query-jpa): ActiveDevice InMemory 데이터베이스로 구현
    - InMemory 적용
    - InMemory 엔티티 적용

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/ActiveDeviceEntity.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/ActiveDeviceEntity.java
@@ -1,7 +1,6 @@
 package com.whoz_in.api_query_jpa.device;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -10,7 +9,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ActiveDeviceEntity {
