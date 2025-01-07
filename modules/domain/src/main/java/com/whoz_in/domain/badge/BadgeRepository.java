18f217c1a633658548bdec603bd443fdea38471f
westzeroright <kt8267@naver.com>
2025-01-08 05:46:27 +0900

    feat(domain): 뱃지 도메인 모델 요구사항 레포

---
+++ modules/domain/src/main/java/com/whoz_in/domain/badge/BadgeRepository.java
@@ -0,0 +1,10 @@
+package com.whoz_in.domain.badge;
+
+import com.whoz_in.domain.badge.model.Badge;
+import java.util.Optional;
+
+public interface BadgeRepository {
+    void save(Badge badge);
+    Optional<Badge> findByName(String name);
+    void register(Badge badge);
+}
