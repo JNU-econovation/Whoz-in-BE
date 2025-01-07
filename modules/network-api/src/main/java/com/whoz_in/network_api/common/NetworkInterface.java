92941033e764c4f081165a056bc4968696e6f053
coco3x <inferior3x@gmail.com>
2025-01-08 03:18:11 +0900

    update(network-api): 실제 ssid가 아닌 대체 ssid로 network log를 저장합니다

--- modules/network-api/src/main/java/com/whoz_in/network_api/common/NetworkInterface.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/common/NetworkInterface.java
@@ -2,21 +2,24 @@ package com.whoz_in.network_api.common;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@RequiredArgsConstructor
 public final class NetworkInterface{
-    @EqualsAndHashCode.Include
-    private final String name;
-    @EqualsAndHashCode.Include
-    private final String essid;
-    @EqualsAndHashCode.Include
+    //
+    @EqualsAndHashCode.Exclude
+    private final String altSsid; //network log의 ssid는 이걸로 저장된다. (ex: econo)
+    private final String interfaceName;
+    private final String realSsid; //실제 연결될 와이파이 이름 (ex: ECONO_5G)
     private final String mode;
 
-    @Override
-    public String toString() {
-        return "name: " + name +", ssid: "+ essid + ", mode: "+ mode;
+    public NetworkInterface(String altSsid, String interfaceName, String realSsid, String mode) {
+        this.altSsid = altSsid;
+        this.interfaceName = interfaceName;
+        this.realSsid = realSsid;
+        this.mode = mode;
+    }
+
+    public NetworkInterface(String interfaceName, String realSsid, String mode) {
+        this(realSsid, interfaceName, realSsid, mode);
     }
 }
