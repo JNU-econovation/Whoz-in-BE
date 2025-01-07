94fa8e1a7c2c80bf90e2224e1886c2bfb5b16a5d
bellmin <whdlas011203@naver.com>
2025-01-07 16:41:41 +0900

    feat(domain-jpa): MonitorLogEntityRepository 메소드 추가
    - findByUpdatedAtAfterOrderByUpdatedAtDesc : ActiveDevice 들을 찾기 위함

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
@@ -1,6 +1,11 @@
 package com.whoz_in.domain_jpa.monitor;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MonitorLogEntityRepository extends JpaRepository<MonitorLogEntity, String> {
+
+    List<MonitorLogEntity> findByUpdatedAtAfterOrderByUpdatedAtDesc(LocalDateTime updatedAt);
+
 }
