4189f1b43d0d73d558d2ad88201778318d818c32
bellmin <whdlas011203@naver.com>
2025-01-07 16:39:53 +0900

    feat(api-query-jpa): ActiveDeviceEventHandler 구현
    - ActiveDevice 와 관련된 이벤트 핸들러
    - ActiveDeviceFinded 이벤트 처리
    - InActiveDeviceFinded 이벤트 처리

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/event/ActiveDeviceEventHandler.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/event/ActiveDeviceEventHandler.java
@@ -3,12 +3,15 @@ package com.whoz_in.api_query_jpa.device.event;
 import com.whoz_in.api_query_jpa.device.ActiveDeviceEntity;
 import com.whoz_in.api_query_jpa.device.ActiveDeviceRepository;
 import com.whoz_in.domain.device.model.Device;
-import com.whoz_in.main_api.shared.domain.device.active.event.ActiveDeviceFinded;
+import com.whoz_in.main_api.query.device.application.active.event.ActiveDeviceFinded;
+import com.whoz_in.main_api.query.device.application.active.event.InActiveDeviceFinded;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -16,14 +19,29 @@ public class ActiveDeviceEventHandler {
 
     private final ActiveDeviceRepository activeDeviceRepository;
 
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     @EventListener(ActiveDeviceFinded.class)
-    public void handle(ActiveDeviceFinded event) {
+    public void saveActiveDevices(ActiveDeviceFinded event) {
         List<Device> devices = event.getDevices();
         List<ActiveDeviceEntity> entities = devices.stream()
                         .map(device -> ActiveDeviceEntity.create(device.getId(), LocalDateTime.now())) // TODO: active Time 을 이 시점으로 설정해도 될까?
                         .toList();
 
         activeDeviceRepository.saveAll(entities);
+    }
+
+    // 바로 위의 이벤트 핸들러에서 데이터를 수정할 수 있으므로, 격리 수준을 가장 강하게 설정
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @EventListener(ActiveDeviceFinded.class)
+    public void processInActiveDevices(InActiveDeviceFinded event) {
+        // InActiveDevice 찾는 로직
+        List<Device> devices = event.getDevices();
+        List<ActiveDeviceEntity> entities = activeDeviceRepository.findAll();
+
+        entities.stream()
+                .filter(activeDevice -> devices.stream()
+                        .anyMatch(device -> device.getId().id().equals(activeDevice.getDeviceId())))
+                .forEach(activeDevice -> activeDevice.inActiveOn(LocalDateTime.now()));
     }
 
 }
