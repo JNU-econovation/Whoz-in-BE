1d86e68d965fd85da9e7c0f47f9b5ddbe3ce16c9
westzeroright <kt8267@naver.com>
2025-01-08 05:58:49 +0900

    style(domain-jpa): 미처 못 지운 주석 제거

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/badge/BadgeEntityRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/badge/BadgeEntityRepository.java
@@ -2,13 +2,9 @@ package com.whoz_in.domain_jpa.badge;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BadgeEntityRepository extends JpaRepository<BadgeEntity, Long> {
     Optional<BadgeEntity> findByName(String name);
-//    @Query(value = "SELECT * FROM badge_entity WHERE name = :name", nativeQuery = true)
-//    Optional<BadgeEntity> findByName(@Param("name") String name);
 }
