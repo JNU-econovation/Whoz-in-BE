3ceb327b35c55fc110539a250cbee99be63c9786
bellmin <whdlas011203@naver.com>
2025-01-08 20:08:23 +0900

    feat(main-api): DeviceController 작성 및 ActiveDeviceListHandler 구현

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/ActiveDeviceListHandler.java
@@ -0,0 +1,51 @@
+package com.whoz_in.main_api.query.device.application.active;
+
+import com.whoz_in.main_api.query.member.application.MemberViewer;
+import com.whoz_in.main_api.query.shared.application.QueryHandler;
+import com.whoz_in.main_api.shared.application.Handler;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@Handler
+@RequiredArgsConstructor
+public class ActiveDeviceListHandler implements QueryHandler<ActiveDeviceList, ActiveDeviceListResponse> {
+
+    private final ActiveDeviceViewer activeDeviceViewer;
+    private final MemberViewer memberViewer;
+
+    @Override
+    public ActiveDeviceListResponse handle(ActiveDeviceList query) {
+        int page = query.page();
+        int size = query.size();
+        String sortType = query.sortType();
+
+        List<ActiveDevice> activeDevices = activeDeviceViewer.findAll();
+        List<ActiveDeviceResponse> responses = new ArrayList<>();
+
+        int start = page * size;
+        int end = start + size;
+
+        for(int i = start; i<end; i++){
+            ActiveDevice activeDevice = activeDevices.get(i);
+            ActiveDeviceResponse oneResponse = new ActiveDeviceResponse(
+                    activeDevice.deviceId().toString(),
+                    activeDevice.memberId().toString(),
+                    "MemberName",
+                    Duration.between(activeDevice.disconnectedTime(), activeDevice.connectedTime()).toString(),
+                    activeDevice.totalConnectedTime().toString()
+            );
+            responses.add(oneResponse);
+        }
+
+        // TODO : 정렬 자동화
+        if(sortType.equals("asc"))
+            responses.sort(Comparator.comparing(ActiveDeviceResponse::memberName));
+        else
+            responses.sort(Comparator.comparing(ActiveDeviceResponse::totalActiveTime));
+
+        return new ActiveDeviceListResponse(responses);
+    }
+}
