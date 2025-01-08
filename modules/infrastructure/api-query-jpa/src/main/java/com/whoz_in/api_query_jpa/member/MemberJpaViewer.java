3bde4a5ac62af64f3b71d39d3559ce79bbd94031
bellmin <whdlas011203@naver.com>
2025-01-08 20:04:18 +0900

    feat(main-api,api-query-jpa): MemberName 을 조회하기 위한 View 작성 및 구현

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberJpaViewer.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberJpaViewer.java
@@ -1,8 +1,10 @@
 package com.whoz_in.api_query_jpa.member;
 
 import com.whoz_in.main_api.query.member.application.MemberAuthInfo;
+import com.whoz_in.main_api.query.member.application.MemberName;
 import com.whoz_in.main_api.query.member.application.MemberViewer;
 import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +17,12 @@ public class MemberJpaViewer implements MemberViewer {
     public Optional<MemberAuthInfo> findAuthInfoByLoginId(String loginId) {
         return repository.findByLoginId(loginId)
                 .map(member -> new MemberAuthInfo(member.getId(), member.getLoginId(), member.getEncodedPassword()));
+    }
+
+    @Override
+    public Optional<MemberName> findNameByMemberId(String memberId) {
+        return repository.findById(UUID.fromString(memberId))
+                .map(member -> new MemberName(member.getName()));
+
     }
 }
