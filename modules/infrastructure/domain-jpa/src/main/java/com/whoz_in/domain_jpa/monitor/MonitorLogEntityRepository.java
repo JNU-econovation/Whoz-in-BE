08e3c9e0dbbb4571863778297a5974282e2c9841
bellmin <whdlas011203@naver.com>
2025-01-09 01:11:19 +0900

    refactor(domain): 필요 없는 메소드 삭제

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MonitorLogEntityRepository extends JpaRepository<MonitorLogEntity, String> {
 
