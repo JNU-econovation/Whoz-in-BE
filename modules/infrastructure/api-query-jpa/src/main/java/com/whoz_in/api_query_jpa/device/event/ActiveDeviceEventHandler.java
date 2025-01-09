8bf47fa82e58121fce0ef101e0423b46886d2938
bellmin <whdlas011203@naver.com>
2025-01-10 02:41:44 +0900

    feat(api-query-jpa): DeviceEventHandler 수정
    - DeviceStatusEventHandler 삭제
    - ActiveDeviceEventHandler 와 InActiveDeviceEventHandler 로 분리
    - ActiveDeviceEventHandler 저장 로직 수정

---
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/event/ActiveDeviceEventHandler.java
@@ -0,0 +1,66 @@
+package com.whoz_in.api_query_jpa.device.event;
+
+import com.whoz_in.api_query_jpa.device.ActiveDeviceEntity;
+import com.whoz_in.api_query_jpa.device.InMemoryActiveDeviceRepository;
+import com.whoz_in.main_api.query.device.application.active.event.ActiveDeviceFinded;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class ActiveDeviceEventHandler {
+
+//    private final ActiveDeviceRepository activeDeviceRepository;
+    // TODO: JPA 적용
+    private final InMemoryActiveDeviceRepository activeDeviceRepository;
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @EventListener(ActiveDeviceFinded.class)
+    public void saveActiveDevices(ActiveDeviceFinded event) {
+        List<UUID> deviceIds = event.getDevices();
+        List<ActiveDeviceEntity> activeDeviceEntities = activeDeviceRepository.findAll();
+
+        List<UUID> deviceIdFromDB = activeDeviceEntities.stream().map(ActiveDeviceEntity::getDeviceId).toList();
+
+        List<ActiveDeviceEntity> firstActiveDevices = deviceIds.stream()
+                .filter(deviceId -> deviceIdFromDB.stream().noneMatch(idFromDB -> idFromDB.equals(deviceId)))
+                .map(deviceId -> ActiveDeviceEntity.create(deviceId, LocalDateTime.now())) // TODO: active Time 을 이 시점으로 설정해도 될까?
+                .toList();
+
+
+        List<ActiveDeviceEntity> nonFirstActiveDevice = deviceIds.stream()
+                .filter(deviceIdFromDB::contains)
+                .map(deviceId -> {
+                        return activeDeviceEntities.stream()
+                                    .filter(active -> active.getDeviceId().equals(deviceId))
+                                    .findAny()
+                                    .orElse(null);
+                        })
+                .filter(Objects::nonNull)
+                .toList();
+
+
+        saveFirstActive(firstActiveDevices);
+        saveNonFirstActive(nonFirstActiveDevice);
+    }
+
+    private void saveFirstActive(List<ActiveDeviceEntity> entities){
+        // 처음으로 Active 된 Device 는 그냥 저장 가능
+        activeDeviceRepository.saveAll(entities);
+    }
+
+    private void saveNonFirstActive(List<ActiveDeviceEntity> entities){
+        // 참조를 이용한 JPA 변경 감지 짝퉁
+        entities.forEach(activeDevice -> {
+                    if(!activeDevice.isActive()) activeDevice.activeOn(LocalDateTime.now());
+                });
+    }
+
+}
--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/event/InActiveDeviceEventHandler.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/event/InActiveDeviceEventHandler.java
@@ -2,7 +2,6 @@ package com.whoz_in.api_query_jpa.device.event;
 
 import com.whoz_in.api_query_jpa.device.ActiveDeviceEntity;
 import com.whoz_in.api_query_jpa.device.InMemoryActiveDeviceRepository;
-import com.whoz_in.main_api.query.device.application.active.event.ActiveDeviceFinded;
 import com.whoz_in.main_api.query.device.application.active.event.InActiveDeviceFinded;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,24 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
-public class DeviceStatusEventHandler {
+public class InActiveDeviceEventHandler {
 
-//    private final ActiveDeviceRepository activeDeviceRepository;
-    // TODO: JPA 적용
     private final InMemoryActiveDeviceRepository activeDeviceRepository;
 
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    @EventListener(ActiveDeviceFinded.class)
-    public void saveActiveDevices(ActiveDeviceFinded event) {
-        List<UUID> devices = event.getDevices();
-        List<ActiveDeviceEntity> entities = devices.stream()
-                        .map(device -> ActiveDeviceEntity.create(device, LocalDateTime.now())) // TODO: active Time 을 이 시점으로 설정해도 될까?
-                        .toList();
-
-        activeDeviceRepository.saveAll(entities);
-    }
-
-    // 바로 위의 이벤트 핸들러에서 데이터를 수정할 수 있지만, ConcurrentHashMap 사용하므로 격리 수준을 default로 설정
+    // ActiveDeviceEvent 핸들러에서 데이터를 수정할 수 있으므로 격리 수준을 serializable 로 설정
     @Transactional(isolation = Isolation.SERIALIZABLE)
     @EventListener(InActiveDeviceFinded.class)
     public void processInActiveDevices(InActiveDeviceFinded event) {
