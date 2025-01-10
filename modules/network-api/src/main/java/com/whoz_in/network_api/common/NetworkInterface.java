daaa304737aa171cbc9cec4983a23627bbd9f5e1
coco3x <inferior3x@gmail.com>
2025-01-11 04:14:48 +0900

    refactor(network-api): 전체적으로 책임 분리

--- modules/network-api/src/main/java/com/whoz_in/network_api/common/NetworkInterface.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/common/NetworkInterface.java
@@ -1,25 +1,30 @@
 package com.whoz_in.network_api.common;
 
+import jakarta.annotation.Nullable;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public final class NetworkInterface{
-    //
-    @EqualsAndHashCode.Exclude
-    private final String altSsid; //network log의 ssid는 이걸로 저장된다. (ex: econo)
     private final String interfaceName;
     private final String realSsid; //실제 연결될 와이파이 이름 (ex: ECONO_5G)
     private final String mode;
 
-    public NetworkInterface(String altSsid, String interfaceName, String realSsid, String mode) {
-        this.altSsid = altSsid;
+    @EqualsAndHashCode.Exclude
+    @Nullable private final String altSsid; //network log의 ssid는 이걸로 저장된다. (ex: econo)
+    @EqualsAndHashCode.Exclude
+    @Nullable private final String command; //실행할 명령어
+
+    public NetworkInterface(String interfaceName, String realSsid, String mode, String altSsid, String command) {
         this.interfaceName = interfaceName;
         this.realSsid = realSsid;
         this.mode = mode;
+        this.altSsid = altSsid;
+        this.command = command;
     }
 
     public NetworkInterface(String interfaceName, String realSsid, String mode) {
-        this(realSsid, interfaceName, realSsid, mode);
+        this(interfaceName, realSsid, mode, realSsid, null);
     }
 }
