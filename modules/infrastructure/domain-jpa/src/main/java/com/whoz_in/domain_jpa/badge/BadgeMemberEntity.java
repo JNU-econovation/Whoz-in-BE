cfaa9cf03112cb0fdb4c6ee63eea4c177f06cbd3
westzeroright <kt8267@naver.com>
2025-01-12 00:30:14 +0900

    feat(domain-jpa): memberId, badgeId 복합키 설정

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/badge/BadgeMemberEntity.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/badge/BadgeMemberEntity.java
@@ -1,15 +1,13 @@
 package com.whoz_in.domain_jpa.badge;
 
+import com.whoz_in.domain_jpa.member.MemberEntity;
 import com.whoz_in.domain_jpa.shared.BaseEntity;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,20 +15,20 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@IdClass(BadgeMemberId.class)
 public class BadgeMemberEntity extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private MemberEntity memberEntity;
 
-    @Column(columnDefinition = "BINARY(16)", nullable = false)
-    private UUID memberId;
-
+    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "badge_id", nullable = false)
     private BadgeEntity badgeEntity;
 
-    public BadgeMemberEntity(UUID memberId, BadgeEntity badgeEntity) {
-        this.memberId = memberId;
+    public BadgeMemberEntity(MemberEntity memberEntity, BadgeEntity badgeEntity) {
+        this.memberEntity = memberEntity;
         this.badgeEntity = badgeEntity;
     }
 }
