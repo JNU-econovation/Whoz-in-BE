2632deff4584dba2a244a8abc68b2401038609b6
westzeroright <kt8267@naver.com>
2025-01-12 00:36:51 +0900

    refactor(domain-jpa): 멤버와 뱃지 관계 저장

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/badge/BadgeJpaRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/badge/BadgeJpaRepository.java
@@ -2,7 +2,6 @@ package com.whoz_in.domain_jpa.badge;
 
 import com.whoz_in.domain.badge.BadgeRepository;
 import com.whoz_in.domain.badge.model.Badge;
-import com.whoz_in.domain.member.model.MemberId;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class BadgeJpaRepository implements BadgeRepository {
     private final BadgeEntityRepository badgeRepo; //이거 도메인 아니고 JpaRepo
-    private final BadgeMemberEntityRepository badgeMemberRepo;
     private final BadgeConverter converter;
     @Override
     public void save(Badge badge) {
@@ -21,10 +19,5 @@ public class BadgeJpaRepository implements BadgeRepository {
     @Override
     public Optional<Badge> findByName(String name) {
         return badgeRepo.findByName(name).map(converter::to);
-    }
-
-    @Override
-    public void register(Badge badge) {
-        badgeMemberRepo.save(new BadgeMemberEntity(badge.getOwners().get(0).id(), converter.from(badge)));
     }
 }
--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/member/MemberJpaRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/member/MemberJpaRepository.java
@@ -3,6 +3,12 @@ package com.whoz_in.domain_jpa.member;
 import com.whoz_in.domain.member.MemberRepository;
 import com.whoz_in.domain.member.model.Member;
 import com.whoz_in.domain.member.model.MemberId;
+import com.whoz_in.domain.member.model.SocialProvider;
+import com.whoz_in.domain_jpa.badge.BadgeConverter;
+import com.whoz_in.domain_jpa.badge.BadgeEntity;
+import com.whoz_in.domain_jpa.badge.BadgeEntityRepository;
+import com.whoz_in.domain_jpa.badge.BadgeMemberEntity;
+import com.whoz_in.domain_jpa.badge.BadgeMemberEntityRepository;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,45 +18,49 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class MemberJpaRepository implements MemberRepository {
-  private final MemberConverter converter;
-  private final MemberEntityJpaRepository jpaRepository;
+  private final MemberConverter memberConverter;
+  private final MemberEntityJpaRepository memberRepo;
+  private final BadgeMemberEntityRepository badgeMemberRepo;
+  private final BadgeConverter badgeConverter;
+  private final BadgeEntityRepository badgeRepo;
+  @Override
+  public void addBadge(MemberId memberId, BadgeId badgeId) {
+    MemberEntity memberEntity = memberRepo.findById(memberId.id()).orElseThrow();
+    BadgeEntity badgeEntity = badgeRepo.findById(badgeId.id()).orElseThrow();
+    badgeMemberRepo.save(new BadgeMemberEntity(memberEntity,badgeEntity));
+  }
 
   @Override
   public void save(Member member) {
-    MemberEntity memberEntity = converter.from(member);
-    jpaRepository.save(memberEntity);
+    MemberEntity memberEntity = memberConverter.from(member);
+    memberRepo.save(memberEntity);
   }
 
   @Override
   public Optional<Member> findByLoginId(String loginId) {
-    return jpaRepository.findByLoginId(loginId).map(converter::to);
+    return memberRepo.findByLoginId(loginId).map(memberConverter::to);
   }
 
   @Override
-  public boolean existsBySocialId(String socialId) {
-    return jpaRepository.existsBySocialId(socialId);
-  }
-
-  @Override
-  public boolean existsByMemberId(MemberId memberId) {
-    return jpaRepository.existsById(memberId.id());
+  public boolean existsBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId) {
+    return memberRepo.existsBySocialProviderAndSocialId(socialProvider, socialId);
   }
 
   @Override
   public List<Member> findByName(String name) {
-    List<MemberEntity> entities = jpaRepository.findByName(name);
-    return entities.stream().map(converter::to).toList();
+    List<MemberEntity> entities = memberRepo.findByName(name);
+    return entities.stream().map(memberConverter::to).toList();
   }
 
   @Override
   public Optional<Member> findByMemberId(MemberId id) {
     UUID memberId = id.id();
-    Optional<MemberEntity> entity = jpaRepository.findById(memberId);
-    return entity.map(converter::to);
+    Optional<MemberEntity> entity = memberRepo.findById(memberId);
+    return entity.map(memberConverter::to);
   }
 
   @Override
-  public Optional<Member> findBySocialId(String socialId) {
-    return jpaRepository.findBySocialId(socialId).map(converter::to);
+  public Optional<Member> findBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId) {
+    return memberRepo.findBySocialProviderAndSocialId(socialProvider, socialId).map(memberConverter::to);
   }
 }
