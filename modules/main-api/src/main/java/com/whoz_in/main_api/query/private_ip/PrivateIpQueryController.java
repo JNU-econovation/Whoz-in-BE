ff8cbd3358a5d4ec384362aaaa6436bf71dfa923
coco3x <inferior3x@gmail.com>
2025-01-15 14:05:18 +0900

    update(main-api): 내부 아이피 조회 api - 전체를 반환할 수 있도록 수정

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpQueryController.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpQueryController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +19,8 @@ public class PrivateIpQueryController extends QueryController {
         super(queryBus);
     }
 
-    @GetMapping("/private-ip/{room}")
-    public ResponseEntity<SuccessBody<PrivateIpList>> getPrivateIps(@PathVariable String room){
+    @GetMapping("/private-ip")
+    public ResponseEntity<SuccessBody<PrivateIpList>> getPrivateIps(@RequestParam(required = false) String room){
         return ResponseEntityGenerator.success(ask(new PrivateIpListGet(room)), CrudResponseCode.READ);
     }
 }
