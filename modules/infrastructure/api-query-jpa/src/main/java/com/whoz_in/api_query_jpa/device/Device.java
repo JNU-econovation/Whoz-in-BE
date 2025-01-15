8d50c30e334787545f470d16985c6adc41c669ce
bellmin <whdlas011203@naver.com>
2025-01-15 23:30:17 +0900

    fix(api-query-jpa): 세미콜론 추가

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/Device.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/Device.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import java.util.List;
 import java.util.UUID;
