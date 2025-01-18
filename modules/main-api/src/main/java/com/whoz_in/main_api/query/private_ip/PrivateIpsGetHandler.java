c71bb9fccec968a5869fa0530ccbc1b9aa1d6366
coco3x <inferior3x@gmail.com>
2025-01-18 21:06:03 +0900

    update(main-api): network-api의 내부 아이피를 ssid에 매핑하여 반환

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpsGetHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpsGetHandler.java
@@ -3,18 +3,18 @@ package com.whoz_in.main_api.query.private_ip;
 import com.whoz_in.main_api.query.shared.application.QueryHandler;
 import com.whoz_in.main_api.shared.application.Handler;
 import com.whoz_in.main_api.shared.caching.private_ip.PrivateIpStore;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 @Handler
 @RequiredArgsConstructor
-public class PrivateIpListGetHandler implements QueryHandler<PrivateIpListGet, PrivateIpList> {
+public class PrivateIpsGetHandler implements QueryHandler<PrivateIpsGet, PrivateIps> {
     private final PrivateIpStore privateIpStore;
     @Override
-    public PrivateIpList handle(PrivateIpListGet query) {
-        List<String> ipList = (query.room() != null) ?
-                privateIpStore.getIpList(query.room()) :
-                privateIpStore.getIpList();
-        return new PrivateIpList(ipList);
+    public PrivateIps handle(PrivateIpsGet query) {
+        return new PrivateIps(
+                (query.room() != null) ?
+                        privateIpStore.get(query.room()) :
+                        privateIpStore.get()
+        );
     }
 }
