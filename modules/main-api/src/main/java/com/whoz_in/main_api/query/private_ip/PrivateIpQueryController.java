4b6802b98a600066f7a5348eb59d07a42241a13e
coco3x <inferior3x@gmail.com>
2025-01-18 21:06:03 +0900

    update(main-api): network-api의 내부 아이피를 ssid에 매핑하여 반환

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpQueryController.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpQueryController.java
@@ -7,7 +7,6 @@ import com.whoz_in.main_api.shared.presentation.ResponseEntityGenerator;
 import com.whoz_in.main_api.shared.presentation.SuccessBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +18,8 @@ public class PrivateIpQueryController extends QueryController {
         super(queryBus);
     }
 
-    @GetMapping("/private-ip")
-    public ResponseEntity<SuccessBody<PrivateIpList>> getPrivateIps(@RequestParam(required = false) String room){
-        return ResponseEntityGenerator.success(ask(new PrivateIpListGet(room)), CrudResponseCode.READ);
+    @GetMapping("/private-ips")
+    public ResponseEntity<SuccessBody<PrivateIps>> getPrivateIps(@RequestParam(required = false) String room){
+        return ResponseEntityGenerator.success(ask(new PrivateIpsGet(room)), CrudResponseCode.READ);
     }
 }
--- modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpsGet.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpsGet.java
@@ -3,6 +3,6 @@ package com.whoz_in.main_api.query.private_ip;
 import com.whoz_in.domain.shared.Nullable;
 import com.whoz_in.main_api.query.shared.application.Query;
 
-public record PrivateIpListGet(
+public record PrivateIpsGet(
   @Nullable String room
 ) implements Query {}
