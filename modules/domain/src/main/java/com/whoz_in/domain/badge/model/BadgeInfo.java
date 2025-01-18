bceb967730ab932760c81edbb6dd84eabd9230ce
westzeroright <kt8267@naver.com>
2025-01-18 19:37:00 +0900

    feat(domain): 색상코드의 형식이 올바른지 검사하고 예외 처리

--- modules/domain/src/main/java/com/whoz_in/domain/badge/model/BadgeInfo.java
+++ modules/domain/src/main/java/com/whoz_in/domain/badge/model/BadgeInfo.java
@@ -1,8 +1,10 @@
 package com.whoz_in.domain.badge.model;
 
+import com.whoz_in.domain.badge.exception.InvalidFormColorCodeException;
 import com.whoz_in.domain.member.model.MemberId;
 import java.awt.Color;
 import java.util.Random;
+import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +12,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class BadgeInfo {
+    public static final String COLOR_CODE_REGEX = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$\n";
     private final String name;
     private final BadgeType badgeType;
     private final MemberId creator;
     private final String colorCode;
 
     public static BadgeInfo create(String name, BadgeType badgeType, String colorCode,MemberId creator) {
+        if (!Pattern.matches(COLOR_CODE_REGEX,colorCode)) {
+            throw InvalidFormColorCodeException.EXCEPTION;
+        }
         return new BadgeInfo(name,badgeType,creator,colorCode);
     }
 
