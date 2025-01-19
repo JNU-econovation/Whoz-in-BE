b451b4c7ce9f11bb28f98a726ef7441d2a655f72
coco3x <inferior3x@gmail.com>
2025-01-19 23:53:17 +0900

    feat(main-api): security - JwtAuthentication이 없는 요청일 경우 에러 코드를 반환

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.whoz_in.main_api.config.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+//JwtAuthentication이 없을경우 실행됨 (access token이 없다는 뜻)
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException{
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        response.getWriter().write("{\"message\": \"인증이 필요합니다\", \"error_code\": 2001}");
+    }
+}
