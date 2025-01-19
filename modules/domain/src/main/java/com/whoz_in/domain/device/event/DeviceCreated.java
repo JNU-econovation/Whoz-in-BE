2a39eb20cb5ae0f8873f7b2e2c1eb4185522a988
rlajm1203 <rlajm1203@naver.com>
2025-01-20 02:03:50 +0900

    feat(domain): DeviceCreated deviceId 필드 추가

--- modules/domain/src/main/java/com/whoz_in/domain/device/event/DeviceCreated.java
+++ modules/domain/src/main/java/com/whoz_in/domain/device/event/DeviceCreated.java
@@ -1,7 +1,12 @@
 package com.whoz_in.domain.device.event;
 
 import com.whoz_in.domain.shared.event.DomainEvent;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public final class DeviceCreated extends DomainEvent {
-
+    private final UUID deviceId;
 }
