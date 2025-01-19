e96e02ec0e6493bff730ffcfcb4be02a7b852be2
rlajm1203 <whdlas011203@naver.com>
2025-01-19 21:20:54 +0900

    refactor(main-api): 기기마다 조회가 아닌, Member 마다 조회로 변경

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/MembersInRoomHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/MembersInRoomHandler.java
@@ -7,6 +7,7 @@ import com.whoz_in.main_api.query.member.application.MemberInfo;
 import com.whoz_in.main_api.query.member.application.MemberViewer;
 import com.whoz_in.main_api.query.shared.application.QueryHandler;
 import com.whoz_in.main_api.shared.application.Handler;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
@@ -40,25 +41,14 @@ public class MembersInRoomHandler implements QueryHandler<MembersInRoom, Members
             int start = page * size;
             int end = Math.min((start + size), activeDevicesByMemberId.keySet().size());
 
+            List<MemberId> memberIds = activeDevicesByMemberId.keySet().stream().toList();
+
             for (int i = start; i < end; i++) {
-                ActiveDevice activeDevice = activeDevices.get(i);
-                MemberInfo ownerInfo = getMemberName(activeDevice.memberId().toString());
+                MemberId memberId = memberIds.get(i);
+                List<ActiveDevice> activeDevicesByMember = activeDevicesByMemberId.get(memberId);
 
-                String memberId = activeDevice.memberId().toString();
-                int generation = ownerInfo.generation();
-                String memberName = ownerInfo.memberName();
-                Long continuousMinute = activeDevice.continuousTime().toMinutes();
-                Long totalConnectedMinute = activeDevice.totalConnectedTime().toMinutes();
-                boolean isActive = activeDevice.isActive();
+                MemberInRoomResponse oneResponse = toResponse(memberId, activeDevicesByMember);
 
-                MemberInRoomResponse oneResponse = new MemberInRoomResponse(
-                        generation,
-                        memberId,
-                        memberName,
-                        String.format("%s시간 %s분", continuousMinute / 60, continuousMinute % 60),
-                        String.format("%s시간 %s분", totalConnectedMinute / 60, totalConnectedMinute % 60),
-                        isActive
-                );
                 responses.add(oneResponse);
             }
 
@@ -68,10 +58,45 @@ public class MembersInRoomHandler implements QueryHandler<MembersInRoom, Members
             else
                 responses.sort(Comparator.comparing(MemberInRoomResponse::totalActiveTime));
 
-            return new MembersInRoomResponse(responses);
+            return new MembersInRoomResponse(responses, responses.size());
         }
 
-        return new MembersInRoomResponse(responses);
+        return new MembersInRoomResponse(responses, 0);
+    }
+
+    private MemberInRoomResponse toResponse(MemberId memberId, List<ActiveDevice> devices){
+        MemberInfo ownerInfo = getMemberName(memberId.id().toString());
+
+        int generation = ownerInfo.generation();
+        String memberName = ownerInfo.memberName();
+
+        // active 기기가 여러 개라면, 여러 기기 중, 가장 큰 연속 접속 시간만 보여준다.
+        Long continuousMinute = devices.stream()
+                .filter(ActiveDevice::isActive)
+                .map(ActiveDevice::continuousTime)
+                .max(Duration::compareTo)
+                .orElse(Duration.ZERO)
+                .toMinutes();
+
+        Long totalConnectedMinute = devices.stream()
+                .map(ActiveDevice::totalConnectedTime)
+                .max(Duration::compareTo)
+                                .orElse(Duration.ZERO)
+                                        .toMinutes();
+
+        boolean isActive = devices.stream()
+                .anyMatch(ActiveDevice::isActive);
+
+        // 1. 여러 기기 중, 연속 접속 시간, 누적 접속 시간을 합한 정보를 보여준다.
+
+        return new MemberInRoomResponse(
+                generation,
+                memberId.id().toString(),
+                memberName,
+                String.format("%s시간 %s분", continuousMinute / 60, continuousMinute % 60),
+                String.format("%s시간 %s분", totalConnectedMinute / 60, totalConnectedMinute % 60),
+                isActive
+        );
     }
 
     private Map<MemberId, List<ActiveDevice>> createMemberDeviceMap(List<ActiveDevice> activeDevices) {
