d663209783324b2feacf7162e686d9a97b9c58be
rlajm1203 <whdlas011203@naver.com>
2025-01-19 20:40:20 +0900

    refactor(main-api): Response 클래스 Member 기준으로 변경
    - 동아리방 내부에 있는 Member들

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/MembersInRoomResponse.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/active/MembersInRoomResponse.java
@@ -3,7 +3,7 @@ package com.whoz_in.main_api.query.device.application.active;
 import com.whoz_in.main_api.query.shared.application.Response;
 import java.util.List;
 
-public record MemberListResponse(
-        List<MemberResponse> responses
+public record MembersInRoomResponse(
+        List<MemberInRoomResponse> responses
 ) implements Response {
 }
