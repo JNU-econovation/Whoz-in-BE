8d961ec0570180b9335f0c5077ba55d301f33670
coco3x <inferior3x@gmail.com>
2025-01-21 02:21:05 +0900

    feat(main-api): 필터에서 발생한 예외 혹은 DispatcherSevlet에서 잡지 않은 예외 로깅

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/ExceptionLoggingFilter.java
@@ -0,0 +1,34 @@
+package com.whoz_in.main_api.config;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ExceptionLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        try {
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            logRequestDetails(req);
+            throw e;
+        }
+    }
+
+    private void logRequestDetails(HttpServletRequest request) {
+        log.error("요청 URL: {}", request.getRequestURL());
+        log.error("요청 메서드: {}", request.getMethod());
+        log.error("User-Agent: {}", request.getHeader("User-Agent"));
+    }
+}
