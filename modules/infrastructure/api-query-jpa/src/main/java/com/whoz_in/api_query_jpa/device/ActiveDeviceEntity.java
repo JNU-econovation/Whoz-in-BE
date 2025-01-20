2f9813a08da59823b0a39924faffaa5395775f8d
rlajm1203 <whdlas011203@naver.com>
2025-01-21 00:15:04 +0900

    refactor(api-query-jpa): ActiveDeviceEntity 필드 수정

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/ActiveDeviceEntity.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/ActiveDeviceEntity.java
@@ -2,9 +2,7 @@ package com.whoz_in.api_query_jpa.device;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Objects;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,31 +20,8 @@ public class ActiveDeviceEntity {
 
     private LocalDateTime disConnectedTime;
 
-    private Duration dailyActiveTime;
-
-    private Duration totalActiveTime;
-
-    private boolean isActive;
-
     private ActiveDeviceEntity(UUID deviceId) {
         this.deviceId = deviceId;
-    }
-
-    public void activeOn(LocalDateTime connectedTime){
-        this.isActive = true;
-        this.connectedTime = connectedTime;
-        this.disConnectedTime = null;
-    }
-
-    public void inActiveOn(LocalDateTime disConnectedTime){
-        this.isActive = false;
-        this.disConnectedTime = disConnectedTime;
-        addDailyActiveTime();
-    }
-
-    private void addDailyActiveTime(){
-        Duration add = Duration.between(connectedTime, disConnectedTime).abs();
-        this.dailyActiveTime = Objects.nonNull(dailyActiveTime) ? dailyActiveTime.plus(add) :  add;
     }
 
     public static ActiveDeviceEntity create(UUID deviceId){
