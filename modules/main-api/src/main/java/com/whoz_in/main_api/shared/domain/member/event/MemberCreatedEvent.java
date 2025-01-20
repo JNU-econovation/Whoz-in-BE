9b583bd8fc48a64eeb66b783db2c80ec700d05a8
rlajm1203 <whdlas011203@naver.com>
2025-01-21 01:36:11 +0900

    refactor(main-api): 인터페이스 이름 변경
    - ApplicationEvent로 이름 변경

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/member/event/MemberCreatedEvent.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/member/event/MemberCreatedEvent.java
@@ -1,9 +1,9 @@
 package com.whoz_in.main_api.shared.domain.member.event;
 
-import com.whoz_in.main_api.shared.event.Event;
+import com.whoz_in.main_api.shared.event.ApplicationEvent;
 import java.util.UUID;
 
 // TODO: 이벤트 이름 다시 생각해보기
-public record MemberCreatedEvent(UUID memberId) implements Event {
+public record MemberCreatedEvent(UUID memberId) implements ApplicationEvent {
 
 }
