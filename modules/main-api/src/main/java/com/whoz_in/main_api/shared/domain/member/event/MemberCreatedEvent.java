5a7f5ed8be90aaefbec503b6bb5ba7e71649de59
rlajm1203 <whdlas011203@naver.com>
2025-01-21 01:30:20 +0900

    refactor(main-api): 레코드로 변경

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/member/event/MemberCreatedEvent.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/member/event/MemberCreatedEvent.java
@@ -2,12 +2,8 @@ package com.whoz_in.main_api.shared.domain.member.event;
 
 import com.whoz_in.main_api.shared.event.Event;
 import java.util.UUID;
-import lombok.RequiredArgsConstructor;
 
 // TODO: 이벤트 이름 다시 생각해보기
-@RequiredArgsConstructor
-public class MemberCreatedEvent implements Event {
-
-    private final UUID memberId;
+public record MemberCreatedEvent(UUID memberId) implements Event {
 
 }
