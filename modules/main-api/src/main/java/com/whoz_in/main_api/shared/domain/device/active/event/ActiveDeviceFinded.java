a3e8024e79c06cb303fbbc1f003df62d68969924
rlajm1203 <whdlas011203@naver.com>
2025-01-21 01:36:11 +0900

    refactor(main-api): 인터페이스 이름 변경
    - ApplicationEvent로 이름 변경

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/event/ActiveDeviceFinded.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/event/ActiveDeviceFinded.java
@@ -1,13 +1,12 @@
 package com.whoz_in.main_api.shared.domain.device.active.event;
 
-import com.whoz_in.domain.network_log.MonitorLog;
-import com.whoz_in.main_api.shared.event.Event;
+import com.whoz_in.main_api.shared.event.ApplicationEvent;
 import java.util.List;
 import java.util.UUID;
 import lombok.Getter;
 
 @Getter
-public class ActiveDeviceFinded implements Event {
+public class ActiveDeviceFinded implements ApplicationEvent {
 
     private final List<UUID> devices;
     // 모니터 로그는 ActiveDevice 가 언제 Active 되었는지 시간을 삽입하기 위해 필요하다.
--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/event/InActiveDeviceFinded.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/event/InActiveDeviceFinded.java
@@ -1,12 +1,12 @@
 package com.whoz_in.main_api.shared.domain.device.active.event;
 
-import com.whoz_in.main_api.shared.event.Event;
+import com.whoz_in.main_api.shared.event.ApplicationEvent;
 import java.util.List;
 import java.util.UUID;
 import lombok.Getter;
 
 @Getter
-public class InActiveDeviceFinded implements Event {
+public class InActiveDeviceFinded implements ApplicationEvent {
 
     private final List<UUID> devices;
 
