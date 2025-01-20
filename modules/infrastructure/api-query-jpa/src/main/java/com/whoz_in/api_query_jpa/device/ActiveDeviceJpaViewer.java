f3e250df4fbede8f868239cd3731a97d92736167
rlajm1203 <rlajm1203@naver.com>
2025-01-20 21:33:26 +0900

    refactor(api-query-jpa): ActiveDevice 구조 변경에 따른 수정

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/ActiveDeviceJpaViewer.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/ActiveDeviceJpaViewer.java
@@ -1,6 +1,7 @@
 package com.whoz_in.api_query_jpa.device;
 
 import com.whoz_in.api_query_jpa.member.Member;
+import com.whoz_in.api_query_jpa.member.MemberConnectionInfoRepository;
 import com.whoz_in.api_query_jpa.member.MemberRepository;
 import com.whoz_in.main_api.query.device.application.active.view.ActiveDevice;
 import com.whoz_in.main_api.query.device.application.active.view.ActiveDeviceViewer;
@@ -16,45 +17,29 @@ import org.springframework.stereotype.Component;
 public class ActiveDeviceJpaViewer implements ActiveDeviceViewer {
 
     private final ActiveDeviceRepository activeDeviceRepository;
-    private final DeviceRepository deviceRepository;
-    private final MemberRepository memberRepository;
 
     @Override
     public Optional<ActiveDevice> findByDeviceId(String deviceId) {
         ActiveDeviceEntity activeDevice = activeDeviceRepository.findByDeviceId(UUID.fromString(deviceId)).orElse(null);
-        Member member = memberRepository.findByDeviceId(UUID.fromString(deviceId)).orElse(null);
 
-        if(member == null || activeDevice == null) return Optional.empty();
+        if(activeDevice == null) return Optional.empty();
 
-        return createOptionalActiveDevice(activeDevice, member);
+        return Optional.of(createOptionalActiveDevice(activeDevice));
     }
 
     @Override
     public List<ActiveDevice> findAll() {
         List<ActiveDeviceEntity> entities = activeDeviceRepository.findAll();
-        List<UUID> deviceIds = entities.stream().map(ActiveDeviceEntity::getDeviceId).toList();
-        List<Device> devices = deviceRepository.findAll();
-        List<Member> members = memberRepository.findByDeviceIds(deviceIds);
 
         return entities.stream()
-                .map(entity-> {
-                    Device device = devices.stream().filter(d -> d.getId().equals(entity.getDeviceId())).findFirst().orElse(null);
-                    if(device == null) return null;
-                    Member member = members.stream().filter(m -> m.getId().equals(device.getMemberId())).findFirst().orElse(null);
-                    if (member == null) return null;
-                    return createOptionalActiveDevice(entity, member).orElse(null);
-                })
-                .filter(Objects::nonNull)
+                .map(this::createOptionalActiveDevice)
                 .toList();
     }
 
-    private Optional<ActiveDevice> createOptionalActiveDevice(ActiveDeviceEntity entity, Member member){
-        return Optional.of(new ActiveDevice(
+    private ActiveDevice createOptionalActiveDevice(ActiveDeviceEntity entity){
+        return new ActiveDevice(
                 entity.getDeviceId(),
-                member.getId(),
                 entity.getConnectedTime(),
-                entity.getDisConnectedTime(),
-                entity.getDailyActiveTime(),
-                entity.isActive()));
+                entity.getDisConnectedTime());
     }
 }
