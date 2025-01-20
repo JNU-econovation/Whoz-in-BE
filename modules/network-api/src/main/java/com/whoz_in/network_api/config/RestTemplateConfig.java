54f556b8c303ec2db892ec1da4b4d8c2eede224f
coco3x <inferior3x@gmail.com>
2025-01-21 01:50:56 +0900

    fix: HttpTemplate timeout 설정

--- modules/network-api/src/main/java/com/whoz_in/network_api/config/RestTemplateConfig.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package com.whoz_in.network_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,9 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000); // 연결 타임아웃
+        factory.setReadTimeout(5000);    // 응답 대기 타임아웃
+        return new RestTemplate(factory);
     }
 }
