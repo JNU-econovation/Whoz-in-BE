33ebfc38a59f2354b06eb8c95ffa4133ad4954df
rlajm1203 <whdlas011203@naver.com>
2025-01-21 20:58:43 +0900

    refactor(main-api): InActiveDeviceFilter 로직 간결화

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/InActiveDeviceFilter.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/InActiveDeviceFilter.java
@@ -3,8 +3,6 @@ package com.whoz_in.main_api.shared.domain.device.active;
 import com.whoz_in.domain.device.DeviceRepository;
 import com.whoz_in.domain.device.model.Device;
 import com.whoz_in.domain.device.model.DeviceId;
-import com.whoz_in.domain.device.model.DeviceInfo;
-import com.whoz_in.domain.device.model.MacAddress;
 import com.whoz_in.domain.network_log.MonitorLog;
 import com.whoz_in.domain.network_log.MonitorLogRepository;
 import com.whoz_in.main_api.query.device.application.active.view.ActiveDevice;
@@ -16,8 +14,6 @@ import com.whoz_in.main_api.shared.event.Events;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -49,18 +45,13 @@ public class InActiveDeviceFilter extends DeviceFilter{
 
         if(!activeDevices.isEmpty()) {
             // DeviceFilter 는 무조건, 후즈인에 등록된 Device 만을 넘겨야 한다.
-            List<UUID> monitorLogDeviceIds = deviceRepository.findByMacs(macs).stream()
-                    .map(Device::getId)
-                    .map(DeviceId::id)
-                    .toList();
+            List<UUID> monitorLogDeviceIds = getDeviceInMonitorLog(macs);
 
-            List<DeviceId> notInMonitorLog = activeDevices.stream().map(ActiveDevice::deviceId)
-                    .filter(deviceId -> !monitorLogDeviceIds.contains(deviceId))
-                    .map(DeviceId::new)
-                    .toList();
+            List<UUID> notInMonitorLog = getDeviceNotInMonitorLog(monitorLogDeviceIds, activeDevices);
 
-            return deviceRepository.findByDeviceIds(notInMonitorLog);
+            return findDevices(notInMonitorLog);
         }
+
         log.info("[InActiveDeviceFilter] 처리할 정보 없음");
         return List.of();
     }
@@ -71,8 +62,7 @@ public class InActiveDeviceFilter extends DeviceFilter{
         UUID ownerId = device.getMemberId().id();
 
         ActiveDevice activeDevice = activeDeviceViewer.getByDeviceId(deviceId.toString());
-        MemberConnectionInfo connectionInfo = memberViewer.findConnectionInfo(ownerId.toString())
-                .orElse(null);
+        MemberConnectionInfo connectionInfo = memberViewer.findConnectionInfo(ownerId.toString()).orElse(null);
 
         // 이미 inActive 상태인 기기의 경우 이벤트에서 제외
         if(connectionInfo==null || !connectionInfo.isActive()) return false;
@@ -83,7 +73,6 @@ public class InActiveDeviceFilter extends DeviceFilter{
         Duration term = Duration.between(activeDeviceActiveTime, now).abs();
 
         // 로그 발생 시간과의 차이가 기준치보다 클 경우 InActive
-
         if(term.compareTo(MEASURE) > 0){
             log.info("[InActiveDeviceFilter] InActive 전환 {}", deviceId);
             return true;
@@ -100,5 +89,23 @@ public class InActiveDeviceFilter extends DeviceFilter{
                 .toList();
 
         Events.raise(new InActiveDeviceFinded(deviceIds));
+    }
+
+    private List<UUID> getDeviceInMonitorLog(Set<String> macs) {
+        return deviceRepository.findByMacs(macs).stream()
+                .map(Device::getId)
+                .map(DeviceId::id)
+                .toList();
+    }
+
+    private List<UUID> getDeviceNotInMonitorLog(List<UUID> monitorLogDeviceIds, List<ActiveDevice> activeDevices) {
+        return activeDevices.stream().map(ActiveDevice::deviceId)
+                .filter(deviceId -> !monitorLogDeviceIds.contains(deviceId))
+                .toList();
+
+    }
+
+    private List<Device> findDevices(List<UUID> deviceIds){
+        return deviceRepository.findByDeviceIds(deviceIds.stream().map(DeviceId::new).toList());
     }
 }
