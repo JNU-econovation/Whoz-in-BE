cd472807de60b74a8ed668c008ba1e2c92479cf5
rlajm1203 <whdlas011203@naver.com>
2025-01-21 12:31:17 +0900

    feat(main-api, api-query-jpa): DeviceCount View 구현 및 1개 이상의 기기를 등록했는지 검증 로직 추가

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/device/application/DeviceCount.java
@@ -0,0 +1,8 @@
+package com.whoz_in.main_api.query.device.application;
+
+import com.whoz_in.main_api.query.shared.application.View;
+
+public record DeviceCount(
+        int value
+) implements View {
+}
