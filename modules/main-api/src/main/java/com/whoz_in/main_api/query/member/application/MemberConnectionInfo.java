94888f6930945d5c75a83133cf625f94d9c24836
rlajm1203 <whdlas011203@naver.com>
2025-01-22 02:26:59 +0900

    refactor(main-api): @Nullable 어노테이션 추가

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/MemberConnectionInfo.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/MemberConnectionInfo.java
@@ -1,13 +1,14 @@
 package com.whoz_in.main_api.query.member.application;
 
+import com.whoz_in.domain.shared.Nullable;
 import com.whoz_in.main_api.query.shared.application.View;
 import java.time.Duration;
 import java.util.UUID;
 
 public record MemberConnectionInfo(
         UUID memberId,
-        Duration dailyTime,
-        Duration totalTime,
+        @Nullable Duration dailyTime,
+        @Nullable Duration totalTime,
         boolean isActive
     ) implements View {
 }
