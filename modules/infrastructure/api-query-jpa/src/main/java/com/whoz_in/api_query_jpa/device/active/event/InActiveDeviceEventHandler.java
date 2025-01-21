a32c83a811bc5128071deabb61f1367bfc98334a
rlajm1203 <whdlas011203@naver.com>
2025-01-21 22:52:54 +0900

    refactor(api-query-jpa): InActiveDeviceEventHandler 로직 수정
    - 로직 간결화

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/active/event/InActiveDeviceEventHandler.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/active/event/InActiveDeviceEventHandler.java
@@ -2,6 +2,7 @@ package com.whoz_in.api_query_jpa.device.active.event;
 
 import com.whoz_in.api_query_jpa.device.active.ActiveDeviceEntity;
 import com.whoz_in.api_query_jpa.device.active.ActiveDeviceRepository;
+import com.whoz_in.api_query_jpa.shared.util.ActiveTimeUpdateWriter;
 import com.whoz_in.main_api.shared.domain.device.active.event.InActiveDeviceFinded;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class InActiveDeviceEventHandler {
 
     private final ActiveDeviceRepository activeDeviceRepository;
+    private final ActiveTimeUpdateWriter activeTimeUpdateWriter;
 
     // ActiveDeviceEvent 핸들러에서 데이터를 수정할 수 있으므로 격리 수준을 serializable 로 설정
     @Transactional(isolation = Isolation.SERIALIZABLE)
@@ -25,13 +27,28 @@ public class InActiveDeviceEventHandler {
         // 찾은 InActiveDevice 들을 처리하는 로직
         // Event 에 담기는 Device 의 ID 들은, 반드시 후즈인에 등록된 디바이들이다.
         List<UUID> devices = event.getDevices();
-        List<ActiveDeviceEntity> entities = activeDeviceRepository.findAll();
+        List<ActiveDeviceEntity> inActives = activeDeviceRepository.findByDeviceIds(devices);
 
-        List<ActiveDeviceEntity> inActiveDevices =entities.stream()
-                .peek(activeDevice -> activeDevice.disConnect(LocalDateTime.now()))
-                .toList();
+        // disConnect 시간 삽입
+        disConnect(inActives);
+        // 저장
+        save(inActives);
+        // DailyTime 업데이트
+        updateDailyTime(inActives);
+    }
 
-        activeDeviceRepository.saveAll(inActiveDevices); // TODO: 변경 감지 적용
+    private void disConnect(List<ActiveDeviceEntity> entities){
+        LocalDateTime now = LocalDateTime.now();
+        entities.forEach(activeDevice -> activeDevice.disConnect(now));
+    }
+
+    private void save(List<ActiveDeviceEntity> entities){
+        activeDeviceRepository.saveAll(entities);  // TODO: 변경 감지 적용
+    }
+
+    private void updateDailyTime(List<ActiveDeviceEntity> activeDevices){
+        List<UUID> deviceIds = activeDevices.stream().map(ActiveDeviceEntity::getDeviceId).toList();
+        deviceIds.forEach(activeTimeUpdateWriter::updateDailyTime);
     }
 
 }
