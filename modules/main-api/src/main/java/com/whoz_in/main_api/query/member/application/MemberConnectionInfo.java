dbde6697820f1d26ad39a7b784c4690a07994367
rlajm1203 <whdlas011203@naver.com>
2025-01-22 02:29:04 +0900

    refactor(main-api): getter 메소드 재정의

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/MemberConnectionInfo.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/MemberConnectionInfo.java
@@ -3,6 +3,7 @@ package com.whoz_in.main_api.query.member.application;
 import com.whoz_in.domain.shared.Nullable;
 import com.whoz_in.main_api.query.shared.application.View;
 import java.time.Duration;
+import java.util.Objects;
 import java.util.UUID;
 
 public record MemberConnectionInfo(
@@ -11,4 +12,15 @@ public record MemberConnectionInfo(
         @Nullable Duration totalTime,
         boolean isActive
     ) implements View {
+
+    public Duration totalTime(){
+        if(Objects.isNull(totalTime)) return Duration.ZERO;
+        return totalTime;
+    }
+    
+    public Duration dailyTime(){
+        if(Objects.isNull(dailyTime)) return Duration.ZERO;
+        return dailyTime;
+    }
+
 }
