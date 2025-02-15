2885d85369b4f1b40865688900281bf48fb8e6b0
coco3x <inferior3x@gmail.com>
2025-02-15 21:26:41 +0900

    chore(main-api): 도커 수정

--- modules/main-api/docker/build-and-push.sh
+++ modules/main-api/docker/build-and-push.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# 운영 서버가 느리기 때문에, 로컬에서 jar 빌드하고 도커 이미지로 빌드해서 도커 허브에 푸시할 수 있도록 만든 스크립트입니다.
+# 이 디렉토리에 .env.docker-credentials 파일 만들고 아래에서 요구하는 도커 아이디와 비밀번호 설정해주면 됩니다.
+
 set -e
 cd "$(dirname "$0")"
 source .env.docker-credentials
