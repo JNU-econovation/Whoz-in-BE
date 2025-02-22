c9546d502a6379474df590ed15febc38d13ab3a9
coco3x <inferior3x@gmail.com>
2025-02-23 03:52:43 +0900

    fix: 서버 jar에 변경이 없어도 컨테이너 재실행

--- modules/main-api/docker/build-and-deploy.sh
+++ modules/main-api/docker/build-and-deploy.sh
@@ -16,5 +16,5 @@ cd -
 docker container prune -f
 docker image prune -f
 # main-api 이외엔 재실행되지 않음
-# main-api 변경 사항이 없으면 재실행되지 않음
-docker compose up -d main-api --build
+# 빌드한 main-api가 변경 사항이 없어도 무조건 재실행 (환경 변수 파일을 변경해도 jar에 포함되지 않기 때문에)
+docker compose up -d --build --force-recreate main-api
