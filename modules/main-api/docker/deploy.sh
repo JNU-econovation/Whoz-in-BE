576ce8dfac2ce756c43331a3908d62d86ba701c4
coco3x <inferior3x@gmail.com>
2025-02-23 18:19:39 +0900

    chore: 배포 스크립트 분할

---
+++ modules/main-api/docker/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"
+
+docker container prune -f
+docker image prune -f
+# main-api 이외엔 재실행되지 않음
+# 빌드한 main-api가 변경 사항이 없어도 무조건 재실행 (환경 변수 파일을 변경해도 jar에 포함되지 않기 때문에)
+docker compose up -d --build --force-recreate main-api
