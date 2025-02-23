862cc79128abc1335d02481506297f636150a166
coco3x <inferior3x@gmail.com>
2025-02-23 23:10:07 +0900

    fix(main-api): 예외 처리 핸들러 순위 지정

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/presentation/WhozinExceptionHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/presentation/WhozinExceptionHandler.java
@@ -4,13 +4,16 @@ import com.whoz_in.domain.shared.BusinessException;
 import com.whoz_in.main_api.shared.application.ApplicationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 // 후즈인에서 정의한 예외를 처리한다.
+
 @Slf4j
+@Order(1)
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class WhozinExceptionHandler {
