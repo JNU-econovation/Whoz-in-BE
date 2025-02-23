6de76828026c0e9732a38f123afc751b62764612
coco3x <inferior3x@gmail.com>
2025-02-23 23:10:07 +0900

    fix(main-api): 예외 처리 핸들러 순위 지정

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/presentation/ExternalExceptionHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/presentation/ExternalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.whoz_in.main_api.shared.presentation;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingRequestCookieException;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 //라이브러리나 프레임워크에서 정의된 예외를 처리한다.
 @Slf4j
+@Order(0)
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ExternalExceptionHandler {
