d5859289eee5bc8e4b0ed5737f0749b281cbda13
coco3x <inferior3x@gmail.com>
2025-02-28 21:33:08 +0900

    refactor(main-api): 예외 로깅 리팩토링

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/ExceptionLoggingFilter.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/ExceptionLoggingFilter.java
@@ -1,11 +1,13 @@
 package com.whoz_in.main_api.config;
 
+import com.whoz_in.main_api.shared.presentation.ExceptionLogger;
 import com.whoz_in.main_api.shared.presentation.HttpRequestInfoExtractor;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
@@ -13,24 +15,20 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+//
 @Slf4j
 @Component
 @RequiredArgsConstructor
 @Order(Ordered.HIGHEST_PRECEDENCE+1) // 캐싱 먼저 수행
 public class ExceptionLoggingFilter extends OncePerRequestFilter {
-    private final HttpRequestInfoExtractor httpRequestInfoExtractor;
+    private final ExceptionLogger exceptionLogger;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
         } catch (Exception e) {
-            logRequestDetails(request, e);
+            exceptionLogger.log("필터에서 예외 발생!", request, e);
             throw e;
         }
-    }
-
-    private void logRequestDetails(HttpServletRequest request, Exception e) {
-        log.error("필터에서 예외가 발생했습니다!\n-예외 정보-\n클래스: {}\n메세지: {}\n-요청 정보-\n{}\n-스택 트레이스-",
-                e.getClass().getName(), e.getMessage(), httpRequestInfoExtractor.extractInfoFrom(request), e);
     }
 }
