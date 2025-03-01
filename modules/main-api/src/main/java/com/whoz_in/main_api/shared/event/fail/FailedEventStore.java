74db76c6ecf5410e575f1e88c84210900ef2e1b3
rlajm1203 <whdlas011203@naver.com>
2025-03-02 03:22:20 +0900

    refactor(main-api): 메모리 누수 방지

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/event/fail/FailedEventStore.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/event/fail/FailedEventStore.java
@@ -5,16 +5,19 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 // 다른 모듈에서 사용할 수 있도록 static 으로 구성
 public class FailedEventStore {
-
-    // TODO: 어떤 자료구조를 써야 할지?
-    private static final Queue<ApplicationEvent> eventStore = new ConcurrentLinkedDeque<>();
+    private static final Queue<ApplicationEvent> eventStore = new ConcurrentLinkedQueue<>();
+    private static final int MAX_SIZE = 10000; // 메모리 누수 방지용 최대 크기
 
     public static void add(ApplicationEvent event) {
-        eventStore.add(event);
+        if (eventStore.size() < MAX_SIZE) {
+            eventStore.add(event);
+        } else {
+            eventStore.poll();
+        }
     }
 
     public static List<ApplicationEvent> poll() {
@@ -22,5 +25,4 @@ public class FailedEventStore {
         eventStore.clear();
         return events;
     }
-
 }
