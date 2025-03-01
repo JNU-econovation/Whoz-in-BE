1e74271fa1b74774aa81d70869bc8821a04fb2f5
rlajm1203 <whdlas011203@naver.com>
2025-03-02 03:16:37 +0900

    refactor(main-api): 이벤트 저장소 자료구조 변경
    - ConcurrentLinkedDeque

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/event/fail/FailedEventStore.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/event/fail/FailedEventStore.java
@@ -2,21 +2,25 @@ package com.whoz_in.main_api.shared.event.fail;
 
 import com.whoz_in.main_api.shared.event.ApplicationEvent;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 // 다른 모듈에서 사용할 수 있도록 static 으로 구성
 public class FailedEventStore {
 
     // TODO: 어떤 자료구조를 써야 할지?
-    private static final Queue<ApplicationEvent> eventStore = new ArrayDeque<>();
+    private static final Queue<ApplicationEvent> eventStore = new ConcurrentLinkedDeque<>();
 
     public static void add(ApplicationEvent event) {
         eventStore.add(event);
     }
 
     public static List<ApplicationEvent> poll() {
-        return eventStore.stream().toList();
+        List<ApplicationEvent> events = new ArrayList<>(eventStore);
+        eventStore.clear();
+        return events;
     }
 
 }
