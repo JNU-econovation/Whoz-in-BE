6c0fa5b0989548c4af1814ab7eae89ee1b87061f
rlajm1203 <whdlas011203@naver.com>
2025-03-02 03:31:10 +0900

    refactor(main-api): 시도할 이벤트가 있을 경우에만 실행

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/event/fail/FailedEventRetryer.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/event/fail/FailedEventRetryer.java
@@ -1,7 +1,9 @@
 package com.whoz_in.main_api.shared.event.fail;
 
 import com.whoz_in.domain.shared.event.EventBus;
+import com.whoz_in.main_api.shared.event.ApplicationEvent;
 import com.whoz_in.main_api.shared.event.Events;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
@@ -17,10 +19,14 @@ public class FailedEventRetryer {
 
     @Scheduled(fixedRate = 1000 * 30)
     public void retry() {
-        FailedEventStore.poll().forEach(event -> {
-            log.info("[이벤트 재시도] {} ", event.getClass().getName());
-            eventPublisher.publishEvent(event);
-        });
+        List<ApplicationEvent> events = FailedEventStore.poll();
+
+        if (!events.isEmpty()) {
+            events.forEach(event -> {
+                log.info("[이벤트 재시도] {} ", event.getClass().getName());
+                eventPublisher.publishEvent(event);
+            });
+        }
     }
 
 }
