efca4bff4ac4f47735055814b9678bb5d43e51e7
rlajm1203 <whdlas011203@naver.com>
2025-03-02 01:35:33 +0900

    refactor(main-api,api-query-jpa): 메소드 추가 및 코드 리팩터링
    - List<MemberInfo> 메소드 추가
    - List<MemberConnectionInfo> 메소드 추가

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberJpaViewer.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberJpaViewer.java
@@ -6,6 +6,7 @@ import com.whoz_in.main_api.query.member.application.view.MemberDetailInfo;
 import com.whoz_in.main_api.query.member.application.view.MemberConnectionInfo;
 import com.whoz_in.main_api.query.member.application.view.MemberInfo;
 import com.whoz_in.main_api.query.member.application.MemberViewer;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -17,33 +18,54 @@ public class MemberJpaViewer implements MemberViewer {
     private final MemberRepository repository;
     private final ActiveDeviceRepository activeDeviceRepository;
     private final MemberConnectionInfoRepository connectionInfoRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public Optional<MemberAuthInfo> findAuthInfoByLoginId(String loginId) {
         return repository.findByLoginId(loginId)
-                .map(member -> new MemberAuthInfo(member.getId(), member.getLoginId(), member.getEncodedPassword()));
+                .map(this::toMemberAuthInfo);
     }
 
     @Override
     public Optional<MemberInfo> findNameByMemberId(String memberId) {
         return repository.findById(UUID.fromString(memberId))
-                .map(member -> new MemberInfo(member.getGeneration(), member.getName()));
+                .map(this::toMemberInfo);
     }
 
     @Override
     public Optional<MemberConnectionInfo> findConnectionInfo(String memberId) {
         return connectionInfoRepository.findByMemberId(UUID.fromString(memberId))
-                .map(connectInfo ->
-                        new MemberConnectionInfo(
-                        connectInfo.getMemberId(),
-                        connectInfo.getDailyTime(),
-                        connectInfo.getTotalTime(),
-                        connectInfo.isActive())
-                );
+                .map(this::toMemberConnectionInfo);
     }
 
     @Override
     public Optional<MemberDetailInfo> findDetailByMemberId(UUID memberId) {
-        return repository.findById(memberId).map(member -> new MemberDetailInfo(memberId, member.getName(), member.getGeneration(), member.getPosition(), member.getStatusMessage()));
+        return repository.findById(memberId).map(this::toMemberDetailInfo);
+    }
+
+    @Override
+    public List<MemberConnectionInfo> findAllMemberConnectionInfo() {
+        return connectionInfoRepository.findAll().stream().map(this::toMemberConnectionInfo).toList();
+    }
+
+    @Override
+    public List<MemberInfo> findAllMemberInfo() {
+        return memberRepository.findAll().stream().map(this::toMemberInfo).toList();
+    }
+
+    private MemberConnectionInfo toMemberConnectionInfo(com.whoz_in.api_query_jpa.member.MemberConnectionInfo entity){
+        return new MemberConnectionInfo(entity.getMemberId(), entity.getDailyTime(), entity.getTotalTime(), entity.isActive());
+    }
+
+    private MemberInfo toMemberInfo(com.whoz_in.api_query_jpa.member.Member entity){
+        return new MemberInfo(entity.getGeneration(), entity.getName());
+    }
+
+    private MemberAuthInfo toMemberAuthInfo(com.whoz_in.api_query_jpa.member.Member entity){
+        return new MemberAuthInfo(entity.getId(), entity.getLoginId(), entity.getEncodedPassword());
+    }
+
+    private MemberDetailInfo toMemberDetailInfo(com.whoz_in.api_query_jpa.member.Member entity){
+        return new MemberDetailInfo(entity.getId(), entity.getName(), entity.getGeneration(), entity.getPosition(), entity.getStatusMessage());
     }
 }
