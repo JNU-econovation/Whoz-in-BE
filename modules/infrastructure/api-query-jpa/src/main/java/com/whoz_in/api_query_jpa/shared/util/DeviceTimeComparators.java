26b4c3f21a0de4a1c69d88117ef0a13e01b48f12
rlajm1203 <whdlas011203@naver.com>
2025-03-01 22:19:08 +0900

    feat(api-query-jpa): continuousTimeComparator 추가
    - 연속 접속 시간을 기준으로 하는 비교기

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/shared/util/DeviceTimeComparators.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/shared/util/DeviceTimeComparators.java
@@ -1,6 +1,7 @@
 package com.whoz_in.api_query_jpa.shared.util;
 
 import com.whoz_in.api_query_jpa.device.active.ActiveDeviceEntity;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 
@@ -14,6 +15,16 @@ public class DeviceTimeComparators {
             if(ad1DisconnectedAt.isEqual(ad2DisconnectedAt)) return 0;
             if(ad1DisconnectedAt.isAfter(ad2DisconnectedAt)) return 1;
             return -1;
+        };
+    }
+
+    public static Comparator<ActiveDeviceEntity> continuousTimeComparator() {
+        return (activeDevice1, activeDevice2) -> {
+            Duration ad1continuousTime = Duration.between(activeDevice1.getConnectedAt(),
+                    activeDevice1.getDisConnectedAt()).abs();
+            Duration ad2continuousTime = Duration.between(activeDevice2.getConnectedAt(),
+                    activeDevice2.getDisConnectedAt()).abs();
+            return ad1continuousTime.compareTo(ad2continuousTime);
         };
     }
 
