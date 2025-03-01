8b8c64f3ce021b10d5d917e7659cdf12fcb34cff
rlajm1203 <whdlas011203@naver.com>
2025-03-02 00:14:12 +0900

    refactor(main-api): 기기 삭제 시, 도메인 이벤트 발생

--- modules/main-api/src/main/java/com/whoz_in/main_api/command/device/application/DeviceRemoveHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/command/device/application/DeviceRemoveHandler.java
@@ -1,10 +1,14 @@
 package com.whoz_in.main_api.command.device.application;
 
 import com.whoz_in.domain.device.DeviceRepository;
+import com.whoz_in.domain.device.event.DeviceDeleted;
+import com.whoz_in.domain.device.model.Device;
 import com.whoz_in.domain.member.service.MemberFinderService;
+import com.whoz_in.domain.shared.event.EventBus;
 import com.whoz_in.main_api.command.shared.application.CommandHandler;
 import com.whoz_in.main_api.shared.application.Handler;
 import com.whoz_in.main_api.shared.utils.RequesterInfo;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,12 +18,14 @@ public class DeviceRemoveHandler implements CommandHandler<DeviceRemove, Void> {
     private final RequesterInfo requesterInfo;
     private final MemberFinderService memberFinderService;
     private final DeviceRepository deviceRepository;
+    private final EventBus eventBus;
 
     @Transactional
     @Override
     public Void handle(DeviceRemove command) {
         memberFinderService.mustExist(requesterInfo.getMemberId());
         deviceRepository.delete(command.getDeviceId());
+        eventBus.publish(List.of(new DeviceDeleted(command.getDeviceId().id())));
         return null;
     }
 }
