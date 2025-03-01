558ea26ca438ff8456bc0a48d2452be3abb4efa5
rlajm1203 <whdlas011203@naver.com>
2025-03-01 23:27:54 +0900

    feat(api-query-jpa): InActiveMemberConnectionManager 정의

---
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/shared/util/InActiveMemberConnectionManager.java
@@ -0,0 +1,47 @@
+package com.whoz_in.api_query_jpa.shared.util;
+
+import com.whoz_in.api_query_jpa.device.active.ActiveDeviceEntity;
+import com.whoz_in.api_query_jpa.member.Member;
+import com.whoz_in.api_query_jpa.member.MemberConnectionInfo;
+import com.whoz_in.api_query_jpa.member.MemberConnectionInfoRepository;
+import com.whoz_in.api_query_jpa.member.MemberRepository;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InActiveMemberConnectionManager {
+
+    private final ActiveTimeUpdateWriter activeTimeUpdateWriter;
+    private final MemberConnectionInfoRepository connectionInfoRepository;
+    private final MemberRepository memberRepository;
+
+    public void processInActiveDevices(List<ActiveDeviceEntity> entities){
+        // 기기가 접속이 종료되는 마지막 기기인지 판단하기
+        updateDailyTime(entities);
+        inActiveOn(entities);
+    }
+
+    private void updateDailyTime(List<ActiveDeviceEntity> activeDevices){
+        List<UUID> deviceIds = activeDevices.stream().map(ActiveDeviceEntity::getDeviceId).toList();
+        deviceIds.forEach(activeTimeUpdateWriter::updateDailyTime);
+    }
+
+    private void inActiveOn(List<ActiveDeviceEntity> entities) {
+        List<MemberConnectionInfo> connectionInfos = entities.stream()
+                .map(ActiveDeviceEntity::getDeviceId)
+                .map(memberRepository::findByDeviceId)
+                .map(opt -> opt.orElse(null)).filter(Objects::nonNull)
+                .map(Member::getId)
+                .map(connectionInfoRepository::findByMemberId)
+                .map(opt -> opt.orElse(null)).filter(Objects::nonNull)
+                .peek(MemberConnectionInfo::inActiveOn)
+                .toList();
+
+        connectionInfoRepository.saveAll(connectionInfos);
+    }
+
+}
