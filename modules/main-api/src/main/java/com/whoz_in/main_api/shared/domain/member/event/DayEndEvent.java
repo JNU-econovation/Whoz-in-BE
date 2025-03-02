3269941008cdc523dd8e0fdd2f9ea92a1feaee91
rlajm1203 <whdlas011203@naver.com>
2025-03-02 23:19:25 +0900

    refactor(main-api): DayEndEvent 빈 등록 및 스케줄링 활성화

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/member/event/DayEndEvent.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/member/event/DayEndEvent.java
@@ -2,14 +2,19 @@ package com.whoz_in.main_api.shared.domain.member.event;
 
 import com.whoz_in.main_api.shared.event.ApplicationEvent;
 import com.whoz_in.main_api.shared.event.Events;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-public record DayEndEvent() implements ApplicationEvent {
+@Component
+@RequiredArgsConstructor
+public class DayEndEvent implements ApplicationEvent {
 
     // 매일 자정에 이벤트 발생
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 * 23 * * *")
     public void raiseDayEndEvent(){
-        Events.raise(new DayEndEvent());
+        Events.raise(this);
+        System.out.println("자정 이벤트 발생");
     }
 
 }
