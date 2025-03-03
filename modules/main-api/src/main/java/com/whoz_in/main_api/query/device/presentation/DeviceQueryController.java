532665f798e65cbb79ef4a325c1302953f533bf1
rlajm1203 <whdlas011203@naver.com>
2025-03-04 04:05:04 +0900

    feat(main-api): DeviceQueryController 스웨거 적용

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/device/presentation/DeviceQueryController.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/device/presentation/DeviceQueryController.java
@@ -9,6 +9,8 @@ import com.whoz_in.main_api.query.shared.presentation.QueryController;
 import com.whoz_in.main_api.shared.presentation.CrudResponseCode;
 import com.whoz_in.main_api.shared.presentation.ResponseEntityGenerator;
 import com.whoz_in.main_api.shared.presentation.SuccessBody;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name="기기", description = "기기 조회에 관한 API")
 public class DeviceQueryController extends QueryController {
 
     protected DeviceQueryController(QueryBus queryBus) {
         super(queryBus);
     }
 
+    @Operation(
+            summary = "기기 상태 조회",
+            description = "기기 Id, 기기 mac, 기기 이름, ssid를 조회합니다."
+    )
     @GetMapping("/devices")
     public ResponseEntity<SuccessBody<DevicesStatus>> getDeviceStatus(){
         return ResponseEntityGenerator.success(ask(new DevicesStatusGet()), CrudResponseCode.READ);
     }
 
 
+    @Operation(
+            summary = "기기 상태 조회",
+            description = "기기 등록을 위한 임시적인 기기들의 상태 정보를 조회합니다."
+    )
     @GetMapping("/device/info-status")
     public ResponseEntity<SuccessBody<TempDeviceInfosStatus>> getTempDeviceInfosStatus(@RequestParam String room, @RequestParam String ip){
         return ResponseEntityGenerator.success(
