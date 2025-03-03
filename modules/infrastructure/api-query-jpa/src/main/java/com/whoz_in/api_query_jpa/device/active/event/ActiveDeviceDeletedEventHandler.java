647c0c6cbfba3d882863012ef6e893ee1044d7b0
rlajm1203 <whdlas011203@naver.com>
2025-03-04 01:41:47 +0900

    refactor(api-query-jpa): 이벤트 핸들러 메소드 이름 변경 및 로직 수정

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/active/event/ActiveDeviceDeletedEventHandler.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/active/event/ActiveDeviceDeletedEventHandler.java
@@ -1,6 +1,7 @@
 package com.whoz_in.api_query_jpa.device.active.event;
 
 import com.whoz_in.api_query_jpa.device.active.ActiveDeviceRepository;
+import com.whoz_in.api_query_jpa.shared.service.DeviceConnectionService;
 import com.whoz_in.api_query_jpa.shared.util.ActiveTimeUpdateWriter;
 import com.whoz_in.main_api.shared.domain.device.active.event.DeviceDeletedEvent;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +17,15 @@ public class ActiveDeviceDeletedEventHandler {
 
     private final ActiveDeviceRepository activeDeviceRepository;
     private final ActiveTimeUpdateWriter activeTimeUpdateWriter;
+    private final DeviceConnectionService deviceConnectionService;
 
     @Transactional(isolation = Isolation.SERIALIZABLE, propagation = Propagation.REQUIRED)
     @EventListener(DeviceDeletedEvent.class)
     public void handle(DeviceDeletedEvent event) {
-            activeTimeUpdateWriter.updateDailyTime(event.deviceId());
-            activeDeviceRepository.deleteById(event.deviceId());
+        // Device를 inActive 처리, DailyTime 까지 자동으로 업데이트
+        deviceConnectionService.disconnectDevice(event.deviceId());
+//        activeTimeUpdateWriter.updateDailyTime(event.deviceId());
+        activeDeviceRepository.deleteById(event.deviceId());
     }
 
 }
