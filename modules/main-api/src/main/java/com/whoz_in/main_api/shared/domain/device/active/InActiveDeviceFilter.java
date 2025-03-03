82a3479b84999a646c75f702f41ebc60b7af1111
rlajm1203 <whdlas011203@naver.com>
2025-03-04 01:51:53 +0900

    refactor(main-api): DeviceFilter 로그 제거

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/InActiveDeviceFilter.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/domain/device/active/InActiveDeviceFilter.java
@@ -50,8 +50,6 @@ public class InActiveDeviceFilter extends DeviceFilter{
 
             List<UUID> notInMonitorLog = getDeviceNotInMonitorLog(monitorLogDeviceIds, activeDevices);
 
-            log.info("[InActiveDeviceFilter] InActive Device find : {}", notInMonitorLog.size());
-
             return findDevices(notInMonitorLog);
         }
 
@@ -80,7 +78,6 @@ public class InActiveDeviceFilter extends DeviceFilter{
 
         // 로그 발생 시간과의 차이가 기준치보다 클 경우 InActive
         if(term.compareTo(MEASURE) > 0){
-            log.info("[InActiveDeviceFilter] InActive 전환 {}", deviceId);
             return true;
         }
         return false;
