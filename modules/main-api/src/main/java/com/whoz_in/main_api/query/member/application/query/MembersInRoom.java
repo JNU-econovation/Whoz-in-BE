3f2e27a15421fe06f62deaca7a78a9e4b7025477
rlajm1203 <whdlas011203@naver.com>
2025-03-04 21:04:57 +0900

    refactor(main-api): 동방 현황 조회 API ParameterObject 로 변경

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/query/MembersInRoom.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/query/MembersInRoom.java
@@ -1,10 +1,21 @@
 package com.whoz_in.main_api.query.member.application.query;
 
 import com.whoz_in.main_api.query.shared.application.Query;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public record MembersInRoom(
-        int page,
-        int size,
-        String sortType
-) implements Query {
+public record MembersInRoom(int page, int size, String sortType, String status) implements Query {
+
+    public MembersInRoom(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortType") String sortType,
+            @RequestParam("status") String status
+    ) {
+        this.page = page;
+        this.size = size;
+        this.sortType = sortType;
+        this.status = status;
+    }
+
+
 }
