a01956f0d23a7ce7ff5b4fb65716da9292f29af4
rlajm1203 <whdlas011203@naver.com>
2025-03-04 20:30:21 +0900

    feat(main-api,api-query-jpa): countActiveMember 메소드 구현

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberConnectionInfoRepository.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberConnectionInfoRepository.java
@@ -3,9 +3,13 @@ package com.whoz_in.api_query_jpa.member;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberConnectionInfoRepository extends JpaRepository<MemberConnectionInfo, UUID> {
 
     Optional<MemberConnectionInfo> findByMemberId(UUID memberId);
+
+    @Query("SELECT connectionInfo FROM MemberConnectionInfo connectionInfo WHERE connectionInfo.isActive = true")
+    Long countActiveMember();
 
 }
