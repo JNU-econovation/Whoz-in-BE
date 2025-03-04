362aaa98c012c85a492ef45afecd695a35a4a4e5
coco3x <inferior3x@gmail.com>
2025-03-04 20:40:29 +0900

    update(main-api): 에러 코드 수정

--- modules/domain/src/main/java/com/whoz_in/domain/member/exception/NotAuthMemberException.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/exception/NotAuthMemberException.java
@@ -5,6 +5,6 @@ import com.whoz_in.domain.shared.BusinessException;
 public class NotAuthMemberException extends BusinessException {
     public static final NotAuthMemberException EXCEPTION = new NotAuthMemberException();
     private NotAuthMemberException() {
-        super("2006", "일반 로그인 정보가 없는 회원입니다.");
+        super("2022", "일반 로그인 정보가 없는 회원입니다.");
     }
 }
