3347c22455b14dde17a8c72c8d6e6cc211fbd157
rlajm1203 <whdlas011203@naver.com>
2025-03-04 21:11:36 +0900

    feat(main-api): MembersInRoom 정의
    - ParameterObject

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/query/MembersInRoom.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/query/MembersInRoom.java
@@ -11,10 +11,29 @@ public record MembersInRoom(int page, int size, String sortType, String status) 
             @RequestParam("sortType") String sortType,
             @RequestParam("status") String status
     ) {
+        validateParameter(page, size, sortType, status);
         this.page = page;
         this.size = size;
         this.sortType = sortType;
         this.status = status;
+    }
+
+    private void validateParameter(
+            int page,
+            int size,
+            String sortType,
+            String status
+    ){
+        if(page < 1 || size < 1)
+            throw new IllegalArgumentException("잘못된 파라미터 : page, size");
+
+        // TODO: 하드코딩 말고 다른 방법 적용하기
+        if(!status.equalsIgnoreCase("all") &&
+                !status.equalsIgnoreCase("active") &&
+                !status.equalsIgnoreCase("inactive")){
+            throw new IllegalArgumentException("잘못된 파라미터 : status");
+        }
+
     }
 
 
