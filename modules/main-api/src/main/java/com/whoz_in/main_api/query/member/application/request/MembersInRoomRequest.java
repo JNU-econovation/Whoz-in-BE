cd279d1234329af2efcfc9f3d33130a8887c5abb
rlajm1203 <whdlas011203@naver.com>
2025-03-04 22:52:20 +0900

    refactor(main-api): HttpRequest 클래스 와 Query 분리

---
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/request/MembersInRoomRequest.java
@@ -0,0 +1,43 @@
+package com.whoz_in.main_api.query.member.application.request;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+public record MembersInRoomRequest(
+        int page,
+        int size,
+        String sortType,
+        String status
+) {
+
+    public MembersInRoomRequest(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortType") String sortType,
+            @RequestParam("status") String status
+    ) {
+        validateParameter(page, size, sortType, status);
+        this.page = page;
+        this.size = size;
+        this.sortType = sortType;
+        this.status = status;
+    }
+
+    private void validateParameter(
+            int page,
+            int size,
+            String sortType,
+            String status
+    ){
+        if(page < 1 || size < 1)
+            throw new IllegalArgumentException("잘못된 파라미터 : page, size");
+
+        // TODO: 하드코딩 말고 다른 방법 적용하기
+        if(!status.equalsIgnoreCase("all") &&
+                !status.equalsIgnoreCase("active") &&
+                !status.equalsIgnoreCase("inactive")){
+            throw new IllegalArgumentException("잘못된 파라미터 : status");
+        }
+
+    }
+
+}
--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/presentation/docs/MemberQueryApi.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/presentation/docs/MemberQueryApi.java
@@ -1,6 +1,6 @@
 package com.whoz_in.main_api.query.member.presentation.docs;
 
-import com.whoz_in.main_api.query.member.application.query.MembersInRoom;
+import com.whoz_in.main_api.query.member.application.request.MembersInRoomRequest;
 import com.whoz_in.main_api.query.member.application.response.MembersInRoomResponse;
 import com.whoz_in.main_api.query.member.application.view.MemberDetailInfo;
 import com.whoz_in.main_api.shared.presentation.SuccessBody;
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "회원", description = "회원 Api")
 public interface MemberQueryApi {
@@ -17,7 +16,7 @@ public interface MemberQueryApi {
             summary = "동아리 방 현황 조회",
             description = "동아리 방에 누가 있는지 현황을 조회합니다."
     )
-    ResponseEntity<SuccessBody<MembersInRoomResponse>> getActiveDevices(@ParameterObject MembersInRoom query);
+    ResponseEntity<SuccessBody<MembersInRoomResponse>> getActiveDevices(@ParameterObject MembersInRoomRequest query);
 
 
     @Operation(
