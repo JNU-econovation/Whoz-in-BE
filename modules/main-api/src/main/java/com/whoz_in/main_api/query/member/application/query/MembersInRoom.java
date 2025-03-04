4fdd65c5bc02356cf68d6c058e9989da6febdc7c
rlajm1203 <whdlas011203@naver.com>
2025-03-04 22:52:20 +0900

    refactor(main-api): HttpRequest 클래스 와 Query 분리

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/query/MembersInRoom.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/query/MembersInRoom.java
@@ -1,40 +1,16 @@
 package com.whoz_in.main_api.query.member.application.query;
 
+import com.whoz_in.main_api.query.member.application.request.MembersInRoomRequest;
 import com.whoz_in.main_api.query.shared.application.Query;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public record MembersInRoom(int page, int size, String sortType, String status) implements Query {
 
-    public MembersInRoom(
-            @RequestParam("page") int page,
-            @RequestParam("size") int size,
-            @RequestParam("sortType") String sortType,
-            @RequestParam("status") String status
-    ) {
-        validateParameter(page, size, sortType, status);
-        this.page = page;
-        this.size = size;
-        this.sortType = sortType;
-        this.status = status;
+    public static MembersInRoom of(int page, int size, String sortType, String status) {
+        return new MembersInRoom(page, size, sortType, status);
     }
 
-    private void validateParameter(
-            int page,
-            int size,
-            String sortType,
-            String status
-    ){
-        if(page < 1 || size < 1)
-            throw new IllegalArgumentException("잘못된 파라미터 : page, size");
-
-        // TODO: 하드코딩 말고 다른 방법 적용하기
-        if(!status.equalsIgnoreCase("all") &&
-                !status.equalsIgnoreCase("active") &&
-                !status.equalsIgnoreCase("inactive")){
-            throw new IllegalArgumentException("잘못된 파라미터 : status");
-        }
-
+    public static MembersInRoom of(MembersInRoomRequest request) {
+        return new MembersInRoom(request.page(), request.size(), request.sortType(), request.status());
     }
-
 
 }
