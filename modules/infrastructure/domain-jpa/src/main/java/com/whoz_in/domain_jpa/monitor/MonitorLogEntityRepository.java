1dbc09130d71c55976689a4cc68160409b71e423
rlajm1203 <whdlas011203@naver.com>
2025-03-05 15:25:01 +0900

    refactor(domain-jpa): @Param 추가

--- modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
+++ modules/infrastructure/domain-jpa/src/main/java/com/whoz_in/domain_jpa/monitor/MonitorLogEntityRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MonitorLogEntityRepository extends JpaRepository<MonitorLogEntity, String> {
 
     @Query("SELECT m FROM MonitorLogEntity m WHERE m.updatedAt > :updatedAt ORDER BY m.updatedAt DESC")
-    List<MonitorLogEntity> findByUpdatedAtAfterOrderByUpdatedAtDesc(LocalDateTime updatedAt);
+    List<MonitorLogEntity> findByUpdatedAtAfterOrderByUpdatedAtDesc(@Param("updatedAt") LocalDateTime updatedAt);
 
     @Query("SELECT 1 FROM MonitorLogEntity m WHERE m.mac = :mac AND m.updatedAt > :time ORDER BY m.updatedAt DESC")
     boolean existsTopByMacOrderByUpdatedAtDescAfter(String mac, LocalDateTime time);
