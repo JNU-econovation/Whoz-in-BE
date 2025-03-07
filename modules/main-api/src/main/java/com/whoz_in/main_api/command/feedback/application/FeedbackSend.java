42bf3bc4faec9960038642a1fcdf9ca7581bcbff
westzeroright <kt8267@naver.com>
2025-03-07 21:04:10 +0900

    feat(main-api): 커맨드 객체에서 피드백 제목과 내용에 대한 검증을 수행함

--- modules/main-api/src/main/java/com/whoz_in/main_api/command/feedback/application/FeedbackSend.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/command/feedback/application/FeedbackSend.java
@@ -1,9 +1,23 @@
 package com.whoz_in.main_api.command.feedback.application;
 
+import com.whoz_in.domain.shared.Nullable;
+import com.whoz_in.main_api.command.feedback.exception.ContentLengthExceedException;
+import com.whoz_in.main_api.command.feedback.exception.TitleLengthExceededException;
 import com.whoz_in.main_api.command.shared.application.Command;
 
 public record FeedbackSend(
-        String title,
-        String content
+        @Nullable String title,
+        @Nullable String content
 ) implements Command {
+    public static final int MAX_LENGTH_TITLE = 30;
+    public static final int MAX_LENGTH_CONTENT = 500;
+
+    public FeedbackSend {
+        if (title.length() > MAX_LENGTH_TITLE) {
+            throw TitleLengthExceededException.EXCEPTION;
+        }
+        if (content.length() > MAX_LENGTH_CONTENT) {
+            throw ContentLengthExceedException.EXCEPTION;
+        }
+    }
 }
