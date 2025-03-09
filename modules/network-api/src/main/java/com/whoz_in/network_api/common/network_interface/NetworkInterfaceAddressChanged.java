71f09ce979686a2fd6896aad0f653d700f247c95
coco3x <inferior3x@gmail.com>
2025-03-09 21:23:48 +0900

    feat(network-api): 동적 cors allowed origin 수정

--- modules/network-api/src/main/java/com/whoz_in/network_api/common/network_interface/NetworkInterfaceAddressChanged.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/common/network_interface/NetworkInterfaceAddressChanged.java
@@ -3,16 +3,17 @@ package com.whoz_in.network_api.common.network_interface;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+// now가 네트워크에 연결되어있는 경우에만 발생한다. (monitor 모드는 발생하지 않는다는 뜻이기도 하다.)
 @Getter
 @RequiredArgsConstructor
 public class NetworkInterfaceAddressChanged {
         private final String interfaceName;
-        private final NetworkAddress pre;
-        private final NetworkAddress now;
+        private final NetworkInterface pre;
+        private final NetworkInterface now;
 
         public NetworkInterfaceAddressChanged(NetworkInterface pre, NetworkInterface now) {
-                this.interfaceName = pre.getName();
-                this.pre = pre.getNetworkAddress();
-                this.now = now.getNetworkAddress();
+                this.interfaceName = now.getName();
+                this.pre = pre;
+                this.now = now;
         }
 }
