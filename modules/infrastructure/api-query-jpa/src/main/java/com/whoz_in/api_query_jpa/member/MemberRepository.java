027986941819b9ece807ea2f679b5e624e9ce03c
Bellmin <71818636+rlajm1203@users.noreply.github.com>
2025-03-10 15:21:38 +0900

    [FIX] active member query 수정 (#259)
    * refactor(api-query-jpa): cross JOIN 으로 변경
    
    - 엔티티 상에서 연관관계가 매핑되지 않은 경우, cross JOIN 으로 변경
    
    * refactor(main-api): Transactional readOnly true 설정
    
    ---------
    
    Co-authored-by: rlajm1203 <whdlas011203@naver.com>

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberRepository.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/member/MemberRepository.java
@@ -21,12 +21,12 @@ public interface MemberRepository extends JpaRepository<Member, UUID> {
     @Query("SELECT m FROM Member m WHERE m.id IN (SELECT d.memberId FROM Device d WHERE d.id IN (:deviceIds))")
     List<Member> findByDeviceIds(@Param("deviceIds") List<UUID> deviceIds);
 
-    @Query("SELECT m FROM Member m JOIN MemberConnectionInfo mci WHERE m.id=mci.memberId ORDER BY mci.isActive desc")
+    @Query("SELECT m FROM Member m, MemberConnectionInfo mci WHERE m.id=mci.memberId ORDER BY mci.isActive desc")
     List<Member> findAllOrderByStatus();
 
     // TODO: 현재는 동방 현황 조회가 isActive, dailyTime, memberName 순으로 고정되게 정렬
     // TODO: 정렬 조건이 바뀌면, 나중에 동적으로 정렬할 수 있게 변환하기
-    @Query("SELECT m FROM Member m JOIN MemberConnectionInfo mci "
+    @Query("SELECT m FROM Member m, MemberConnectionInfo mci "
             + "WHERE m.id=mci.memberId AND mci.isActive=:isActive "
             + "ORDER BY mci.isActive desc , mci.dailyTime desc , m.name asc")
     List<Member> findByStatus(@Param("isActive") boolean isActive);
