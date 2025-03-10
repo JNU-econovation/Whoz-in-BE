c9e3918309d25f0367ccf6142988c3c3402b2539
Bellmin <71818636+rlajm1203@users.noreply.github.com>
2025-03-10 23:48:11 +0900

    [FEAT] 토큰 재발급, 로그아웃 기능 구현 (#263)
    * feat(main-api): api-redis 모듈 의존성 추가
    
    * refactor(main-api,api-redis): redis 설정 추가
    
    * chore(main-api): command 패키지 내부로 이동
    
    * feat(main-api): RefreshTokenStore 정의 및 구현
    
    - stub
    
    * refactor(main-api): StubRefreshTokenStore 수정
    
    - 메소드 추가
    
    * feat(main-api): AccessToken tokenId 추가
    
    - 토큰 구별을 위한, tokenId (uuid) 추가
    
    * chore(main-api): command 패키지 내부로 이동
    
    * feat(main-api): reissue API 구현
    
    - Reissue Command
    - ReissueHandler
    
    * refactor(api-query-jpa): cross JOIN 으로 변경
    
    * refactor(main-api): SecurityFilterChain URI 추가
    
    - logout
    - reissue
    
    * refactor(main-api): RefreshTokenStore 메소드 추가
    
    - clear : 전체 초기화
    - delete : 특정 Key 삭제
    
    * test(main-api): RefreshTokenStore 테스트 코드 작성
    
    * feat(main-api): 로그아웃 추가, reissue Swagger 적용
    
    * refactor(main-api): reissue , logout auth URI 에서 제거
    
    * refactor(main-api): reissue , logout 파라미터 명세 수정
    
    - hidden 속성 쿠가 및 Cookie 속성 추가
    
    * refactor(main-api): MemberCommandApi 설명 수정
    
    * feat(main-api): LogOut 기능 구현
    
    - logout 핸들러
    - logout Command
    
    * refactor(main-api): reissue , logout 스웨거 명세 수정
    
    - 파라미터 hidden
    - reissue 명세 상세화
    
    * refactor(main-api): oAuthSignUp 명세 수정
    
    - 쿠키 필요 상세화
    
    * refactor(main-api): RefreshTokenStore , StubRefreshTokenStore 패키지 이동
    
    * refactor(main-api): RefreshTokenStore -> TokenStore 클래스명 변경
    
    - RefreshToken 뿐만 아니라, 다른 토큰도 저장하기 위함
    
    * comment(main-api): 주석 수정
    
    * refactor(main-api): 토큰 저장소 이름 변경
    
    * refactor(main-api): TokenException errorCode 수정
    
    * refactor(main-api): StubTokenStore 구아바 캐시로 구현 수정
    
    - ConcurrentHashMap -> 구아바 Cache
    
    * refactor(main-api): 구아바 캐시 설정 수정 및 테스트 코드 수정
    
    - Mock 제거
    - clear 메소드 수정 (cleanUp -> invalidateAll)
    
    ---------
    
    Co-authored-by: rlajm1203 <whdlas011203@naver.com>

--- modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/AccessToken.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/shared/jwt/tokens/AccessToken.java
@@ -2,12 +2,22 @@ package com.whoz_in.main_api.shared.jwt.tokens;
 
 import com.whoz_in.domain.member.model.AccountType;
 import com.whoz_in.domain.member.model.MemberId;
+import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public final class AccessToken extends Token {
     private final MemberId memberId;
+    private final UUID tokenId;
     private final AccountType accountType;
+
+    public AccessToken(MemberId memberId, AccountType accountType){
+        this.memberId = memberId;
+        this.accountType = accountType;
+        this.tokenId = UUID.randomUUID();
+    }
+
 }
