d91e16e75f5814ef6d096caaec9da0aef440df1d
coco3x <inferior3x@gmail.com>
2025-03-12 23:52:24 +0900

    fix(network-api): 서버 시작 시 모니터 모드로 변경하는 로직 실행되도록 수정

--- modules/network-api/src/main/java/com/whoz_in/network_api/system/SystemInitializer.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/system/SystemInitializer.java
@@ -2,15 +2,18 @@ package com.whoz_in.network_api.system;
 
 import com.whoz_in.network_api.common.network_interface.NetworkInterface;
 import com.whoz_in.network_api.common.network_interface.NetworkInterfaceManager;
+import com.whoz_in.network_api.config.NetworkInterfaceProfileConfig;
 import com.whoz_in.network_api.system.routing_policy.IpRouteManager;
 import com.whoz_in.network_api.system.routing_policy.IpRuleManager;
 import com.whoz_in.network_api.system.routing_policy.MappedRtTables;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 // managed 인터페이스 중 무선 인터페이스들을 라우팅 테이블에 매칭시켜놓음
+@Slf4j
 @Profile("prod")
 @Component
 @DependsOn("systemStartupValidator") // 시스템의 초기 상태가 검증된 이후에 초기화 작업 시작하기 위함
@@ -20,7 +23,8 @@ public final class SystemInitializer {
             IpRuleManager ipRuleManager,
             IpRouteManager ipRouteManager,
             NetworkInterfaceManager niManager,
-            MonitorModeSwitcher monitorModeSwitcher
+            MonitorModeSwitcher monitorModeSwitcher,
+            NetworkInterfaceProfileConfig profileConfig
         ) {
 
         Map<String, NetworkInterface> niMap = niManager.get();
@@ -38,7 +42,13 @@ public final class SystemInitializer {
             ipRouteManager.addRoute(wirelessInterface.getNetworkAddress().gateway(), interfaceName, table);
         });
 
-        // 모니터 모드로 전환
-        monitorModeSwitcher.switchToMonitor();
+
+        NetworkInterface monitorInterface = niMap.get(profileConfig.getMonitorProfile().interfaceName());
+        String mode = monitorInterface.getWirelessInfo().mode();
+        if (!mode.equals("monitor")) {
+            log.info("{}가 모니터 모드가 아닙니다. (현재 {})", monitorInterface.getName(), mode);
+            // 모니터 모드로 전환
+            monitorModeSwitcher.switchToMonitor();
+        }
     }
 }
