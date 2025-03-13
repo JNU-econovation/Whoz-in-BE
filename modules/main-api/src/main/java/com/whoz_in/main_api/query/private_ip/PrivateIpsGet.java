22979c48fd92d7105da4568754da3ea930108489
coco3x <inferior3x@gmail.com>
2025-03-14 00:05:50 +0900

    feat: network-api에 배포된 프론트 페이지에서 main-api로 접근할 수 있도록 cors update(network->main)하는 기능

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/private_ip/PrivateIpsGet.java
+++
@@ -1,8 +0,0 @@
-package com.whoz_in.main_api.query.private_ip;
-
-import com.whoz_in.domain.shared.Nullable;
-import com.whoz_in.main_api.query.shared.application.Query;
-
-public record PrivateIpsGet(
-  @Nullable String room
-) implements Query {}
---
+++ modules/network-api/src/main/java/com/whoz_in/network_api/config/CorsOriginProvider.java
@@ -0,0 +1,35 @@
+package com.whoz_in.network_api.config;
+
+import com.whoz_in.network_api.common.network_interface.NetworkInterface;
+import com.whoz_in.network_api.common.network_interface.NetworkInterfaceManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+// 기기 등록 페이지를 위해 origin을 계산하여 제공하는 역할
+@Component
+public final class CorsOriginProvider {
+    private final NetworkInterfaceManager manager;
+    private final String internalAccessInterface;
+    private final String networkApiFrontendPort;
+
+    public CorsOriginProvider(
+            NetworkInterfaceManager manager,
+            NetworkInterfaceProfileConfig profileConfig,
+            @Value("${frontend.network-api.internal-access-ssid}") String internalAccessSsid,
+            @Value("${frontend.network-api.port}") String networkApiFrontendPort
+    ) {
+        this.manager = manager;
+        // 사용자가 어떤 와이파이에 연결돼있든 network-api에 배포된 프론트로 접근할 수 있는 ssid를 인터페이스로 변환
+        this.internalAccessInterface = profileConfig.getBySsid(internalAccessSsid).interfaceName();
+        this.networkApiFrontendPort = networkApiFrontendPort;
+    }
+
+    public String get(){
+        NetworkInterface internalAccessNI = manager.getByName(internalAccessInterface);
+        if (!internalAccessNI.isConnected()) throw new IllegalStateException(internalAccessNI + "가 인터넷에 연결되어있지 않음");
+        return "http://%s:%s".formatted(
+                internalAccessNI.getNetworkAddress().ip(),
+                networkApiFrontendPort
+        );
+    }
+}
