19868820e21110834ddcbb979875258bd35e5052
coco3x <inferior3x@gmail.com>
2025-03-22 18:29:39 +0900

    rename(m-api): 바디 캐싱 클래스 이름 변경

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/RequestBodyCachingFilter.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/RequestBodyCachingFilter.java
@@ -2,8 +2,6 @@ package com.whoz_in.main_api.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,7 +14,7 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 // HttpServletRequest를 ContentCachingRequestWrapper로 감싸서 요청 바디를 여러 번 읽을 수 있도록 함
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class RequestCachingFilter extends OncePerRequestFilter {
+public class RequestBodyCachingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
