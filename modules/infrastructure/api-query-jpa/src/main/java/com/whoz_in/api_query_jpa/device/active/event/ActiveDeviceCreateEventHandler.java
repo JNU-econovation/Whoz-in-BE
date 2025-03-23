7df66be7d8dbf3d12d96146a3b3dbad6beeae555
rlajm1203 <rlajm1203@naver.com>
2025-03-23 22:38:02 +0900

    refactor(api-query-jpa): ActiveDeviceEntity 구조 수정
    - memberId 필드 추가

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/active/event/ActiveDeviceCreateEventHandler.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/device/active/event/ActiveDeviceCreateEventHandler.java
@@ -3,6 +3,7 @@ package com.whoz_in.api_query_jpa.device.active.event;
 
 import com.whoz_in.api_query_jpa.device.active.ActiveDeviceEntity;
 import com.whoz_in.api_query_jpa.device.active.ActiveDeviceRepository;
+import com.whoz_in.api_query_jpa.member.MemberRepository;
 import com.whoz_in.main_api.shared.domain.device.active.event.DeviceCreatedEvent;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class ActiveDeviceCreateEventHandler {
 
     private final ActiveDeviceRepository activeDeviceRepository;
+    private final MemberRepository memberRepository;
 
     @EventListener(DeviceCreatedEvent.class)
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public void handle(DeviceCreatedEvent event) {
         UUID deviceId = event.deviceId();
+        UUID memberId = memberRepository.getByDeviceId(deviceId).getId();
 
-        ActiveDeviceEntity activeDevice = ActiveDeviceEntity.create(deviceId);
+        ActiveDeviceEntity activeDevice = ActiveDeviceEntity.create(deviceId, memberId);
 
         activeDeviceRepository.save(activeDevice);
     }
