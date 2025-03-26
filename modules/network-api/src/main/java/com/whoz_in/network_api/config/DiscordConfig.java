d3b1c39fcf10960d8b2013a2937302034097ee01
coco3x <inferior3x@gmail.com>
2025-03-27 07:09:05 +0900

    refactor(n-api): 디스코드 비동기로 연결

--- modules/network-api/src/main/java/com/whoz_in/network_api/config/DiscordConfig.java
+++ modules/network-api/src/main/java/com/whoz_in/network_api/config/DiscordConfig.java
@@ -1,20 +1,29 @@
 package com.whoz_in.network_api.config;
 
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@Slf4j
 @Configuration
 @Profile("prod")
-public class JdaConfig {
+public class DiscordConfig {
 
     @Bean
-    public JDA jda(@Value("${discord.bot-token}") String discordBotToken) throws InterruptedException {
-        return JDABuilder.createDefault(discordBotToken).build().awaitReady();
+    public JDA jda(@Value("${discord.bot-token}") String discordBotToken) {
+        JDA jda = JDABuilder.createDefault(discordBotToken).build();
+        jda.addEventListener(new ListenerAdapter() {
+            @Override
+            public void onReady(ReadyEvent event) {log.info("디스코드 봇이 준비되었습니다.");}
+        });
+        return jda;
     }
 
     @Bean
