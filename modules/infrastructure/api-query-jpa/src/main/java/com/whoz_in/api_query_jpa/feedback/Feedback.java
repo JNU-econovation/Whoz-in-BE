6048290edfb2a710a22eeb77b5aa86ac46a29c52
rlajm1203 <rlajm1203@naver.com>
2025-03-28 20:53:40 +0900

    refactor(api-query-jpa): @Column 어노테이션 적용

--- modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/feedback/Feedback.java
+++ modules/infrastructure/api-query-jpa/src/main/java/com/whoz_in/api_query_jpa/feedback/Feedback.java
@@ -1,5 +1,6 @@
 package com.whoz_in.api_query_jpa.feedback;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
@@ -18,16 +19,22 @@ import org.hibernate.annotations.Subselect;
 public class Feedback {
 
     @Id
+    @Column(name = "id")
     private UUID id;
 
+    @Column(name = "member_id")
     private UUID memberId;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "content")
     private String content;
 
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
 }
