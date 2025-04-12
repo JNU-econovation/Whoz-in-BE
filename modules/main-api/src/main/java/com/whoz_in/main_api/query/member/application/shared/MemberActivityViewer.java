a085db166d9cd782a0c71079a52255bf848dc94b
coco3x <inferior3x@gmail.com>
2025-04-13 00:21:01 +0900

    fix(m-api): Viewer 상속

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/shared/MemberActivityViewer.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/shared/MemberActivityViewer.java
@@ -1,7 +1,8 @@
 package com.whoz_in.main_api.query.member.application.shared;
 
+import com.whoz_in.main_api.query.shared.application.Viewer;
 import java.util.List;
 
-public interface MemberActivityViewer {
+public interface MemberActivityViewer extends Viewer {
     List<MemberActivityView> findAll();
 }
