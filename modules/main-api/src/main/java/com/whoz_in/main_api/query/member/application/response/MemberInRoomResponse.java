9d4ccfe70b33312fccdf014328a5b464e7842e21
westzeroright <kt8267@naver.com>
2025-04-24 17:15:57 +0900

    refactor: 놓친 변수명 수정

--- modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/response/MemberInRoomResponse.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/query/member/application/response/MemberInRoomResponse.java
@@ -30,7 +30,7 @@ public record MemberInRoomResponse(
     // 이 응답 내에서만 사용하는 레코드
     public record Badge(
             String badgeName,
-            String colorCode
+            String colorString
     ){
         public Badge(BadgeInfo badge) {
             this(badge.name(), badge.colorString());
