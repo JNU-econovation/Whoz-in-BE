27cb2b80debb56ff34ca988342714d3caf14a990
westzeroright <kt8267@naver.com>
2025-09-03 12:05:41 +0900

    refactor(domain): member 도메인 모델에서 이미지 관련 필드 수정
    member는 image url을 몰라야 한다. -> member가 image url을 조작하면 안 됨

--- modules/domain/src/main/java/com/whoz_in/domain/member/model/Member.java
+++ modules/domain/src/main/java/com/whoz_in/domain/member/model/Member.java
@@ -28,7 +28,7 @@ public final class Member extends AggregateRoot {
     private OAuthCredentials oAuthCredentials;
     private final Map<BadgeId, Boolean> badges; // Map<가진뱃지, 보여줌?>
     @Getter private BadgeId mainBadge; // 대표 뱃지
-    @Getter private String imageUrl; // 사용자 프로필 이미지
+    @Getter private ImageId imageId; // 사용자 프로필 이미지
 
     public OAuthCredentials getOAuthCredentials(){
         return oAuthCredentials;
@@ -67,7 +67,7 @@ public final class Member extends AggregateRoot {
     }
 
     public static Member load(MemberId id, String name, Position mainPosition, int generation, String statusMessage,
-                              OAuthCredentials oAuthCredentials, Map<BadgeId, Boolean> badges, BadgeId mainBadge, String imageUrl){
+                              OAuthCredentials oAuthCredentials, Map<BadgeId, Boolean> badges, BadgeId mainBadge, ImageId imageId){
         return builder()
                 .id(id)
                 .name(name)
@@ -77,7 +77,7 @@ public final class Member extends AggregateRoot {
                 .oAuthCredentials(oAuthCredentials)
                 .badges(badges)
                 .mainBadge(mainBadge)
-                .imageUrl(imageUrl)
+                .imageId(imageId)
                 .build();
     }
 
@@ -109,7 +109,7 @@ public final class Member extends AggregateRoot {
         this.mainBadge = badgeId;
     }
 
-    public void updateProfileImage(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void updateProfileImage(ImageId imageId) {
+        this.imageId = imageId;
     }
 }
