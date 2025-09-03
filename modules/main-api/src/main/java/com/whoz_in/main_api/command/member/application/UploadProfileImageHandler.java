c9a929918a0aa9ba97ef1e7418693775d1ad2dc4
westzeroright <kt8267@naver.com>
2025-09-03 15:51:43 +0900

    refactor(main-api): 핸들러 로직 수정
    member의 imageId를 가지고 파일 이름 사용

--- modules/main-api/src/main/java/com/whoz_in/main_api/command/member/application/UploadProfileImageHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/command/member/application/UploadProfileImageHandler.java
@@ -1,6 +1,6 @@
 package com.whoz_in.main_api.command.member.application;
 
-import com.whoz_in.domain.member.ImageRepository;
+import com.whoz_in.domain.member.ImageUploadStorage;
 import com.whoz_in.domain.member.model.Member;
 import com.whoz_in.domain.member.model.MemberId;
 import com.whoz_in.domain.member.service.MemberFinderService;
@@ -15,15 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class UploadProfileImageHandler implements CommandHandler<UploadProfileImage, Void> {
     private final RequesterInfo requesterInfo;
     private final MemberFinderService memberFinderService;
-    private final ImageRepository repository;
+    private final ImageUploadStorage imageUploadStorage;
+
 
     @Transactional
     @Override
     public Void handle(UploadProfileImage cmd) {
         MemberId requesterId = requesterInfo.getMemberId();
         Member member = memberFinderService.find(requesterId);
-        String imageUrl = repository.save(cmd.bytes());
-        member.updateProfileImage(imageUrl);
+        imageUploadStorage.save(member.getImageId(), cmd.bytes());
         return null;
     }
 }
