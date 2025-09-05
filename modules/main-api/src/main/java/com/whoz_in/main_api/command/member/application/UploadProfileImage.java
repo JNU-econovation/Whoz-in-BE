3cfd62b61de073d7330137623fc059569445f790
westzeroright <kt8267@naver.com>
2025-09-05 15:24:24 +0900

    feat(main-api): 파일 크기에 대한 유효성 검증

--- modules/main-api/src/main/java/com/whoz_in/main_api/command/member/application/UploadProfileImage.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/command/member/application/UploadProfileImage.java
@@ -1,11 +1,16 @@
 package com.whoz_in.main_api.command.member.application;
 
 import com.whoz_in.main_api.command.member.exception.EmptyFileException;
+import com.whoz_in.main_api.command.member.exception.InvalidFileSizeException;
 import com.whoz_in.main_api.command.shared.application.Command;
 
 public record UploadProfileImage(byte[] bytes) implements Command {
+    private static final int MAX_SIZE = 2 * 1024 * 1024; // 2MB
+    private static final int MIN_SIZE = 1024; // 1KB
+
     public UploadProfileImage {
         valideFileNotEmpty(bytes);
+        validateFileSize(bytes);
     }
 
     private void valideFileNotEmpty(byte[] bytes) {
@@ -14,6 +19,15 @@ public record UploadProfileImage(byte[] bytes) implements Command {
         }
         if (bytes.length == 0) {
             throw EmptyFileException.EXCEPTION;
+        }
+    }
+
+    private void validateFileSize(byte[] bytes) {
+        if (bytes.length < MIN_SIZE) {
+            throw InvalidFileSizeException.EXCEPTION;
+        }
+        if (bytes.length > MAX_SIZE) {
+            throw InvalidFileSizeException.EXCEPTION;
         }
     }
 }
