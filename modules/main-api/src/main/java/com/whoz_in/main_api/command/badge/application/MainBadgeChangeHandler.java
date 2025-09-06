42013951fb2fefaf7ba26641801c189a17ae456e
westzeroright <kt8267@naver.com>
2025-09-06 21:11:29 +0900

    feat(main-api): 뱃지 존재하는지 검증

--- modules/main-api/src/main/java/com/whoz_in/main_api/command/badge/application/MainBadgeChangeHandler.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/command/badge/application/MainBadgeChangeHandler.java
@@ -1,6 +1,8 @@
 package com.whoz_in.main_api.command.badge.application;
 
+import com.whoz_in.domain.badge.model.Badge;
 import com.whoz_in.domain.badge.model.BadgeId;
+import com.whoz_in.domain.badge.service.BadgeFinderService;
 import com.whoz_in.domain.member.MemberRepository;
 import com.whoz_in.domain.member.exception.NoMemberException;
 import com.whoz_in.domain.member.model.Member;
@@ -16,13 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class MainBadgeChangeHandler implements CommandHandler<MainBadgeChange, Void> {
     private final RequesterInfo requesterInfo;
     private final MemberRepository repository;
+    private final BadgeFinderService badgeFinderService;
 
     @Transactional
     @Override
     public Void handle(MainBadgeChange req) {
         MemberId requesterId = requesterInfo.getMemberId();
         Member member = repository.findByMemberId(requesterId).orElseThrow(()-> NoMemberException.EXCEPTION);
-        member.changeMainBadge(new BadgeId(req.badgeId()));
+        Badge badge = badgeFinderService.find(new BadgeId(req.badgeId()));
+        member.changeMainBadge(badge.getId());
         repository.save(member);
         return null;
     }
