a11a2987378f9bd964c9c35aae5dd906f7096f41
westzeroright <kt8267@naver.com>
2025-09-08 22:58:21 +0900

    feat(main-api): etag를 통한 이미지 변경사항 검사

--- modules/main-api/src/main/java/com/whoz_in/main_api/config/WebMvcConfig.java
+++ modules/main-api/src/main/java/com/whoz_in/main_api/config/WebMvcConfig.java
@@ -2,13 +2,19 @@ package com.whoz_in.main_api.config;
 
 import com.whoz_in.main_api.shared.presentation.TokenArgumentResolver;
 import com.whoz_in.main_api.shared.presentation.logging.RequesterLoggingInterceptor;
+import jakarta.servlet.Filter;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 @RequiredArgsConstructor
@@ -29,6 +35,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:/app/uploads/images/");
+                .addResourceLocations("file:/app/uploads/images/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS))
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
